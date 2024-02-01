5aea82b4ca4bf1780e781798d84108cecc9e9579
Haihao Xiang <haihao.xiang@intel.com>
2024-02-01 16:35:35 +0800

    lavc/qsvenc: update the selection of bitrate control method
    The default method is changed to CQP
    
    Signed-off-by: Haihao Xiang <haihao.xiang@intel.com>
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/qsvenc_hevc.c
+++ libavcodec/qsvenc_hevc.c
@@ -373,7 +373,7 @@ static const AVClass class = {
 };
 
 static const FFCodecDefault qsv_enc_defaults[] = {
-    { "b",         "1M"    },
+    { "b",         "0"     },
     { "refs",      "0"     },
     { "g",         "248"   },
     { "bf",        "-1"    },
