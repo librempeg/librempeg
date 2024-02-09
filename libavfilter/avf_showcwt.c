105e19fc3ae5456be00ff5166b9c8b22500dc6ad
Paul B Mahol <onemda@gmail.com>
2024-02-09 23:59:27 +0100

    avfilter/avf_showcwt: add mono/stereo cool display modes

--- libavfilter/avf_showcwt.c
+++ libavfilter/avf_showcwt.c
@@ -90,6 +90,8 @@ enum DisplayMode {
     STEREO,
     MONOHUE,
     STEREOHUE,
+    MONOCOOL,
+    STEREOCOOL,
     NB_MODE
 };
 
@@ -190,6 +192,8 @@ static const AVOption showcwt_options[] = {
     {  "stereo",    "stereo difference", 0, AV_OPT_TYPE_CONST,{.i64=STEREO},    0, 0, FLAGS, "mode" },
     {  "monohue",   "mono hue",          0, AV_OPT_TYPE_CONST,{.i64=MONOHUE},   0, 0, FLAGS, "mode" },
     {  "stereohue", "stereo hue",        0, AV_OPT_TYPE_CONST,{.i64=STEREOHUE}, 0, 0, FLAGS, "mode" },
+    {  "monocool",  "mono cool intensity",0,AV_OPT_TYPE_CONST,{.i64=MONOCOOL},  0, 0, FLAGS, "mode" },
+    {  "stereocool","stereo cool intensity",0,AV_OPT_TYPE_CONST,{.i64=STEREOCOOL},0,0,FLAGS, "mode" },
     { "slide", "set slide mode", OFFSET(slide), AV_OPT_TYPE_INT,  {.i64=0}, 0, NB_SLIDE-1, FLAGS, "slide" },
     {  "replace", "replace", 0, AV_OPT_TYPE_CONST,{.i64=SLIDE_REPLACE},0, 0, FLAGS, "slide" },
     {  "scroll",  "scroll",  0, AV_OPT_TYPE_CONST,{.i64=SLIDE_SCROLL}, 0, 0, FLAGS, "slide" },
@@ -627,6 +631,53 @@ static int draw(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
 skip:
 
         switch (mode) {
+        case STEREOCOOL:
+            {
+                const AVComplexFloat *src2 = (nb_channels > 1) ? src + ihop_size: src;
+                float z, u, v;
+
+                z = hypotf(src[0].re + src2[0].re, src[0].im + src2[0].im);
+                u = hypotf(src[0].re, src[0].im);
+                v = hypotf(src2[0].re, src2[0].im);
+
+                z  = remap_log(s, z, weight, iscale, log_factor);
+                u  = remap_log(s, u, weight, iscale, log_factor);
+                v  = remap_log(s, v, weight, iscale, log_factor);
+
+                Y  = z;
+                z  = fabsf(u - v);
+                U  = z * 0.5f * sinf(2.f * u * M_PI) + 0.5f;
+                V  = z * 0.5f * sinf(2.f * v * M_PI - M_PI) + 0.5f;
+
+                if (sono_size > 0) {
+                    dstY[0] = av_clip_uint8(lrintf(Y * 255.f));
+                    dstU[0] = av_clip_uint8(lrintf(U * 255.f));
+                    dstV[0] = av_clip_uint8(lrintf(V * 255.f));
+                    if (dstA)
+                        dstA[0] = dstY[0];
+                }
+
+                if (bar_size > 0)
+                    draw_bar(s, y, Y, U, V);
+            }
+            break;
+        case MONOCOOL:
+            Y = hypotf(src[0].re, src[0].im);
+            Y = remap_log(s, Y, weight, iscale, log_factor);
+            U = 0.25f * sinf(2.f * Y * M_PI) + 0.5f;
+            V = 0.25f * sinf(2.f * Y * M_PI - M_PI) + 0.5f;
+
+            if (sono_size > 0) {
+                dstY[0] = av_clip_uint8(lrintf(Y * 255.f));
+                dstU[0] = av_clip_uint8(lrintf(U * 255.f));
+                dstV[0] = av_clip_uint8(lrintf(V * 255.f));
+                if (dstA)
+                    dstA[0] = dstY[0];
+            }
+
+            if (bar_size > 0)
+                draw_bar(s, y, Y, U, V);
+            break;
         case STEREOHUE:
             {
                 const AVComplexFloat *src2 = (nb_channels > 1) ? src + ihop_size: src;
