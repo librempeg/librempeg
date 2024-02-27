c6d90fa0bba5eedbc67170208a69e00aa97bd1ec
Paul B Mahol <onemda@gmail.com>
2024-02-27 11:46:24 +0100

    avfilter/af_amultiply: rewrite gathering of samples

--- libavfilter/af_amultiply.c
+++ libavfilter/af_amultiply.c
@@ -41,28 +41,27 @@ typedef struct AudioMultiplyContext {
 static int activate(AVFilterContext *ctx)
 {
     AudioMultiplyContext *s = ctx->priv;
-    int i, ret, status;
-    int nb_samples;
-    int64_t pts;
 
     FF_FILTER_FORWARD_STATUS_BACK_ALL(ctx->outputs[0], ctx);
 
-    nb_samples = FFMIN(ff_inlink_queued_samples(ctx->inputs[0]),
-                       ff_inlink_queued_samples(ctx->inputs[1]));
-    for (i = 0; i < ctx->nb_inputs && nb_samples > 0; i++) {
-        if (s->frames[i])
-            continue;
+    if (!s->frames[0]) {
+        int ret = ff_inlink_consume_frame(ctx->inputs[0], &s->frames[0]);
+        if (ret < 0)
+            return ret;
+    }
 
-        if (ff_inlink_check_available_samples(ctx->inputs[i], nb_samples) > 0) {
-            ret = ff_inlink_consume_samples(ctx->inputs[i], nb_samples, nb_samples, &s->frames[i]);
-            if (ret < 0)
-                return ret;
-        }
+    if (s->frames[0] && !s->frames[1]) {
+        int ret = ff_inlink_consume_samples(ctx->inputs[1],
+                                            s->frames[0]->nb_samples,
+                                            s->frames[0]->nb_samples,
+                                            &s->frames[1]);
+        if (ret < 0)
+            return ret;
     }
 
     if (s->frames[0] && s->frames[1]) {
-        AVFrame *out;
         int plane_samples;
+        AVFrame *out;
 
         if (av_sample_fmt_is_planar(ctx->inputs[0]->format))
             plane_samples = FFALIGN(s->frames[0]->nb_samples, s->samples_align);
@@ -70,21 +69,22 @@ static int activate(AVFilterContext *ctx)
             plane_samples = FFALIGN(s->frames[0]->nb_samples * s->channels, s->samples_align);
 
         out = ff_get_audio_buffer(ctx->outputs[0], s->frames[0]->nb_samples);
-        if (!out)
+        if (!out) {
+            av_frame_free(&s->frames[0]);
+            av_frame_free(&s->frames[1]);
             return AVERROR(ENOMEM);
-
-        out->pts = s->frames[0]->pts;
-        out->duration = s->frames[0]->duration;
+        }
+        av_frame_copy_props(out, s->frames[0]);
 
         if (av_get_packed_sample_fmt(ctx->inputs[0]->format) == AV_SAMPLE_FMT_FLT) {
-            for (i = 0; i < s->planes; i++) {
+            for (int i = 0; i < s->planes; i++) {
                 s->fdsp->vector_fmul((float *)out->extended_data[i],
                                      (const float *)s->frames[0]->extended_data[i],
                                      (const float *)s->frames[1]->extended_data[i],
                                      plane_samples);
             }
         } else {
-            for (i = 0; i < s->planes; i++) {
+            for (int i = 0; i < s->planes; i++) {
                 s->fdsp->vector_dmul((double *)out->extended_data[i],
                                      (const double *)s->frames[0]->extended_data[i],
                                      (const double *)s->frames[1]->extended_data[i],
@@ -95,23 +95,22 @@ static int activate(AVFilterContext *ctx)
         av_frame_free(&s->frames[0]);
         av_frame_free(&s->frames[1]);
 
-        ret = ff_filter_frame(ctx->outputs[0], out);
-        if (ret < 0)
-            return ret;
+        return ff_filter_frame(ctx->outputs[0], out);
     }
 
-    if (!nb_samples) {
-        for (i = 0; i < 2; i++) {
-            if (ff_inlink_acknowledge_status(ctx->inputs[i], &status, &pts)) {
-                ff_outlink_set_status(ctx->outputs[0], status, pts);
-                return 0;
-            }
+    for (int i = 0; i < 2; i++) {
+        int64_t pts;
+        int status;
+
+        if (ff_inlink_acknowledge_status(ctx->inputs[i], &status, &pts)) {
+            ff_outlink_set_status(ctx->outputs[0], status, pts);
+            return 0;
         }
     }
 
     if (ff_outlink_frame_wanted(ctx->outputs[0])) {
-        for (i = 0; i < 2; i++) {
-            if (s->frames[i] || ff_inlink_queued_samples(ctx->inputs[i]) > 0)
+        for (int i = 0; i < 2; i++) {
+            if (s->frames[i])
                 continue;
             ff_inlink_request_frame(ctx->inputs[i]);
             return 0;
@@ -147,6 +146,10 @@ static av_cold int init(AVFilterContext *ctx)
 static av_cold void uninit(AVFilterContext *ctx)
 {
     AudioMultiplyContext *s = ctx->priv;
+
+    av_frame_free(&s->frames[0]);
+    av_frame_free(&s->frames[1]);
+
     av_freep(&s->fdsp);
 }
 
