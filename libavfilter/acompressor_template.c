e717f8f5ec2ca26f29ca644eb284f4bc57e2068e
Paul B Mahol <onemda@gmail.com>
2024-03-24 13:04:44 +0100

    avfilter/af_acompressor: refactor using template
    Add float sample format support.
    Add none link type.

---
+++ libavfilter/acompressor_template.c
@@ -0,0 +1,191 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#undef FABS
+#undef FEXP
+#undef FLOG
+#undef FMAX
+#undef ftype
+#undef SAMPLE_FORMAT
+#if DEPTH == 32
+#define FABS fabsf
+#define FEXP expf
+#define FLOG logf
+#define FMAX fmaxf
+#define ftype float
+#define SAMPLE_FORMAT flt
+#else
+#define FABS fabs
+#define FEXP exp
+#define FLOG log
+#define FMAX fmax
+#define ftype double
+#define SAMPLE_FORMAT dbl
+#endif
+
+#define F(x) ((ftype)(x))
+
+#define LIN2LOG(x) (FLOG((x)))
+#define LOG2LIN(x) (FEXP((x)))
+
+#define fn3(a,b)   a##_##b
+#define fn2(a,b)   fn3(a,b)
+#define fn(a)      fn2(a, SAMPLE_FORMAT)
+
+static ftype fn(output_gain)(ftype lin_slope, ftype ratio, ftype thres,
+                             ftype knee, ftype knee_start, ftype knee_stop,
+                             ftype compressed_knee_start,
+                             ftype compressed_knee_stop,
+                             int detection, int mode)
+{
+    ftype slope = FLOG(lin_slope);
+    ftype gain = F(0.0);
+    ftype delta = F(0.0);
+
+    if (detection)
+        slope *= F(0.5);
+
+    gain = (slope - thres) / ratio + thres;
+    delta = F(1.0) / ratio;
+
+    if (mode) {
+        if (knee > F(1.0) && slope > knee_start)
+            gain = hermite_interpolation(slope, knee_stop, knee_start,
+                                         knee_stop, compressed_knee_start,
+                                         F(1.0), delta);
+    } else {
+        if (knee > F(1.0) && slope < knee_stop)
+            gain = hermite_interpolation(slope, knee_start, knee_stop,
+                                         knee_start, compressed_knee_stop,
+                                         F(1.0), delta);
+    }
+
+    return FEXP(gain - slope);
+}
+
+static void fn(compress)(AVFilterContext *ctx, AVFrame *out, const int nb_samples,
+                         AVFilterLink *inlink, AVFilterLink *sclink)
+{
+    AudioCompressorContext *s = ctx->priv;
+    AVFrame *sc = s->sc ? s->sc : s->in;
+    AVFrame *in = s->in;
+    const ftype mix = s->mix;
+    const ftype *scsrc = (const ftype *)sc->data[0];
+    const ftype *src = (const ftype *)in->data[0];
+    ftype *dst = (ftype *)out->data[0];
+    const int sc_nb_channels = sclink->ch_layout.nb_channels;
+    const int nb_channels = inlink->ch_layout.nb_channels;
+    const ftype lin_knee_start = s->lin_knee_start;
+    const ftype lin_knee_stop = s->lin_knee_stop;
+    const ftype makeup = s->makeup;
+    const ftype attack_coeff = s->attack_coeff;
+    const ftype release_coeff = s->release_coeff;
+    const int is_disabled = ctx->is_disabled;
+    const int detection = s->detection;
+    ftype *lin_slopep = s->lin_slope;
+    const ftype ratio = s->ratio;
+    const ftype compressed_knee_start = s->compressed_knee_start;
+    const ftype compressed_knee_stop = s->compressed_knee_stop;
+    const ftype adj_knee_start = s->adj_knee_start;
+    const ftype adj_knee_stop = s->adj_knee_stop;
+    const ftype knee_start = s->knee_start;
+    const ftype knee_stop = s->knee_stop;
+    const ftype level_in = s->level_in;
+    const ftype level_sc = s->level_sc;
+    const ftype thres = s->thres;
+    const ftype knee = s->knee;
+    const int link = s->link;
+    const int mode = s->mode;
+
+    for (int n = 0; n < nb_samples; n++, src += nb_channels, dst += nb_channels, scsrc += sc_nb_channels) {
+        ftype abs_sample, factor, lin_slope, detector;
+        ftype gain = F(1.0);
+        int detected;
+
+        switch (link) {
+        case LINK_MAX:
+        case LINK_AVG:
+            lin_slope = lin_slopep[0];
+            abs_sample = FABS(scsrc[0] * level_sc);
+            if (link == LINK_MAX) {
+                for (int c = 1; c < sc_nb_channels; c++)
+                    abs_sample = FMAX(FABS(scsrc[c] * level_sc), abs_sample);
+            } else {
+                for (int c = 1; c < sc_nb_channels; c++)
+                    abs_sample += FABS(scsrc[c] * level_sc);
+                abs_sample /= sc_nb_channels;
+            }
+
+            if (detection)
+                abs_sample *= abs_sample;
+            lin_slope += (abs_sample - lin_slope) * (abs_sample > lin_slope ? attack_coeff : release_coeff);
+
+            if (mode) {
+                detector = (detection ? adj_knee_stop : lin_knee_stop);
+                detected = lin_slope < detector;
+            } else {
+                detector = (detection ? adj_knee_start : lin_knee_start);
+                detected = lin_slope > detector;
+            }
+
+            if (lin_slope > F(0.0) && detected)
+                gain = fn(output_gain)(lin_slope, ratio, thres,
+                                       knee, knee_start, knee_stop,
+                                       compressed_knee_start,
+                                       compressed_knee_stop,
+                                       detection, mode);
+
+            factor = is_disabled ? F(1.0) : level_in * (gain * makeup * mix + (F(1.0) - mix));
+            for (int c = 0; c < nb_channels; c++)
+                dst[c] = src[c] * factor;
+            lin_slopep[0] = lin_slope;
+            break;
+        case LINK_NONE:
+            for (int c = 0; c < sc_nb_channels; c++) {
+                ftype lin_slope = lin_slopep[c];
+                ftype gain = F(1.0);
+
+                abs_sample = FABS(scsrc[c] * level_sc);
+                if (detection)
+                    abs_sample *= abs_sample;
+                lin_slope += (abs_sample - lin_slope) * (abs_sample > lin_slope ? attack_coeff : release_coeff);
+
+                if (mode) {
+                    detector = (detection ? adj_knee_stop : lin_knee_stop);
+                    detected = lin_slope < detector;
+                } else {
+                    detector = (detection ? adj_knee_start : lin_knee_start);
+                    detected = lin_slope > detector;
+                }
+
+                if (lin_slope > F(0.0) && detected)
+                    gain = fn(output_gain)(lin_slope, ratio, thres,
+                                           knee, knee_start, knee_stop,
+                                           compressed_knee_start,
+                                           compressed_knee_stop,
+                                           detection, mode);
+
+                factor = is_disabled ? F(1.0) : level_in * (gain * makeup * mix + (F(1.0) - mix));
+                dst[c] = src[c] * factor;
+
+                lin_slopep[c] = lin_slope;
+            }
+            break;
+        }
+    }
+}
--- libavfilter/af_acompressor.c
+++ libavfilter/af_acompressor.c
@@ -35,6 +35,13 @@
 #include "hermite.h"
 #include "internal.h"
 
+enum LinkMode {
+    LINK_NONE,
+    LINK_AVG,
+    LINK_MAX,
+    NB_LINK
+};
+
 typedef struct AudioCompressorContext {
     const AVClass *class;
 
@@ -42,7 +49,6 @@ typedef struct AudioCompressorContext {
     double level_sc;
     double attack, attack_coeff;
     double release, release_coeff;
-    double lin_slope;
     double ratio;
     double threshold;
     double makeup;
@@ -62,143 +68,57 @@ typedef struct AudioCompressorContext {
     int mode;
     int sidechain;
 
+    void *lin_slope;
+
     AVFrame *in, *sc;
+
+    void (*compress)(AVFilterContext *ctx, AVFrame *out, const int nb_samples,
+                     AVFilterLink *inlink, AVFilterLink *sclink);
 } AudioCompressorContext;
 
 #define OFFSET(x) offsetof(AudioCompressorContext, x)
-#define A AV_OPT_FLAG_AUDIO_PARAM
-#define F AV_OPT_FLAG_FILTERING_PARAM
-#define R AV_OPT_FLAG_RUNTIME_PARAM
+#define AFR AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_RUNTIME_PARAM
+#define AF AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
 
 static const AVOption acompressor_options[] = {
-    { "level_in",  "set input gain",     OFFSET(level_in),  AV_OPT_TYPE_DOUBLE, {.dbl=1},        0.015625,   64, A|F|R },
-    { "mode",      "set mode",           OFFSET(mode),      AV_OPT_TYPE_INT,    {.i64=0},               0,    1, A|F|R, .unit = "mode" },
-    {   "downward",0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=0},               0,    0, A|F|R, .unit = "mode" },
-    {   "upward",  0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=1},               0,    0, A|F|R, .unit = "mode" },
-    { "threshold", "set threshold",      OFFSET(threshold), AV_OPT_TYPE_DOUBLE, {.dbl=0.125}, 0.000976563,    1, A|F|R },
-    { "ratio",     "set ratio",          OFFSET(ratio),     AV_OPT_TYPE_DOUBLE, {.dbl=2},               1,   20, A|F|R },
-    { "attack",    "set attack",         OFFSET(attack),    AV_OPT_TYPE_DOUBLE, {.dbl=20},           0.01, 2000, A|F|R },
-    { "release",   "set release",        OFFSET(release),   AV_OPT_TYPE_DOUBLE, {.dbl=250},          0.01, 9000, A|F|R },
-    { "makeup",    "set make up gain",   OFFSET(makeup),    AV_OPT_TYPE_DOUBLE, {.dbl=1},               1,   64, A|F|R },
-    { "knee",      "set knee",           OFFSET(knee),      AV_OPT_TYPE_DOUBLE, {.dbl=2.82843},         1,    8, A|F|R },
-    { "link",      "set link type",      OFFSET(link),      AV_OPT_TYPE_INT,    {.i64=0},               0,    1, A|F|R, .unit = "link" },
-    {   "average", 0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=0},               0,    0, A|F|R, .unit = "link" },
-    {   "maximum", 0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=1},               0,    0, A|F|R, .unit = "link" },
-    { "detection", "set detection",      OFFSET(detection), AV_OPT_TYPE_INT,    {.i64=1},               0,    1, A|F|R, .unit = "detection" },
-    {   "peak",    0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=0},               0,    0, A|F|R, .unit = "detection" },
-    {   "rms",     0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=1},               0,    0, A|F|R, .unit = "detection" },
-    { "level_sc",  "set sidechain gain", OFFSET(level_sc),  AV_OPT_TYPE_DOUBLE, {.dbl=1},        0.015625,   64, A|F|R },
-    { "mix",       "set mix",            OFFSET(mix),       AV_OPT_TYPE_DOUBLE, {.dbl=1},               0,    1, A|F|R },
-    { "sidechain", "enable sidechain input",OFFSET(sidechain),AV_OPT_TYPE_BOOL, {.i64=0},               0,    1, A|F },
+    { "level_in",  "set input gain",     OFFSET(level_in),  AV_OPT_TYPE_DOUBLE, {.dbl=1},        0.015625,   64, AFR },
+    { "mode",      "set mode",           OFFSET(mode),      AV_OPT_TYPE_INT,    {.i64=0},               0,    1, AFR, .unit = "mode" },
+    {   "downward",0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=0},               0,    0, AFR, .unit = "mode" },
+    {   "upward",  0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=1},               0,    0, AFR, .unit = "mode" },
+    { "threshold", "set threshold",      OFFSET(threshold), AV_OPT_TYPE_DOUBLE, {.dbl=0.125}, 0.000976563,    1, AFR },
+    { "ratio",     "set ratio",          OFFSET(ratio),     AV_OPT_TYPE_DOUBLE, {.dbl=2},               1,   20, AFR },
+    { "attack",    "set attack",         OFFSET(attack),    AV_OPT_TYPE_DOUBLE, {.dbl=20},           0.01, 2000, AFR },
+    { "release",   "set release",        OFFSET(release),   AV_OPT_TYPE_DOUBLE, {.dbl=250},          0.01, 9000, AFR },
+    { "makeup",    "set make up gain",   OFFSET(makeup),    AV_OPT_TYPE_DOUBLE, {.dbl=1},               1,   64, AFR },
+    { "knee",      "set knee",           OFFSET(knee),      AV_OPT_TYPE_DOUBLE, {.dbl=2.82843},         1,    8, AFR },
+    { "link",      "set link type",      OFFSET(link),      AV_OPT_TYPE_INT,    {.i64=0},               0, NB_LINK-1, AFR, .unit = "link" },
+    {   "none",    0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=LINK_NONE},       0,    0, AFR, .unit = "link" },
+    {   "average", 0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=LINK_AVG},        0,    0, AFR, .unit = "link" },
+    {   "maximum", 0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=LINK_MAX},        0,    0, AFR, .unit = "link" },
+    { "detection", "set detection",      OFFSET(detection), AV_OPT_TYPE_INT,    {.i64=1},               0,    1, AFR, .unit = "detection" },
+    {   "peak",    0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=0},               0,    0, AFR, .unit = "detection" },
+    {   "rms",     0,                    0,                 AV_OPT_TYPE_CONST,  {.i64=1},               0,    0, AFR, .unit = "detection" },
+    { "level_sc",  "set sidechain gain", OFFSET(level_sc),  AV_OPT_TYPE_DOUBLE, {.dbl=1},        0.015625,   64, AFR },
+    { "mix",       "set mix",            OFFSET(mix),       AV_OPT_TYPE_DOUBLE, {.dbl=1},               0,    1, AFR },
+    { "sidechain", "enable sidechain input",OFFSET(sidechain),AV_OPT_TYPE_BOOL, {.i64=0},               0,    1, AF },
     { NULL }
 };
 
 AVFILTER_DEFINE_CLASS(acompressor);
 
-// A fake infinity value (because real infinity may break some hosts)
-#define FAKE_INFINITY (65536.0 * 65536.0)
+#define DEPTH 32
+#include "acompressor_template.c"
 
-// Check for infinity (with appropriate-ish tolerance)
-#define IS_FAKE_INFINITY(value) (fabs(value-FAKE_INFINITY) < 1.0)
-
-static double output_gain(double lin_slope, double ratio, double thres,
-                          double knee, double knee_start, double knee_stop,
-                          double compressed_knee_start,
-                          double compressed_knee_stop,
-                          int detection, int mode)
-{
-    double slope = log(lin_slope);
-    double gain = 0.0;
-    double delta = 0.0;
-
-    if (detection)
-        slope *= 0.5;
-
-    if (IS_FAKE_INFINITY(ratio)) {
-        gain = thres;
-        delta = 0.0;
-    } else {
-        gain = (slope - thres) / ratio + thres;
-        delta = 1.0 / ratio;
-    }
-
-    if (mode) {
-        if (knee > 1.0 && slope > knee_start)
-            gain = hermite_interpolation(slope, knee_stop, knee_start,
-                                         knee_stop, compressed_knee_start,
-                                         1.0, delta);
-    } else {
-        if (knee > 1.0 && slope < knee_stop)
-            gain = hermite_interpolation(slope, knee_start, knee_stop,
-                                         knee_start, compressed_knee_stop,
-                                         1.0, delta);
-    }
-
-    return exp(gain - slope);
-}
-
-static void compressor(AudioCompressorContext *s,
-                       const double *src, double *dst, const double *scsrc, int nb_samples,
-                       double level_in, double level_sc,
-                       AVFilterLink *inlink, AVFilterLink *sclink)
-{
-    AVFilterContext *ctx = inlink->dst;
-    const double makeup = s->makeup;
-    const double mix = s->mix;
-    int i, c;
-
-    for (i = 0; i < nb_samples; i++) {
-        double abs_sample, gain = 1.0;
-        double detector, factor;
-        int detected;
-
-        abs_sample = fabs(scsrc[0] * level_sc);
-
-        if (s->link == 1) {
-            for (c = 1; c < sclink->ch_layout.nb_channels; c++)
-                abs_sample = FFMAX(fabs(scsrc[c] * level_sc), abs_sample);
-        } else {
-            for (c = 1; c < sclink->ch_layout.nb_channels; c++)
-                abs_sample += fabs(scsrc[c] * level_sc);
-
-            abs_sample /= sclink->ch_layout.nb_channels;
-        }
-
-        if (s->detection)
-            abs_sample *= abs_sample;
-
-        s->lin_slope += (abs_sample - s->lin_slope) * (abs_sample > s->lin_slope ? s->attack_coeff : s->release_coeff);
-
-        if (s->mode) {
-            detector = (s->detection ? s->adj_knee_stop : s->lin_knee_stop);
-            detected = s->lin_slope < detector;
-        } else {
-            detector = (s->detection ? s->adj_knee_start : s->lin_knee_start);
-            detected = s->lin_slope > detector;
-        }
-
-        if (s->lin_slope > 0.0 && detected)
-            gain = output_gain(s->lin_slope, s->ratio, s->thres, s->knee,
-                               s->knee_start, s->knee_stop,
-                               s->compressed_knee_start,
-                               s->compressed_knee_stop,
-                               s->detection, s->mode);
-
-        factor = ctx->is_disabled ? 1.0 : level_in * (gain * makeup * mix + (1. - mix));
-        for (c = 0; c < inlink->ch_layout.nb_channels; c++)
-            dst[c] = src[c] * factor;
-
-        src += inlink->ch_layout.nb_channels;
-        dst += inlink->ch_layout.nb_channels;
-        scsrc += sclink->ch_layout.nb_channels;
-    }
-}
+#undef DEPTH
+#define DEPTH 64
+#include "acompressor_template.c"
 
 static int config_output(AVFilterLink *outlink)
 {
     AVFilterContext *ctx = outlink->src;
     AVFilterLink *inlink = ctx->inputs[0];
     AudioCompressorContext *s = ctx->priv;
+    size_t sample_size;
 
     outlink->time_base = inlink->time_base;
 
@@ -214,6 +134,22 @@ static int config_output(AVFilterLink *outlink)
 
     s->attack_coeff = FFMIN(1., 1. / (s->attack * outlink->sample_rate / 4000.));
     s->release_coeff = FFMIN(1., 1. / (s->release * outlink->sample_rate / 4000.));
+
+    switch (outlink->format) {
+    case AV_SAMPLE_FMT_FLT:
+        s->compress = compress_flt;
+        sample_size = sizeof(float);
+        break;
+    case AV_SAMPLE_FMT_DBL:
+        s->compress = compress_dbl;
+        sample_size = sizeof(double);
+        break;
+    }
+
+    if (!s->lin_slope)
+        s->lin_slope = av_calloc(inlink->ch_layout.nb_channels, sample_size);
+    if (!s->lin_slope)
+        return AVERROR(ENOMEM);
 
     return 0;
 }
@@ -238,11 +174,7 @@ static int filter_frame(AVFilterLink *outlink)
     AudioCompressorContext *s = ctx->priv;
     AVFilterLink *inlink = ctx->inputs[0];
     AVFilterLink *sclink = s->sidechain ? ctx->inputs[1] : inlink;
-    AVFrame *sc = s->sc ? s->sc : s->in;
-    const double *scsrc = (const double *)sc->data[0];
-    const double *src = (const double *)s->in->data[0];
     AVFrame *out;
-    double *dst;
 
     if (av_frame_is_writable(s->in)) {
         out = s->in;
@@ -255,11 +187,8 @@ static int filter_frame(AVFilterLink *outlink)
         }
         av_frame_copy_props(out, s->in);
     }
-    dst = (double *)out->data[0];
 
-    compressor(s, src, dst, scsrc, s->in->nb_samples,
-               s->level_in, s->level_in,
-               inlink, sclink);
+    s->compress(ctx, out, s->in->nb_samples, inlink, sclink);
 
     if (out != s->in)
         av_frame_free(&s->in);
@@ -339,6 +268,8 @@ static av_cold void uninit(AVFilterContext *ctx)
 
     av_frame_free(&s->in);
     av_frame_free(&s->sc);
+
+    av_freep(&s->lin_slope);
 }
 
 static const AVFilterPad outputs[] = {
@@ -359,7 +290,7 @@ const AVFilter ff_af_acompressor = {
     .uninit         = uninit,
     .inputs         = NULL,
     FILTER_OUTPUTS(outputs),
-    FILTER_SINGLE_SAMPLEFMT(AV_SAMPLE_FMT_DBL),
+    FILTER_SAMPLEFMTS(AV_SAMPLE_FMT_FLT, AV_SAMPLE_FMT_DBL),
     .process_command = process_command,
     .flags          = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL |
                       AVFILTER_FLAG_DYNAMIC_INPUTS,
