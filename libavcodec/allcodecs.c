AUGxhub/FFmpeg

avcodec: add ADPCM IMA HVQM4 decoder

libavcodec/allcodecs.c
@@ -637,6 +637,7 @@ extern const AVCodec ff_adpcm_ima_dk3_decoder;
 extern const AVCodec ff_adpcm_ima_dk4_decoder;
 extern const AVCodec ff_adpcm_ima_ea_eacs_decoder;
 extern const AVCodec ff_adpcm_ima_ea_sead_decoder;
+extern const AVCodec ff_adpcm_ima_hvqm4_decoder;
 extern const AVCodec ff_adpcm_ima_iss_decoder;
 extern const AVCodec ff_adpcm_ima_moflex_decoder;
 extern const AVCodec ff_adpcm_ima_mtf_decoder;

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavcodec/allcodecs.c
@@ -668,6 +668,7 @@ extern const FFCodec ff_adpcm_ima_dk3_decoder;
 extern const FFCodec ff_adpcm_ima_dk4_decoder;
 extern const FFCodec ff_adpcm_ima_ea_eacs_decoder;
 extern const FFCodec ff_adpcm_ima_ea_sead_decoder;
+extern const FFCodec ff_adpcm_ima_hvqm4_decoder;
 extern const FFCodec ff_adpcm_ima_iss_decoder;
 extern const FFCodec ff_adpcm_ima_moflex_decoder;
 extern const FFCodec ff_adpcm_ima_mtf_decoder;