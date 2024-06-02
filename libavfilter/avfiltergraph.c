0794047464b2af1d3cebb176985ee76787390b68
Paul B Mahol <onemda@gmail.com>
2024-06-03 00:19:42 +0200

    avfilter/avfiltergraph: fix missed line in 7d05d878e4275e256

--- libavfilter/avfiltergraph.c
+++ libavfilter/avfiltergraph.c
@@ -694,6 +694,7 @@ static int pick_format(AVFilterLink *link, AVFilterLink *ref)
                 av_get_bytes_per_sample(link->incfg.formats->formats[n]))
                 link->format = link->incfg.formats->formats[n];
         }
+        link->incfg.formats->formats[0] = link->format;
         link->incfg.formats->nb_formats = 1;
     }
 
