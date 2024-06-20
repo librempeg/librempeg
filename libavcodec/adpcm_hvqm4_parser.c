f7da7419e1d8f78762f885b700cfaf8f51610a95
Paul B Mahol <onemda@gmail.com>
2024-06-20 21:47:45 +0200

    avcodec: add ADPCM HVQM4 parser

---
+++ libavcodec/adpcm_hvqm4_parser.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * ADPCM HVQM4 parser
+ *
+ * Determines the duration for each packet.
+ */
+
+#include "libavutil/intreadwrite.h"
+#include "parser.h"
+
+static int adpcm_hvqm4_parse(AVCodecParserContext *s1, AVCodecContext *avctx,
+                             const uint8_t **poutbuf, int *poutbuf_size,
+                             const uint8_t *buf, int buf_size)
+{
+    if (buf_size > 2) {
+        const int channels = avctx->ch_layout.nb_channels;
+        const int frame_format = AV_RB16(buf);
+        int skip = 6;
+
+        if (frame_format == 1)
+            skip += 2 * channels;
+        if (frame_format == 3)
+            skip += 3 * channels;
+
+        s1->duration = (buf_size - skip) * 2 / channels;
+    }
+
+    /* always return the full packet. this parser isn't doing any splitting or
+       combining, only packet analysis */
+    *poutbuf      = buf;
+    *poutbuf_size = buf_size;
+    return buf_size;
+}
+
+const AVCodecParser ff_adpcm_hvqm4_parser = {
+    .codec_ids      = { AV_CODEC_ID_ADPCM_IMA_HVQM4 },
+    .parser_parse   = adpcm_hvqm4_parse,
+};
