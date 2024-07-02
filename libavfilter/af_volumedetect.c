dbf20122a9dbc7a86f587246630dccc7b38c06fc
Paul B Mahol <onemda@gmail.com>
2024-07-02 12:24:41 +0200

    avfilter/af_volumedetect: rewrite using template

--- libavfilter/af_volumedetect.c
+++ libavfilter/af_volumedetect.c
@@ -20,191 +20,82 @@
 
 #include "libavutil/channel_layout.h"
 #include "libavutil/avassert.h"
+#include "libavutil/ffmath.h"
 #include "libavutil/mem.h"
 #include "audio.h"
 #include "avfilter.h"
 #include "internal.h"
 
-#define MAX_DB_FLT 1024
-#define MAX_DB 91
-#define HISTOGRAM_SIZE 0x10000
-#define HISTOGRAM_SIZE_FLT (MAX_DB_FLT*2)
+#define HISTOGRAM_SIZE 32769
 
 typedef struct VolDetectContext {
-    uint64_t* histogram; ///< for integer number of samples at each PCM value, for float number of samples at each dB
-    uint64_t nb_samples; ///< number of samples
-    double sum2;         ///< sum of the squares of the samples
-    double max;          ///< maximum sample value
-    int is_float;        ///< true if the input is in floating point
+    uint64_t histogram[HISTOGRAM_SIZE];
+    float max;
+    double sum2;
+
+    void (*update_histogram)(AVFilterContext *ctx, const AVFrame *in);
+    void (*print_stats)(AVFilterContext *ctx);
 } VolDetectContext;
 
-static inline double logdb(double v, enum AVSampleFormat sample_fmt)
-{
-    if (sample_fmt == AV_SAMPLE_FMT_FLT) {
-        if (!v)
-            return MAX_DB_FLT;
-        return -log10(v) * 10;
-    } else {
-        double d = v / (double)(0x8000 * 0x8000);
-        if (!v)
-            return MAX_DB;
-        return -log10(d) * 10;
-    }
-}
+#define DEPTH 16
+#define PLANAR 0
+#include "volumedetect_template.c"
 
-static void update_float_stats(VolDetectContext *vd, float *audio_data)
-{
-    double sample;
-    int idx;
+#undef PLANAR
+#define PLANAR 1
+#include "volumedetect_template.c"
 
-    if (!isnormal(*audio_data))
-        return;
-    sample = fabsf(*audio_data);
-    if (sample > vd->max)
-        vd->max = sample;
-    vd->sum2 += sample * sample;
-    idx = MAX_DB_FLT + lrintf(floorf(logdb(sample*sample, AV_SAMPLE_FMT_FLT)));
-    vd->histogram[idx]++;
-}
+#undef DEPTH
+#define DEPTH 32
+#include "volumedetect_template.c"
 
-static int filter_frame(AVFilterLink *inlink, AVFrame *samples)
+#undef PLANAR
+#define PLANAR 0
+#include "volumedetect_template.c"
+
+static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 {
     AVFilterContext *ctx = inlink->dst;
-    VolDetectContext *vd = ctx->priv;
-    int nb_channels = samples->ch_layout.nb_channels;
-    int nb_planes   = nb_channels;
-    int planar      = 0;
-    int plane, i;
+    VolDetectContext *s = ctx->priv;
 
-    planar = av_sample_fmt_is_planar(samples->format);
-    if (!planar)
-        nb_planes = 1;
-    if (vd->is_float) {
-        float *audio_data;
-        for (plane = 0; plane < nb_planes; plane++) {
-            audio_data = (float *)samples->extended_data[plane];
-            for (i = 0; i < samples->nb_samples; i++) {
-                if (planar) {
-                    update_float_stats(vd, &audio_data[i]);
-                } else {
-                    for (int j = 0; j < nb_channels; j++)
-                        update_float_stats(vd, &audio_data[i * nb_channels + j]);
-                }
-            }
-        }
-    } else {
-        int16_t *pcm;
-        for (plane = 0; plane < nb_planes; plane++) {
-            pcm = (int16_t *)samples->extended_data[plane];
-            for (i = 0; i < samples->nb_samples; i++) {
-                if (planar) {
-                    vd->histogram[pcm[i] + 0x8000]++;
-                } else {
-                    for (int j = 0; j < nb_channels; j++) {
-                        vd->histogram[pcm[i * nb_channels + j] + 0x8000]++;
-                    }
-                }
-            }
-        }
-    }
-    vd->nb_samples += samples->nb_samples * nb_channels;
-    return ff_filter_frame(inlink->dst->outputs[0], samples);
-}
+    s->update_histogram(ctx, in);
 
-static void print_stats(AVFilterContext *ctx)
-{
-    VolDetectContext *vd = ctx->priv;
-
-    if (!vd->nb_samples)
-        return;
-    if (vd->is_float) {
-        uint64_t sum = 0;
-        int i;
-
-        av_log(ctx, AV_LOG_INFO, "n_samples: %" PRId64 "\n", vd->nb_samples);
-        av_log(ctx, AV_LOG_INFO, "mean_volume: %.1f dB\n", -logdb(vd->sum2 / vd->nb_samples, AV_SAMPLE_FMT_FLT));
-        av_log(ctx, AV_LOG_INFO, "max_volume: %.1f dB\n", -2.0*logdb(vd->max, AV_SAMPLE_FMT_FLT));
-        for (i = 0; i < HISTOGRAM_SIZE_FLT && !vd->histogram[i]; i++)
-        for (; i < HISTOGRAM_SIZE_FLT && sum < vd->nb_samples / 1000; i++) {
-            if (!vd->histogram[i])
-                continue;
-            av_log(ctx, AV_LOG_INFO, "histogram_%ddb: %" PRId64 "\n", MAX_DB_FLT-i, vd->histogram[i]);
-            sum += vd->histogram[i];
-        }
-    } else {
-        int i, max_volume, shift;
-        uint64_t nb_samples = 0, power = 0, nb_samples_shift = 0, sum = 0;
-        uint64_t histdb[MAX_DB + 1] = {0};
-        for (i = 0; i < 0x10000; i++)
-            nb_samples += vd->histogram[i];
-        av_log(ctx, AV_LOG_INFO, "n_samples: %" PRId64 "\n", nb_samples);
-        /*
-            * If nb_samples > 1<<34, there is a risk of overflow in the
-            * multiplication or the sum: shift all histogram values to avoid that.
-            * The total number of samples must be recomputed to avoid rounding
-            * errors.
-        */
-        shift = av_log2(nb_samples >> 33);
-        for (i = 0; i < 0x10000; i++) {
-            nb_samples_shift += vd->histogram[i] >> shift;
-            power += (i - 0x8000) * (i - 0x8000) * (vd->histogram[i] >> shift);
-        }
-        if (!nb_samples_shift)
-            return;
-        power = (power + nb_samples_shift / 2) / nb_samples_shift;
-        av_assert0(power <= 0x8000 * 0x8000);
-        av_log(ctx, AV_LOG_INFO, "mean_volume: %.1f dB\n", -logdb((double)power, AV_SAMPLE_FMT_S16));
-        max_volume = 0x8000;
-        while (max_volume > 0 && !vd->histogram[0x8000 + max_volume] &&
-                !vd->histogram[0x8000 - max_volume])
-            max_volume--;
-        av_log(ctx, AV_LOG_INFO, "max_volume: %.1f dB\n", -logdb((double)(max_volume * max_volume), AV_SAMPLE_FMT_S16));
-        for (i = 0; i < 0x10000; i++)
-            histdb[(int)logdb((double)(i - 0x8000) * (i - 0x8000), AV_SAMPLE_FMT_S16)] += vd->histogram[i];
-        for (i = 0; i <= MAX_DB && !histdb[i]; i++);
-        for (; i <= MAX_DB && sum < nb_samples / 1000; i++) {
-            av_log(ctx, AV_LOG_INFO, "histogram_%ddb: %" PRId64 "\n", -i, histdb[i]);
-            sum += histdb[i];
-        }
-    }
+    return ff_filter_frame(ctx->outputs[0], in);
 }
 
 static int config_output(AVFilterLink *outlink)
 {
     AVFilterContext *ctx = outlink->src;
-    VolDetectContext *vd = ctx->priv;
-    size_t histogram_elements;
+    VolDetectContext *s = ctx->priv;
 
-    vd->is_float = outlink->format == AV_SAMPLE_FMT_FLT ||
-                   outlink->format == AV_SAMPLE_FMT_FLTP;
-
-    if (!vd->is_float) {
-        /*
-        * Number of samples at each PCM value.
-        * Only used for integer formats.
-        * For 16 bit signed PCM there are 65536.
-        * histogram[0x8000 + i] is the number of samples at value i.
-        * The extra element is there for symmetry.
-        */
-        histogram_elements = HISTOGRAM_SIZE + 1;
-    } else {
-        /*
-        * The histogram is used to store the number of samples at each dB
-        * instead of the number of samples at each PCM value.
-        */
-        histogram_elements = HISTOGRAM_SIZE_FLT + 1;
+    switch (outlink->format) {
+    case AV_SAMPLE_FMT_FLT:
+        s->update_histogram = update_histogram_flt;
+        s->print_stats = print_stats_flt;
+        break;
+    case AV_SAMPLE_FMT_FLTP:
+        s->update_histogram = update_histogram_fltp;
+        s->print_stats = print_stats_fltp;
+        break;
+    case AV_SAMPLE_FMT_S16:
+        s->update_histogram = update_histogram_s16;
+        s->print_stats = print_stats_s16;
+        break;
+    case AV_SAMPLE_FMT_S16P:
+        s->update_histogram = update_histogram_s16p;
+        s->print_stats = print_stats_s16p;
+        break;
     }
-    vd->histogram = av_calloc(histogram_elements, sizeof(*vd->histogram));
-    if (!vd->histogram)
-        return AVERROR(ENOMEM);
+
     return 0;
 }
 
 static av_cold void uninit(AVFilterContext *ctx)
 {
-    VolDetectContext *vd = ctx->priv;
-    print_stats(ctx);
-    av_freep(&vd->histogram);
+    VolDetectContext *s = ctx->priv;
+
+    if (s->print_stats)
+        s->print_stats(ctx);
 }
 
 static const AVFilterPad volumedetect_inputs[] = {
---
+++ libavfilter/volumedetect_template.c
@@ -0,0 +1,165 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#undef ftype
+#undef stype
+#undef FABS
+#undef SAMPLE_FORMAT
+#if DEPTH == 16
+#define ftype unsigned
+#define stype int16_t
+#define FABS FFABS
+#if PLANAR
+#define SAMPLE_FORMAT s16p
+#else
+#define SAMPLE_FORMAT s16
+#endif
+#elif DEPTH == 32
+#define ftype float
+#define stype float
+#define FABS fabsf
+#if PLANAR
+#define SAMPLE_FORMAT fltp
+#else
+#define SAMPLE_FORMAT flt
+#endif
+#endif
+
+#define MAX_IDX (HISTOGRAM_SIZE-1)
+#define EPSILON (1.f / (1 << 23))
+
+#define fn3(a,b)   a##_##b
+#define fn2(a,b)   fn3(a,b)
+#define fn(a)      fn2(a, SAMPLE_FORMAT)
+
+static inline float fn(get_db)(unsigned x)
+{
+#if DEPTH == 32
+    return x - 16384.f;
+#else
+    return 20.f * log2f(x/(float)MAX_IDX) / log2f(10.f);
+#endif
+}
+
+static inline unsigned fn(get_index)(ftype x)
+{
+#if DEPTH == 32
+    return av_clip(16384+lrintf(ceilf(20.f * log10f(x+EPSILON))), 0, MAX_IDX);
+#else
+    return x;
+#endif
+}
+
+static void fn(print_stats)(AVFilterContext *ctx)
+{
+    VolDetectContext *s = ctx->priv;
+    uint64_t nb_samples = 0, sum = 0, idb_sum = 0;
+    int last_idb = INT_MAX;
+
+    for (int i = 0; i < HISTOGRAM_SIZE; i++)
+        nb_samples += s->histogram[i];
+    if (!nb_samples)
+        return;
+
+    av_log(ctx, AV_LOG_INFO, "n_samples: %" PRId64 "\n", nb_samples);
+#if DEPTH == 16
+    /*
+     * If nb_samples > 1<<34, there is a risk of overflow in the
+     * multiplication or the sum: shift all histogram values to avoid that.
+     * The total number of samples must be recomputed to avoid rounding
+     * errors.
+     */
+    {
+        uint64_t power = 0, nb_samples_shift = 0;
+        int shift = av_log2(nb_samples >> 33);
+
+        for (int i = 0; i < HISTOGRAM_SIZE; i++) {
+            uint64_t count = s->histogram[i];
+
+            if (!count)
+                continue;
+
+            nb_samples_shift += count >> shift;
+            power += i * i * (count >> shift);
+        }
+        if (!nb_samples_shift)
+            return;
+        power = (power + nb_samples_shift / 2) / nb_samples_shift;
+        av_log(ctx, AV_LOG_INFO, "mean_volume: %.1f dB\n", fn(get_db)(sqrt(power)));
+    }
+    for (int i = MAX_IDX; i >= 0; i--) {
+        if (s->histogram[i]) {
+            av_log(ctx, AV_LOG_INFO, "max_volume: %.1f dB\n", fn(get_db)(i));
+            break;
+        }
+    }
+#else
+    av_log(ctx, AV_LOG_INFO, "mean_volume: %.1f dB\n", 20.f * log10f(sqrt(s->sum2/nb_samples)));
+    av_log(ctx, AV_LOG_INFO, "max_volume: %.1f dB\n", 10.f * log10f(s->max));
+#endif
+    for (int i = MAX_IDX; i >= 0; i--) {
+        if (s->histogram[i]) {
+            const float new_db = fn(get_db)(i);
+            const int new_idb = lrintf(ceilf(new_db));
+
+            if (new_idb != last_idb) {
+                if (idb_sum > 0)
+                    av_log(ctx, AV_LOG_INFO, "histogram_%ddb: %" PRId64 "\n", last_idb, idb_sum);
+                idb_sum = 0;
+                last_idb = new_idb;
+                if (sum > nb_samples / 1000)
+                    break;
+            }
+            sum += s->histogram[i];
+            idb_sum += s->histogram[i];
+        }
+    }
+}
+
+static void fn(update_stats)(VolDetectContext *s, stype sample)
+{
+    int idx;
+
+    sample = FABS(sample);
+#if DEPTH == 32
+    s->max = fmaxf(s->max, sample);
+    s->sum2 += sample*sample;
+#endif
+    idx = fn(get_index)(sample);
+    s->histogram[idx]++;
+}
+
+static void fn(update_histogram)(AVFilterContext *ctx, const AVFrame *in)
+{
+    const int nb_channels = in->ch_layout.nb_channels;
+    const int nb_samples = in->nb_samples;
+    VolDetectContext *s = ctx->priv;
+
+#if PLANAR == 1
+    for (int ch = 0; ch < nb_channels; ch++) {
+        const stype *src = (const stype *)in->extended_data[ch];
+        for (int n = 0; n < nb_samples; n++)
+            fn(update_stats)(s, src[n]);
+    }
+#else
+    const stype *src = (const stype *)in->data[0];
+
+    for (int n = 0; n < nb_samples * nb_channels; n++)
+        fn(update_stats)(s, src[n]);
+#endif
+}
