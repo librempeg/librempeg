c83e625e0bb576f900713130035307f93b10a4cb
Niklas Haas <git@haasn.dev>
2024-07-04 12:52:08 +0200

    avfilter/vf_setparams: allow setting chroma location
    Shockingly, there isn't currently _any_ filter for overriding this.
    
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavfilter/avfilter.h
+++ libavfilter/avfilter.h
@@ -595,11 +595,6 @@ struct AVFilterLink {
      * Lists of supported formats / etc. supported by the output filter.
      */
     AVFilterFormatsConfig outcfg;
-
-    /**
-     * Graph the filter belongs to.
-     */
-    struct AVFilterGraph *graph;
 };
 
 /**
