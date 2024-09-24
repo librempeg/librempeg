3ffa713aa8c4c63217c3580e76480dd1c94a0326
Paul B Mahol <onemda@gmail.com>
2024-09-24 17:38:00 +0200

    avfilter/af_stereotools: add float sample format support

--- libavfilter/af_stereotools.c
+++ libavfilter/af_stereotools.c
@@ -53,9 +53,11 @@ typedef struct StereoToolsContext {
     double level_in;
     double level_out;
 
-    double *buffer;
+    void *buffer;
     int length;
     int pos;
+
+    void (*do_filter)(AVFilterContext *ctx, AVFrame *out, AVFrame *in);
 } StereoToolsContext;
 
 #define OFFSET(x) offsetof(StereoToolsContext, x)
@@ -106,6 +108,7 @@ static int query_formats(const AVFilterContext *ctx,
                          AVFilterFormatsConfig **cfg_out)
 {
     static const enum AVSampleFormat formats[] = {
+        AV_SAMPLE_FMT_FLT,
         AV_SAMPLE_FMT_DBL,
         AV_SAMPLE_FMT_NONE,
     };
@@ -113,20 +116,21 @@ static int query_formats(const AVFilterContext *ctx,
         AV_CHANNEL_LAYOUT_STEREO,
         { .nb_channels = 0 },
     };
-
     int ret;
-
 
     ret = ff_set_common_formats_from_list2(ctx, cfg_in, cfg_out, formats);
     if (ret < 0)
         return ret;
 
-    ret = ff_set_common_channel_layouts_from_list2(ctx, cfg_in, cfg_out, layouts);
-    if (ret < 0)
-        return ret;
-
-    return 0;
+    return ff_set_common_channel_layouts_from_list2(ctx, cfg_in, cfg_out, layouts);
 }
+
+#define DEPTH 32
+#include "stereotools_template.c"
+
+#undef DEPTH
+#define DEPTH 64
+#include "stereotools_template.c"
 
 static int config_input(AVFilterLink *inlink)
 {
@@ -135,13 +139,24 @@ static int config_input(AVFilterLink *inlink)
 
     s->length = FFALIGN((inlink->sample_rate + 9) / 10, 2);
     if (!s->buffer)
-        s->buffer = av_calloc(s->length, sizeof(*s->buffer));
+        s->buffer = av_calloc(s->length, av_get_bytes_per_sample(inlink->format));
     if (!s->buffer)
         return AVERROR(ENOMEM);
 
     s->inv_atan_shape = 1.0 / atan(s->sc_level);
-    s->phase_cos_coef = cos(s->phase / 180 * M_PI);
-    s->phase_sin_coef = sin(s->phase / 180 * M_PI);
+    s->phase_cos_coef = cos(s->phase / 180.0 * M_PI);
+    s->phase_sin_coef = sin(s->phase / 180.0 * M_PI);
+
+    switch (inlink->format) {
+    case AV_SAMPLE_FMT_FLT:
+        s->do_filter = do_filter_flt;
+        break;
+    case AV_SAMPLE_FMT_DBL:
+        s->do_filter = do_filter_dbl;
+        break;
+    default:
+        return AVERROR_BUG;
+    }
 
     return 0;
 }
@@ -151,30 +166,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFilterContext *ctx = inlink->dst;
     AVFilterLink *outlink = ctx->outputs[0];
     StereoToolsContext *s = ctx->priv;
-    const double *src = (const double *)in->data[0];
-    const double sb = s->base < 0 ? s->base * 0.5 : s->base;
-    const double sbal = 1 + s->sbal;
-    const double mpan = 1 + s->mpan;
-    const double slev = s->slev;
-    const double mlev = s->mlev;
-    const double balance_in = s->balance_in;
-    const double balance_out = s->balance_out;
-    const double level_in = s->level_in;
-    const double level_out = s->level_out;
-    const double sc_level = s->sc_level;
-    const double delay = s->delay;
-    const int length = s->length;
-    const int mute_l = s->mute_l;
-    const int mute_r = s->mute_r;
-    const int phase_l = s->phase_l;
-    const int phase_r = s->phase_r;
-    double *buffer = s->buffer;
     AVFrame *out;
-    double *dst;
-    int nbuf = inlink->sample_rate * (fabs(delay) / 1000.);
-    int n;
 
-    nbuf -= nbuf % 2;
     if (av_frame_is_writable(in)) {
         out = in;
     } else {
@@ -185,166 +178,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         }
         av_frame_copy_props(out, in);
     }
-    dst = (double *)out->data[0];
 
-    for (n = 0; n < in->nb_samples; n++, src += 2, dst += 2) {
-        double L = src[0], R = src[1], l, r, m, S, gl, gr, gd;
-
-        L *= level_in;
-        R *= level_in;
-
-        gl = 1. - FFMAX(0., balance_in);
-        gr = 1. + FFMIN(0., balance_in);
-        switch (s->bmode_in) {
-        case 1:
-            gd = gl - gr;
-            gl = 1. + gd;
-            gr = 1. - gd;
-            break;
-        case 2:
-            if (balance_in < 0.) {
-                gr = FFMAX(0.5, gr);
-                gl = 1. / gr;
-            } else if (balance_in > 0.) {
-                gl = FFMAX(0.5, gl);
-                gr = 1. / gl;
-            }
-            break;
-        }
-        L *= gl;
-        R *= gr;
-
-        if (s->softclip) {
-            R = s->inv_atan_shape * atan(R * sc_level);
-            L = s->inv_atan_shape * atan(L * sc_level);
-        }
-
-        switch (s->mode) {
-        case 0:
-            m = (L + R) * 0.5;
-            S = (L - R) * 0.5;
-            l = m * mlev * FFMIN(1., 2. - mpan) + S * slev * FFMIN(1., 2. - sbal);
-            r = m * mlev * FFMIN(1., mpan)      - S * slev * FFMIN(1., sbal);
-            L = l;
-            R = r;
-            break;
-        case 1:
-            l = L * FFMIN(1., 2. - sbal);
-            r = R * FFMIN(1., sbal);
-            L = 0.5 * (l + r) * mlev;
-            R = 0.5 * (l - r) * slev;
-            break;
-        case 2:
-            l = L * mlev * FFMIN(1., 2. - mpan) + R * slev * FFMIN(1., 2. - sbal);
-            r = L * mlev * FFMIN(1., mpan)      - R * slev * FFMIN(1., sbal);
-            L = l;
-            R = r;
-            break;
-        case 3:
-            R = L;
-            break;
-        case 4:
-            L = R;
-            break;
-        case 5:
-            L = (L + R) * 0.5;
-            R = L;
-            break;
-        case 6:
-            l = L;
-            L = R;
-            R = l;
-            m = (L + R) * 0.5;
-            S = (L - R) * 0.5;
-            l = m * mlev * FFMIN(1., 2. - mpan) + S * slev * FFMIN(1., 2. - sbal);
-            r = m * mlev * FFMIN(1., mpan)      - S * slev * FFMIN(1., sbal);
-            L = l;
-            R = r;
-            break;
-        case 7:
-            l = L * mlev * FFMIN(1., 2. - mpan) + R * slev * FFMIN(1., 2. - sbal);
-            L = l;
-            R = l;
-            break;
-        case 8:
-            r = L * mlev * FFMIN(1., mpan)      - R * slev * FFMIN(1., sbal);
-            L = r;
-            R = r;
-            break;
-        case 9:
-            l = L * mlev * FFMIN(1., 2. - mpan) + R * slev * FFMIN(1., 2. - sbal);
-            r = L * mlev * FFMIN(1., mpan)      - R * slev * FFMIN(1., sbal);
-            L = r;
-            R = l;
-            break;
-        case 10:
-            L = (L - R) * 0.5;
-            R = L;
-            break;
-        }
-
-        L *= 1. - mute_l;
-        R *= 1. - mute_r;
-
-        L *= (2. * (1. - phase_l)) - 1.;
-        R *= (2. * (1. - phase_r)) - 1.;
-
-        buffer[s->pos  ] = L;
-        buffer[s->pos+1] = R;
-
-        if (delay > 0.) {
-            R = buffer[(s->pos - (int)nbuf + 1 + length) % length];
-        } else if (delay < 0.) {
-            L = buffer[(s->pos - (int)nbuf + length)     % length];
-        }
-
-        l = L + sb * L - sb * R;
-        r = R + sb * R - sb * L;
-
-        L = l;
-        R = r;
-
-        l = L * s->phase_cos_coef - R * s->phase_sin_coef;
-        r = L * s->phase_sin_coef + R * s->phase_cos_coef;
-
-        L = l;
-        R = r;
-
-        s->pos = (s->pos + 2) % s->length;
-
-        gl = 1. - FFMAX(0., balance_out);
-        gr = 1. + FFMIN(0., balance_out);
-        switch (s->bmode_out) {
-        case 1:
-            gd = gl - gr;
-            gl = 1. + gd;
-            gr = 1. - gd;
-            break;
-        case 2:
-            if (balance_out < 0.) {
-                gr = FFMAX(0.5, gr);
-                gl = 1. / gr;
-            } else if (balance_out > 0.) {
-                gl = FFMAX(0.5, gl);
-                gr = 1. / gl;
-            }
-            break;
-        }
-        L *= gl;
-        R *= gr;
-
-
-        L *= level_out;
-        R *= level_out;
-
-        if (ctx->is_disabled) {
-            dst[0] = src[0];
-            dst[1] = src[1];
-        } else {
-            dst[0] = L;
-            dst[1] = R;
-        }
-    }
+    s->do_filter(ctx, out, in);
 
     if (out != in)
         av_frame_free(&in);
---
+++ libavfilter/stereotools_template.c
@@ -0,0 +1,247 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#undef ATAN
+#undef LRINT
+#undef FABS
+#undef FMAX
+#undef FMIN
+#undef ftype
+#undef SAMPLE_FORMAT
+#if DEPTH == 32
+#define ATAN atanf
+#define LRINT lrintf
+#define FABS fabsf
+#define FMIN fminf
+#define FMAX fmaxf
+#define ftype float
+#define SAMPLE_FORMAT flt
+#else
+#define ATAN atan
+#define LRINT lrint
+#define FABS fabs
+#define FMIN fmin
+#define FMAX fmax
+#define ftype double
+#define SAMPLE_FORMAT dbl
+#endif
+
+#define F(x) ((ftype)(x))
+
+#define fn3(a,b)   a##_##b
+#define fn2(a,b)   fn3(a,b)
+#define fn(a)      fn2(a, SAMPLE_FORMAT)
+
+static void fn(do_filter)(AVFilterContext *ctx, AVFrame *out, AVFrame *in)
+{
+    const int is_disabled = ctx->is_disabled;
+    AVFilterLink *inlink = ctx->inputs[0];
+    ftype *dst = (ftype *)out->data[0];
+    StereoToolsContext *s = ctx->priv;
+    const ftype delay = s->delay;
+    int nbuf = LRINT(inlink->sample_rate * (FABS(delay) / F(1000.0)));
+    const ftype *src = (const ftype *)in->data[0];
+    const ftype phase_sin_coef = s->phase_sin_coef;
+    const ftype phase_cos_coef = s->phase_cos_coef;
+    const ftype sb = s->base < F(0.0) ? s->base * F(0.5) : s->base;
+    const ftype inv_atan_shape = s->inv_atan_shape;
+    const ftype sbal = F(1.0) + s->sbal;
+    const ftype mpan = F(1.0) + s->mpan;
+    const ftype slev = s->slev;
+    const ftype mlev = s->mlev;
+    const ftype balance_in = s->balance_in;
+    const ftype balance_out = s->balance_out;
+    const int nb_samples = in->nb_samples;
+    const ftype level_in = s->level_in;
+    const ftype level_out = s->level_out;
+    const ftype sc_level = s->sc_level;
+    const int bmode_out = s->bmode_out;
+    const int bmode_in = s->bmode_in;
+    const int softclip = s->softclip;
+    const int mode = s->mode;
+    const int phase_l = s->phase_l;
+    const int phase_r = s->phase_r;
+    const int length = s->length;
+    const int mute_l = s->mute_l;
+    const int mute_r = s->mute_r;
+    ftype *buffer = s->buffer;
+    int pos = s->pos;
+
+    nbuf -= nbuf & 1;
+    for (int n = 0; n < nb_samples; n++, src += 2, dst += 2) {
+        ftype L = src[0], R = src[1], l, r, m, S, gl, gr, gd;
+
+        L *= level_in;
+        R *= level_in;
+
+        gl = F(1.0) - FMAX(F(0.0), balance_in);
+        gr = F(1.0) + FMIN(F(0.0), balance_in);
+
+        switch (bmode_in) {
+        case 1:
+            gd = gl - gr;
+            gl = F(1.0) + gd;
+            gr = F(1.0) - gd;
+            break;
+        case 2:
+            if (balance_in < F(0.0)) {
+                gr = FMAX(F(0.5), gr);
+                gl = F(1.0) / gr;
+            } else if (balance_in > F(0.0)) {
+                gl = FMAX(F(0.5), gl);
+                gr = F(1.0) / gl;
+            }
+            break;
+        }
+        L *= gl;
+        R *= gr;
+
+        if (softclip) {
+            R = inv_atan_shape * ATAN(R * sc_level);
+            L = inv_atan_shape * ATAN(L * sc_level);
+        }
+
+        switch (mode) {
+        case 0:
+            m = (L + R) * F(0.5);
+            S = (L - R) * F(0.5);
+            l = m * mlev * FMIN(F(1.0), F(2.0) - mpan) + S * slev * FMIN(F(1.0), F(2.0) - sbal);
+            r = m * mlev * FMIN(F(1.0),          mpan) - S * slev * FMIN(F(1.0),          sbal);
+            L = l;
+            R = r;
+            break;
+        case 1:
+            l = L * FMIN(F(1.0), F(2.0) - sbal);
+            r = R * FMIN(F(1.0),          sbal);
+            L = F(0.5) * (l + r) * mlev;
+            R = F(0.5) * (l - r) * slev;
+            break;
+        case 2:
+            l = L * mlev * FMIN(F(1.0), F(2.0) - mpan) + R * slev * FMIN(F(1.0), F(2.0) - sbal);
+            r = L * mlev * FMIN(F(1.0),          mpan) - R * slev * FMIN(F(1.0),          sbal);
+            L = l;
+            R = r;
+            break;
+        case 3:
+            R = L;
+            break;
+        case 4:
+            L = R;
+            break;
+        case 5:
+            L = (L + R) * F(0.5);
+            R = L;
+            break;
+        case 6:
+            l = L;
+            L = R;
+            R = l;
+            m = (L + R) * F(0.5);
+            S = (L - R) * F(0.5);
+            l = m * mlev * FMIN(F(1.0), F(2.0) - mpan) + S * slev * FMIN(F(1.0), F(2.0) - sbal);
+            r = m * mlev * FMIN(F(1.0),          mpan) - S * slev * FMIN(F(1.0),          sbal);
+            L = l;
+            R = r;
+            break;
+        case 7:
+            l = L * mlev * FMIN(F(1.0), F(2.0) - mpan) + R * slev * FMIN(F(1.0), F(2.0) - sbal);
+            L = l;
+            R = l;
+            break;
+        case 8:
+            r = L * mlev * FMIN(F(1.0),          mpan) - R * slev * FMIN(F(1.0),          sbal);
+            L = r;
+            R = r;
+            break;
+        case 9:
+            l = L * mlev * FMIN(F(1.0), F(2.0) - mpan) + R * slev * FMIN(F(1.0), F(2.0) - sbal);
+            r = L * mlev * FMIN(F(1.0),          mpan) - R * slev * FMIN(F(1.0),          sbal);
+            L = r;
+            R = l;
+            break;
+        case 10:
+            L = (L - R) * F(0.5);
+            R = L;
+            break;
+        }
+
+        L *= F(1.0) - mute_l;
+        R *= F(1.0) - mute_r;
+
+        L *= (F(2.0) * (F(1.0) - phase_l)) - F(1.0);
+        R *= (F(2.0) * (F(1.0) - phase_r)) - F(1.0);
+
+        buffer[pos  ] = L;
+        buffer[pos+1] = R;
+
+        if (delay > F(0.0)) {
+            R = buffer[(pos - nbuf + 1 + length) % length];
+        } else if (delay < F(0.0)) {
+            L = buffer[(pos - nbuf + length)     % length];
+        }
+
+        l = L + sb * L - sb * R;
+        r = R + sb * R - sb * L;
+
+        L = l;
+        R = r;
+
+        l = L * phase_cos_coef - R * phase_sin_coef;
+        r = L * phase_sin_coef + R * phase_cos_coef;
+
+        L = l;
+        R = r;
+
+        pos = (pos + 2) % length;
+
+        gl = F(1.0) - FMAX(F(0.0), balance_out);
+        gr = F(1.0) + FMIN(F(0.0), balance_out);
+        switch (bmode_out) {
+        case 1:
+            gd = gl - gr;
+            gl = F(1.0) + gd;
+            gr = F(1.0) - gd;
+            break;
+        case 2:
+            if (balance_out < F(0.0)) {
+                gr = FMAX(F(0.5), gr);
+                gl = F(1.0) / gr;
+            } else if (balance_out > F(0.0)) {
+                gl = FMAX(F(0.5), gl);
+                gr = F(1.0) / gl;
+            }
+            break;
+        }
+
+        L *= gl;
+        R *= gr;
+
+        L *= level_out;
+        R *= level_out;
+
+        if (is_disabled) {
+            dst[0] = src[0];
+            dst[1] = src[1];
+        } else {
+            dst[0] = L;
+            dst[1] = R;
+        }
+    }
+
+    s->pos = pos;
+}
