dwbuiten/FFmpeg

avcodec/hevc/refs: export Stereo 3D side data
Use the 3D Reference Displays Info SEI message to link a view_id with
an eye.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/hevc/refs.c
@@ -22,6 +22,7 @@
  */
 
 #include "libavutil/mem.h"
+#include "libavutil/stereo3d.h"
 
 #include "container_fifo.h"
 #include "decode.h"
@@ -172,12 +173,30 @@ int ff_hevc_set_new_ref(HEVCContext *s, HEVCLayerContext *l, int poc)
 
     // add view ID side data if it's nontrivial
     if (vps->nb_layers > 1 || view_id) {
+        HEVCSEITDRDI *tdrdi = &s->sei.tdrdi;
         AVFrameSideData *sd = av_frame_side_data_new(&ref->f->side_data, &ref->f->nb_side_data,
                                                      AV_FRAME_DATA_VIEW_ID, sizeof(int),
                                                      AV_FRAME_SIDE_DATA_FLAG_REPLACE);
         if (!sd)
             return AVERROR(ENOMEM);
         *(int*)sd->data = view_id;
+
+        if (tdrdi->num_ref_displays) {
+            AVStereo3D *stereo_3d;
+
+            av_frame_remove_side_data(ref->f, AV_FRAME_DATA_STEREO3D);
+            stereo_3d = av_stereo3d_create_side_data(ref->f);
+            if (!stereo_3d)
+                return AVERROR(ENOMEM);
+
+            stereo_3d->type = AV_STEREO3D_FRAMESEQUENCE;
+            if (tdrdi->left_view_id[0] == view_id)
+                stereo_3d->view = AV_STEREO3D_VIEW_LEFT;
+            else if (tdrdi->right_view_id[0] == view_id)
+                stereo_3d->view = AV_STEREO3D_VIEW_RIGHT;
+            else
+                stereo_3d->view = AV_STEREO3D_VIEW_UNSPEC;
+        }
     }
 
     if (!(s->layers_active_output & (1 << s->cur_layer)))

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavcodec/hevc/refs.c
@@ -22,6 +22,7 @@
  */
 
 #include "libavutil/mem.h"
+#include "libavutil/stereo3d.h"
 
 #include "container_fifo.h"
 #include "decode.h"
@@ -90,6 +91,7 @@ static HEVCFrame *alloc_frame(HEVCContext *s, HEVCLayerContext *l)
 
         // add view ID side data if it's nontrivial
         if (vps->nb_layers > 1 || view_id) {
+            HEVCSEITDRDI *tdrdi = &s->sei.tdrdi;
             AVFrameSideData *sd = av_frame_side_data_new(&frame->f->side_data,
                                                          &frame->f->nb_side_data,
                                                          AV_FRAME_DATA_VIEW_ID,
@@ -97,6 +99,22 @@ static HEVCFrame *alloc_frame(HEVCContext *s, HEVCLayerContext *l)
             if (!sd)
                 goto fail;
             *(int*)sd->data = view_id;
+
+            if (tdrdi->num_ref_displays) {
+                AVStereo3D *stereo_3d;
+
+                stereo_3d = av_stereo3d_create_side_data(frame->f);
+                if (!stereo_3d)
+                    goto fail;
+
+                stereo_3d->type = AV_STEREO3D_FRAMESEQUENCE;
+                if (tdrdi->left_view_id[0] == view_id)
+                    stereo_3d->view = AV_STEREO3D_VIEW_LEFT;
+                else if (tdrdi->right_view_id[0] == view_id)
+                    stereo_3d->view = AV_STEREO3D_VIEW_RIGHT;
+                else
+                    stereo_3d->view = AV_STEREO3D_VIEW_UNSPEC;
+            }
         }
 
         ret = ff_progress_frame_get_buffer(s->avctx, &frame->tf,