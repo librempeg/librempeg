cb23d5cbbb1a78f1319d71d4e825e0518693cf5e
Peter Ross <pross@xvid.org>
2024-10-01 12:59:30 +1000

    avcodec/rv60: RealVideo 6.0 decoder
    Reviewed-by: Anton Khirnov <anton@khirnov.net>
    Reviewed-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
    Signed-off-by: Peter Ross <pross@xvid.org>

--- libavformat/riff.c
+++ libavformat/riff.c
@@ -515,6 +515,7 @@ const AVCodecTag ff_codec_bmp_tags[] = {
     { AV_CODEC_ID_LEAD,         MKTAG('L', 'E', 'A', 'D') },
     { AV_CODEC_ID_RFBW,         MKTAG('R', 'F', 'B', 'W') },
     { AV_CODEC_ID_EVC,          MKTAG('e', 'v', 'c', '1') },
+    { AV_CODEC_ID_RV60,         MKTAG('R', 'V', '6', '0') },
     { AV_CODEC_ID_NONE,         0 }
 };
 
