b1548d7b7e9c20c1950d20749bb68ac6bc418fdb
Paul B Mahol <onemda@gmail.com>
2024-10-02 23:12:11 +0200

    avfilter/af_acentercut: remove dead store

--- libavfilter/af_acentercut.c
+++ libavfilter/af_acentercut.c
@@ -215,7 +215,7 @@ static int activate(AVFilterContext *ctx)
     AVFilterLink *outlink = ctx->outputs[0];
     AudioCenterCutContext *s = ctx->priv;
     AVFrame *in = NULL;
-    int ret = 0, status;
+    int ret, status;
     int64_t pts;
 
     FF_FILTER_FORWARD_STATUS_BACK(outlink, inlink);
