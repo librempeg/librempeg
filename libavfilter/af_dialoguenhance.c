54f37c32c02f651a870afff9c328bc5878b8a9c6
Paul B Mahol <onemda@gmail.com>
2024-10-02 22:36:30 +0200

    avfilter/af_dialoguenhance: do trim/flush for start/end samples

--- libavfilter/af_dialoguenhance.c
+++ libavfilter/af_dialoguenhance.c
@@ -37,6 +37,10 @@ typedef struct AudioDialogueEnhancementContext {
     int fft_size;
     int overlap;
 
+    int trim_size;
+    int flush_size;
+    int64_t last_pts;
+
     void *window;
     float prev_vad_float;
     double prev_vad_double;
@@ -108,6 +112,8 @@ static int config_input(AVFilterLink *inlink)
 
     s->fft_size = 1 << av_ceil_log2((inlink->sample_rate + 19) / 20);
     s->overlap = (s->fft_size + 3) / 4;
+    s->trim_size = s->fft_size;
+    s->flush_size = s->fft_size - s->overlap;
 
     s->in_frame       = ff_get_audio_buffer(inlink, (s->fft_size + 2) * 2);
     s->center_frame   = ff_get_audio_buffer(inlink, (s->fft_size + 2) * 2);
@@ -154,11 +160,70 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     av_frame_copy_props(out, in);
     out->nb_samples = in->nb_samples;
     out->pts -= av_rescale_q(s->fft_size - s->overlap, av_make_q(1, outlink->sample_rate), outlink->time_base);
+    out->duration = av_rescale_q(out->nb_samples,
+                                 (AVRational){1, outlink->sample_rate},
+                                 outlink->time_base);
+
+    s->last_pts = out->pts + out->duration;
+
+    if (s->trim_size > 0) {
+        s->trim_size -= in->nb_samples;
+
+        if (s->trim_size > 0 && s->trim_size < in->nb_samples) {
+            for (int ch = 0; ch < out->ch_layout.nb_channels; ch++)
+                out->extended_data[ch] += s->trim_size * av_get_bytes_per_sample(out->format);
+
+            s->trim_size = 0;
+        }
+    }
+
+    if (s->trim_size > 0) {
+        ff_inlink_request_frame(inlink);
+        av_frame_free(&out);
+        ret = 0;
+
+        goto fail;
+    }
+
     ret = ff_filter_frame(outlink, out);
 fail:
     av_frame_free(&in);
     s->in = NULL;
     return ret < 0 ? ret : 0;
+}
+
+static int flush_frame(AVFilterLink *outlink)
+{
+    AVFilterContext *ctx = outlink->src;
+    AudioDialogueEnhanceContext *s = ctx->priv;
+    const int nb_samples = s->flush_size;
+    AVFrame *out = ff_get_audio_buffer(outlink, nb_samples);
+
+    if (!out)
+        return AVERROR(ENOMEM);
+
+    s->flush_size = 0;
+
+    {
+        const uint8_t *left_in = (const uint8_t *)s->in_frame->extended_data[0];
+        const uint8_t *right_in = (const uint8_t *)s->in_frame->extended_data[1];
+        const uint8_t *center_in = (const uint8_t *)s->out_dist_frame->extended_data[0];
+        const size_t sample_size = av_get_bytes_per_sample(out->format);
+        uint8_t *center_dst = ((uint8_t *)out->extended_data[2]);
+        uint8_t *right_dst = ((uint8_t *)out->extended_data[1]);
+        uint8_t *left_dst = ((uint8_t *)out->extended_data[0]);
+
+        memcpy(center_dst, center_in, nb_samples * sample_size);
+        memcpy(right_dst, right_in, nb_samples * sample_size);
+        memcpy(left_dst, left_in, nb_samples * sample_size);
+    }
+
+    out->pts = s->last_pts;
+    out->duration = av_rescale_q(out->nb_samples,
+                                 (AVRational){1, outlink->sample_rate},
+                                 outlink->time_base);
+
+    return ff_filter_frame(outlink, out);
 }
 
 static int activate(AVFilterContext *ctx)
@@ -181,8 +246,11 @@ static int activate(AVFilterContext *ctx)
     if (ret > 0) {
         return filter_frame(inlink, in);
     } else if (ff_inlink_acknowledge_status(inlink, &status, &pts)) {
+        if (s->flush_size > 0)
+            ret = flush_frame(outlink);
+
         ff_outlink_set_status(outlink, status, pts);
-        return 0;
+        return ret;
     } else {
         if (ff_inlink_queued_samples(inlink) >= s->overlap) {
             ff_filter_set_ready(ctx, 10);
