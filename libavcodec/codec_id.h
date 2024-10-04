c8df221d4d9fac14f8ca4c20504a6c4196be4541
Martin Schitter <ms+git@mur.at>
2024-10-04 23:07:30 +0200

    libavcodec/: Add ID and desc for DNxUncompressed
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -325,6 +325,7 @@ enum AVCodecID {
     AV_CODEC_ID_RFBW,
     AV_CODEC_ID_HVQM4,
     AV_CODEC_ID_MCRAW,
+    AV_CODEC_ID_DNXUC,
 
     /* various PCM "codecs" */
     AV_CODEC_ID_FIRST_AUDIO = 0x10000,     ///< A dummy id pointing at the start of audio codecs
