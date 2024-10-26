haasn/FFmpeg

swscale: rename SwsContext to SwsInternal
And preserve the public SwsContext as separate name. The motivation here
is that I want to turn SwsContext into a public struct, while keeping the
internal implementation hidden. Additionally, I also want to be able to
use multiple internal implementations, e.g. for GPU devices.

This commit does not include any functional changes. For the most part, it is
a simple rename. The only complications arise from the public facing API
functions, which preserve their current type (and hence require an additional
unwrapping step internally), and the checkasm test framework, which directly
accesses SwsInternal.

For consistency, the affected functions that need to maintain a distionction
have generally been changed to refer to the SwsContext as *sws, and the
SwsInternal as *c.

In an upcoming commit, I will provide a backing definition for the public
SwsContext, and update `sws_internal()` to dereference the internal struct
instead of merely casting it.

Sponsored-by: Sovereign Tech Fund
Signed-off-by: Niklas Haas <git@haasn.dev>

libswscale/tests/swscale.c
@@ -117,7 +117,7 @@ static int doTest(const uint8_t * const ref[4], int refStride[4], int w, int h,
     int dstStride[4] = {0};
     int i;
     uint64_t ssdY, ssdU = 0, ssdV = 0, ssdA = 0;
-    struct SwsContext *dstContext = NULL, *outContext = NULL;
+    SwsInternal *dstContext = NULL, *outContext = NULL;
     uint32_t crc = 0;
     int res      = 0;
 
@@ -125,7 +125,7 @@ static int doTest(const uint8_t * const ref[4], int refStride[4], int w, int h,
         return 0;
 
     if (cur_srcFormat != srcFormat || cur_srcW != srcW || cur_srcH != srcH) {
-        struct SwsContext *srcContext = NULL;
+        SwsInternal *srcContext = NULL;
         int p;
 
         for (p = 0; p < 4; p++)
@@ -427,7 +427,7 @@ int main(int argc, char **argv)
     const uint8_t * const src[4] = { data, data + W * H, data + W * H * 2, data + W * H * 3 };
     int stride[4]       = { W, W, W, W };
     int x, y;
-    struct SwsContext *sws;
+    SwsInternal *sws;
     AVLFG rand;
     int res = -1;
     int i;

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libswscale/tests/swscale.c
@@ -117,7 +117,7 @@ static int doTest(const uint8_t * const ref[4], int refStride[4], int w, int h,
     int dstStride[4] = {0};
     int i;
     uint64_t ssdY, ssdU = 0, ssdV = 0, ssdA = 0;
-    struct SwsContext *dstContext = NULL, *outContext = NULL;
+    SwsContext *dstContext = NULL, *outContext = NULL;
     uint32_t crc = 0;
     int res      = 0;
 
@@ -125,7 +125,7 @@ static int doTest(const uint8_t * const ref[4], int refStride[4], int w, int h,
         return 0;
 
     if (cur_srcFormat != srcFormat || cur_srcW != srcW || cur_srcH != srcH) {
-        struct SwsContext *srcContext = NULL;
+        SwsContext *srcContext = NULL;
         int p;
 
         for (p = 0; p < 4; p++)
@@ -427,7 +427,7 @@ int main(int argc, char **argv)
     const uint8_t * const src[4] = { data, data + W * H, data + W * H * 2, data + W * H * 3 };
     int stride[4]       = { W, W, W, W };
     int x, y;
-    struct SwsContext *sws;
+    SwsContext *sws;
     AVLFG rand;
     int res = -1;
     int i;