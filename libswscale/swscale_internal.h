ramiropolla/ffmpeg

swscale/range_convert: call arch-specific init functions from main init function
This commit also fixes the issue that the call to ff_sws_init_range_convert()
from sws_init_swscale() was not setting up the arch-specific optimizations.

libswscale/swscale_internal.h
@@ -697,6 +697,7 @@ void ff_updateMMXDitherTables(SwsContext *c, int dstY);
 av_cold void ff_sws_init_range_convert(SwsContext *c);
 av_cold void ff_sws_init_range_convert_aarch64(SwsContext *c);
 av_cold void ff_sws_init_range_convert_loongarch(SwsContext *c);
+av_cold void ff_sws_init_range_convert_riscv(SwsContext *c);
 av_cold void ff_sws_init_range_convert_x86(SwsContext *c);
 
 SwsFunc ff_yuv2rgb_init_x86(SwsContext *c);

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libswscale/swscale_internal.h
@@ -718,8 +718,10 @@ void ff_update_palette(SwsInternal *c, const uint32_t *pal);
  
 av_cold void ff_sws_init_range_convert(SwsInternal *c);
 av_cold void ff_sws_init_range_convert_aarch64(SwsInternal *c);
+av_cold void ff_sws_init_range_convert_loongarch(SwsInternal *c);
+av_cold void ff_sws_init_range_convert_riscv(SwsInternal *c);
 av_cold void ff_sws_init_range_convert_x86(SwsInternal *c);
- 
+  
 SwsFunc ff_yuv2rgb_init_x86(SwsInternal *c);
 SwsFunc ff_yuv2rgb_init_ppc(SwsInternal *c);
 