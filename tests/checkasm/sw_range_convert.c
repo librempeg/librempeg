ramiropolla/ffmpeg

checkasm/sw_range_convert: use YUV pixel formats instead of YUVJ
We are already setting the range, so we can use regular YUV pixel
formats instead of YUVJ.

tests/checkasm/sw_range_convert.c
@@ -45,8 +45,8 @@ static void check_lumConvertRange(int from)
     if (sws_init_context(ctx, NULL, NULL) < 0)
         fail();
 
-    ctx->srcFormat = from ? AV_PIX_FMT_YUVJ444P : AV_PIX_FMT_YUV444P;
-    ctx->dstFormat = from ? AV_PIX_FMT_YUV444P : AV_PIX_FMT_YUVJ444P;
+    ctx->srcFormat = AV_PIX_FMT_YUV444P;
+    ctx->dstFormat = AV_PIX_FMT_YUV444P;
     ctx->srcRange = from;
     ctx->dstRange = !from;
 
@@ -91,8 +91,8 @@ static void check_chrConvertRange(int from)
     if (sws_init_context(ctx, NULL, NULL) < 0)
         fail();
 
-    ctx->srcFormat = from ? AV_PIX_FMT_YUVJ444P : AV_PIX_FMT_YUV444P;
-    ctx->dstFormat = from ? AV_PIX_FMT_YUV444P : AV_PIX_FMT_YUVJ444P;
+    ctx->srcFormat = AV_PIX_FMT_YUV444P;
+    ctx->dstFormat = AV_PIX_FMT_YUV444P;
     ctx->srcRange = from;
     ctx->dstRange = !from;
 

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

tests/checkasm/sw_range_convert.c
@@ -46,8 +46,8 @@ static void check_lumConvertRange(int from)
         fail();
 
     c = sws_internal(sws);
-    c->srcFormat = from ? AV_PIX_FMT_YUVJ444P : AV_PIX_FMT_YUV444P;
-    c->dstFormat = from ? AV_PIX_FMT_YUV444P : AV_PIX_FMT_YUVJ444P;
+    c->srcFormat = AV_PIX_FMT_YUV444P;
+    c->dstFormat = AV_PIX_FMT_YUV444P;
     c->srcRange = from;
     c->dstRange = !from;
 
@@ -92,8 +92,8 @@ static void check_chrConvertRange(int from)
         fail();
 
     c = sws_internal(sws);
-    c->srcFormat = from ? AV_PIX_FMT_YUVJ444P : AV_PIX_FMT_YUV444P;
-    c->dstFormat = from ? AV_PIX_FMT_YUV444P : AV_PIX_FMT_YUVJ444P;
+    c->srcFormat = AV_PIX_FMT_YUV444P;
+    c->dstFormat = AV_PIX_FMT_YUV444P;
     c->srcRange = from;
     c->dstRange = !from;
 