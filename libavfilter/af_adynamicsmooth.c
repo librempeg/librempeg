a776091b788f073b114a733d249e3c7598812bda
Paul B Mahol <onemda@gmail.com>
2024-11-30 10:16:54 +0000

    avfilter/af_adynamicsmooth: add slice threading support

--- libavfilter/af_adynamicsmooth.c
+++ libavfilter/af_adynamicsmooth.c
@@ -43,13 +43,57 @@ static int config_input(AVFilterLink *inlink)
     return 0;
 }
 
+typedef struct ThreadData {
+    AVFrame *out, *in;
+} ThreadData;
+
+static int filter_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+{
+    AudioDynamicSmoothContext *s = ctx->priv;
+    ThreadData *td = arg;
+    AVFrame *out = td->out;
+    AVFrame *in = td->in;
+    const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
+    const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int is_disabled = ctx->is_disabled;
+    const double sensitivity = s->sensitivity;
+    const double wc = s->basefreq / in->sample_rate;
+    const int nb_samples = out->nb_samples;
+
+    for (int ch = start; ch < end; ch++) {
+        const double *src = (const double *)in->extended_data[ch];
+        double *dst = (double *)out->extended_data[ch];
+        double *coeffs = (double *)s->coeffs->extended_data[ch];
+        double low1 = coeffs[0];
+        double low2 = coeffs[1];
+        double inz  = coeffs[2];
+
+        for (int n = 0; n < nb_samples; n++) {
+            double low1z = low1;
+            double low2z = low2;
+            double bandz = low2z - low1z;
+            double wd = wc + sensitivity * fabs(bandz);
+            double g = fmin(1., wd * (5.9948827 + wd * (-11.969296 + wd * 15.959062)));
+
+            low1 = low1z + g * (0.5 * (src[n] + inz)   - low1z);
+            low2 = low2z + g * (0.5 * (low1   + low1z) - low2z);
+            inz = src[n];
+            dst[n] = is_disabled ? src[n] : low2;
+        }
+
+        coeffs[0] = low1;
+        coeffs[1] = low2;
+        coeffs[2] = inz;
+    }
+
+    return 0;
+}
+
 static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 {
     AVFilterContext *ctx = inlink->dst;
     AVFilterLink *outlink = ctx->outputs[0];
-    AudioDynamicSmoothContext *s = ctx->priv;
-    const double sensitivity = s->sensitivity;
-    const double wc = s->basefreq / in->sample_rate;
+    ThreadData td;
     AVFrame *out;
 
     if (av_frame_is_writable(in)) {
@@ -63,31 +107,10 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         av_frame_copy_props(out, in);
     }
 
-    for (int ch = 0; ch < out->ch_layout.nb_channels; ch++) {
-        const double *src = (const double *)in->extended_data[ch];
-        double *dst = (double *)out->extended_data[ch];
-        double *coeffs = (double *)s->coeffs->extended_data[ch];
-        double low1 = coeffs[0];
-        double low2 = coeffs[1];
-        double inz  = coeffs[2];
-
-        for (int n = 0; n < out->nb_samples; n++) {
-            double low1z = low1;
-            double low2z = low2;
-            double bandz = low2z - low1z;
-            double wd = wc + sensitivity * fabs(bandz);
-            double g = fmin(1., wd * (5.9948827 + wd * (-11.969296 + wd * 15.959062)));
-
-            low1 = low1z + g * (0.5 * (src[n] + inz)   - low1z);
-            low2 = low2z + g * (0.5 * (low1   + low1z) - low2z);
-            inz = src[n];
-            dst[n] = ctx->is_disabled ? src[n] : low2;
-        }
-
-        coeffs[0] = low1;
-        coeffs[1] = low2;
-        coeffs[2] = inz;
-    }
+    td.in = in;
+    td.out = out;
+    ff_filter_execute(ctx, filter_channels, &td, NULL,
+                      FFMIN(outlink->ch_layout.nb_channels, ff_filter_get_nb_threads(ctx)));
 
     if (out != in)
         av_frame_free(&in);
@@ -130,6 +153,7 @@ const AVFilter ff_af_adynamicsmooth = {
     FILTER_INPUTS(inputs),
     FILTER_OUTPUTS(ff_audio_default_filterpad),
     FILTER_SINGLE_SAMPLEFMT(AV_SAMPLE_FMT_DBLP),
-    .flags           = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL,
+    .flags           = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL |
+                       AVFILTER_FLAG_SLICE_THREADS,
     .process_command = ff_filter_process_command,
 };
