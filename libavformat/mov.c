43b208da4d8c46cffefd96c2a2e5341b895e8278
Michael Niedermayer <michael@niedermayer.cc>
2024-12-11 23:26:02 +0100

    avformat/mov: dereference pointer after null check
    Fixes: null pointer dereference
    Fixes: 383397479/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-4776829338058752
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -10246,10 +10246,11 @@ static int mov_parse_tiles(AVFormatContext *s)
 
             for (k = 0; k < mov->nb_heif_item; k++) {
                 HEIFItem *item = &mov->heif_item[k];
-                AVStream *st = item->st;
+                AVStream *st;
 
-                if (item->item_id != tile_id)
+                if (!item || item->item_id != tile_id)
                     continue;
+                st = item->st;
                 if (!st) {
                     av_log(s, AV_LOG_WARNING, "HEIF item id %d from grid id %d doesn't "
                                               "reference a stream\n",
