4084b796bb9373dde465cb016ab046ffb1aa85cf
Paul B Mahol <onemda@gmail.com>
2024-12-19 21:08:29 +0000

    avcodec/pdvdec: use inflate decoder

--- libavcodec/pdvdec.c
+++ libavcodec/pdvdec.c
@@ -23,13 +23,11 @@
 #include "avcodec.h"
 #include "codec_internal.h"
 #include "decode.h"
-#include "zlib_wrapper.h"
-
-#include <zlib.h>
+#include "inflate.h"
 
 typedef struct PDVContext {
     AVFrame  *previous_frame;
-    FFZStream zstream;
+    InflateContext ic;
 } PDVContext;
 
 static av_cold int decode_init(AVCodecContext *avctx)
@@ -42,7 +40,7 @@ static av_cold int decode_init(AVCodecContext *avctx)
     if (!s->previous_frame)
         return AVERROR(ENOMEM);
 
-    return ff_inflate_init(&s->zstream, avctx);
+    return 0;
 }
 
 static av_cold int decode_end(AVCodecContext *avctx)
@@ -50,7 +48,6 @@ static av_cold int decode_end(AVCodecContext *avctx)
     PDVContext *s = avctx->priv_data;
 
     av_frame_free(&s->previous_frame);
-    ff_inflate_end(&s->zstream);
 
     return 0;
 }
@@ -60,46 +57,34 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *frame,
 {
     PDVContext *s = avctx->priv_data;
     AVFrame *prev_frame = s->previous_frame;
-    z_stream *const zstream = &s->zstream.zstream;
+    InflateContext *ic = &s->ic;
     uint8_t *dst, *prev = prev_frame->data[0];
-    int ret, zret;
+    int ret;
 
     if (avctx->skip_frame >= AVDISCARD_ALL ||
         (avctx->skip_frame >= AVDISCARD_NONINTRA &&
          !(avpkt->flags & AV_PKT_FLAG_KEY)))
         return avpkt->size;
 
-    zret = inflateReset(zstream);
-    if (zret != Z_OK) {
-        av_log(avctx, AV_LOG_ERROR, "Could not reset inflate: %d.\n", zret);
-        return AVERROR_INVALIDDATA;
-    }
-
     if ((ret = ff_get_buffer(avctx, frame, AV_GET_BUFFER_FLAG_REF)) < 0)
         return ret;
 
-    zstream->next_in  = avpkt->data;
-    zstream->avail_in = avpkt->size;
-
     dst = frame->data[0];
-    for (int i = 0; i < avctx->height; i++) {
-        zstream->next_out  = dst;
-        zstream->avail_out = (avctx->width + 7) >> 3;
+    ret = ff_inflate(ic, avpkt->data, avpkt->size,
+                     dst, avctx->height,
+                     (avctx->width + 7) >> 3,
+                     frame->linesize[0]);
+    if (ret < 0)
+        return ret;
 
-        zret = inflate(zstream, Z_SYNC_FLUSH);
-        if (zret != Z_OK && zret != Z_STREAM_END) {
-            av_log(avctx, AV_LOG_ERROR,
-                   "Inflate failed with return code: %d.\n", zret);
-            return AVERROR_INVALIDDATA;
-        }
-
-        if (prev && !(avpkt->flags & AV_PKT_FLAG_KEY)) {
+    if (prev && !(avpkt->flags & AV_PKT_FLAG_KEY)) {
+        for (int i = 0; i < avctx->height; i++) {
             for (int j = 0; j < (avctx->width + 7) >> 3; j++)
                 dst[j] ^= prev[j];
+
+            dst += frame->linesize[0];
             prev += prev_frame->linesize[0];
         }
-
-        dst += frame->linesize[0];
     }
 
     if ((ret = av_frame_replace(s->previous_frame, frame)) < 0)
@@ -112,7 +97,7 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *frame,
         frame->pict_type = AV_PICTURE_TYPE_P;
     }
 
-    *got_frame       = 1;
+    *got_frame = 1;
 
     return avpkt->size;
 }
