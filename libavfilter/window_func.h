3f272df68f6f86602a62c17023485f30cd3acda0
Paul B Mahol <onemda@gmail.com>
2025-02-26 18:21:03 +0000

    avfilter/window_func: split options and add double support

--- libavfilter/window_func.h
+++ libavfilter/window_func.h
@@ -18,86 +18,75 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-
 #ifndef AVFILTER_WINDOW_FUNC_H
 #define AVFILTER_WINDOW_FUNC_H
 
 #include <math.h>
 #include "libavutil/avassert.h"
 #include "libavutil/common.h"
+#include "window_func_opt.h"
 
-enum WindowFunc     { WFUNC_RECT, WFUNC_HANNING, WFUNC_HAMMING, WFUNC_BLACKMAN,
-                      WFUNC_BARTLETT, WFUNC_WELCH, WFUNC_FLATTOP,
-                      WFUNC_BHARRIS, WFUNC_BNUTTALL, WFUNC_SINE, WFUNC_NUTTALL,
-                      WFUNC_BHANN, WFUNC_LANCZOS, WFUNC_GAUSS, WFUNC_TUKEY,
-                      WFUNC_DOLPH, WFUNC_CAUCHY, WFUNC_PARZEN, WFUNC_POISSON,
-                      WFUNC_BOHMAN, WFUNC_KAISER,
-                      NB_WFUNC };
+#ifdef DEPTH
 
-#define WIN_FUNC_OPTION(win_func_opt_name, win_func_offset, flag, default_window_func)                              \
-    { win_func_opt_name, "set window function", win_func_offset, AV_OPT_TYPE_INT, {.i64 = default_window_func}, 0, NB_WFUNC-1, flag, .unit = "win_func" }, \
-        { "rect",     "Rectangular",      0, AV_OPT_TYPE_CONST, {.i64=WFUNC_RECT},     0, 0, flag, .unit = "win_func" }, \
-        { "bartlett", "Bartlett",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BARTLETT}, 0, 0, flag, .unit = "win_func" }, \
-        { "hann",     "Hann",             0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HANNING},  0, 0, flag, .unit = "win_func" }, \
-        { "hanning",  "Hanning",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HANNING},  0, 0, flag, .unit = "win_func" }, \
-        { "hamming",  "Hamming",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HAMMING},  0, 0, flag, .unit = "win_func" }, \
-        { "blackman", "Blackman",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BLACKMAN}, 0, 0, flag, .unit = "win_func" }, \
-        { "welch",    "Welch",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_WELCH},    0, 0, flag, .unit = "win_func" }, \
-        { "flattop",  "Flat-top",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_FLATTOP},  0, 0, flag, .unit = "win_func" }, \
-        { "bharris",  "Blackman-Harris",  0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BHARRIS},  0, 0, flag, .unit = "win_func" }, \
-        { "bnuttall", "Blackman-Nuttall", 0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BNUTTALL}, 0, 0, flag, .unit = "win_func" }, \
-        { "bhann",    "Bartlett-Hann",    0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BHANN},    0, 0, flag, .unit = "win_func" }, \
-        { "sine",     "Sine",             0, AV_OPT_TYPE_CONST, {.i64=WFUNC_SINE},     0, 0, flag, .unit = "win_func" }, \
-        { "nuttall",  "Nuttall",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_NUTTALL},  0, 0, flag, .unit = "win_func" }, \
-        { "lanczos",  "Lanczos",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_LANCZOS},  0, 0, flag, .unit = "win_func" }, \
-        { "gauss",    "Gauss",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_GAUSS},    0, 0, flag, .unit = "win_func" }, \
-        { "tukey",    "Tukey",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_TUKEY},    0, 0, flag, .unit = "win_func" }, \
-        { "dolph",    "Dolph-Chebyshev",  0, AV_OPT_TYPE_CONST, {.i64=WFUNC_DOLPH},    0, 0, flag, .unit = "win_func" }, \
-        { "cauchy",   "Cauchy",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_CAUCHY},   0, 0, flag, .unit = "win_func" }, \
-        { "parzen",   "Parzen",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_PARZEN},   0, 0, flag, .unit = "win_func" }, \
-        { "poisson",  "Poisson",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_POISSON},  0, 0, flag, .unit = "win_func" }, \
-        { "bohman",   "Bohman",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BOHMAN},   0, 0, flag, .unit = "win_func" }, \
-        { "kaiser",   "Kaiser",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_KAISER},   0, 0, flag, .unit = "win_func" }
+#undef ftype
+#undef fun
 
+#if DEPTH == 64
+#define ftype double
+#else
+#define ftype float
+#endif
 
-static inline void generate_window_func(float *lut, int N, int win_func,
-                                        float *overlap)
+#define fun3(a,b)  a##_##b
+#define fun2(a,b)  fun3(a,b)
+#define fun(a)     fun2(a, DEPTH)
+
+#else
+
+#undef ftype
+#undef fun
+
+#define fun(a)      (a)
+#define ftype float
+
+#endif
+
+static inline void fun(generate_window_func)(ftype *lut, int N, int win_func,
+                                             ftype *overlap)
 {
-    int n;
-
     switch (win_func) {
     case WFUNC_RECT:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 1.;
         *overlap = 0.;
         break;
     case WFUNC_BARTLETT:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 1.-fabs((n-(N-1)/2.)/((N-1)/2.));
         *overlap = 0.5;
         break;
     case WFUNC_HANNING:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = .5*(1-cos(2*M_PI*n/(N-1)));
         *overlap = 0.5;
         break;
     case WFUNC_HAMMING:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = .54-.46*cos(2*M_PI*n/(N-1));
         *overlap = 0.75;
         break;
     case WFUNC_BLACKMAN:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = .42659-.49656*cos(2*M_PI*n/(N-1))+.076849*cos(4*M_PI*n/(N-1));
         *overlap = 0.661;
         break;
     case WFUNC_WELCH:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 1.-(n-(N-1)/2.)/((N-1)/2.)*(n-(N-1)/2.)/((N-1)/2.);
         *overlap = 0.293;
         break;
     case WFUNC_FLATTOP:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 1.-1.985844164102*cos( 2*M_PI*n/(N-1))+1.791176438506*cos( 4*M_PI*n/(N-1))-
             1.282075284005*cos( 6*M_PI*n/(N-1))+0.667777530266*cos( 8*M_PI*n/(N-1))-
             0.240160796576*cos(10*M_PI*n/(N-1))+0.056656381764*cos(12*M_PI*n/(N-1))-
@@ -106,45 +95,45 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.841;
         break;
     case WFUNC_BHARRIS:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 0.35875-0.48829*cos(2*M_PI*n/(N-1))+0.14128*cos(4*M_PI*n/(N-1))-0.01168*cos(6*M_PI*n/(N-1));
         *overlap = 0.661;
         break;
     case WFUNC_BNUTTALL:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 0.3635819-0.4891775*cos(2*M_PI*n/(N-1))+0.1365995*cos(4*M_PI*n/(N-1))-0.0106411*cos(6*M_PI*n/(N-1));
         *overlap = 0.75;
         break;
     case WFUNC_BHANN:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 0.62-0.48*fabs(n/(double)(N-1)-.5)-0.38*cos(2*M_PI*n/(N-1));
         *overlap = 0.5;
         break;
     case WFUNC_SINE:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = sin(M_PI*n/(N-1));
         *overlap = 0.75;
         break;
     case WFUNC_NUTTALL:
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = 0.355768-0.487396*cos(2*M_PI*n/(N-1))+0.144232*cos(4*M_PI*n/(N-1))-0.012604*cos(6*M_PI*n/(N-1));
         *overlap = 0.75;
         break;
     case WFUNC_LANCZOS:
         #define SINC(x) (!(x)) ? 1 : sin(M_PI * (x))/(M_PI * (x));
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = SINC((2.*n)/(N-1)-1);
         *overlap = 0.75;
         break;
     case WFUNC_GAUSS:
         #define SQR(x) ((x)*(x))
-        for (n = 0; n < N; n++)
+        for (int n = 0; n < N; n++)
             lut[n] = exp(-0.5 * SQR((n-(N-1)/2)/(0.4*(N-1)/2.f)));
         *overlap = 0.75;
         break;
     case WFUNC_TUKEY:
-        for (n = 0; n < N; n++) {
-            float M = (N-1)/2.;
+        for (int n = 0; n < N; n++) {
+            double M = (N-1)/2.;
 
             if (FFABS(n - M) >= 0.3 * M) {
                 lut[n] = 0.5 * (1 + cos((M_PI*(FFABS(n - M) - 0.3 * M))/((1 - 0.3) * M)));
@@ -155,9 +144,9 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.33;
         break;
     case WFUNC_DOLPH: {
-        double b = cosh(acosh(pow(10.0, 5.0)) / (N-1)), sum, t, c, norm = 0;
+        double b = cosh(acosh(pow(10.0, 5.0)) / (N-1)), sum, t, norm = 0;
         int j;
-        for (c = 1 - 1 / (b*b), n = (N-1) / 2; n >= 0; --n) {
+        for (int c = 1 - 1 / (b*b), n = (N-1) / 2; n >= 0; --n) {
             for (sum = !n, b = t = j = 1; j <= n && sum != t; b *= (n-j) * (1./j), ++j)
                 t = sum, sum += (b *= c * (N - n - j) * (1./j));
             sum /= (N - 1 - n), norm = norm ? norm : sum, sum /= norm;
@@ -167,7 +156,7 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.75;}
         break;
     case WFUNC_CAUCHY:
-        for (n = 0; n < N; n++) {
+        for (int n = 0; n < N; n++) {
             double x = 2 * ((n / (double)(N - 1)) - .5);
 
             if (x <= -.5 || x >= .5) {
@@ -179,7 +168,7 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.75;
         break;
     case WFUNC_PARZEN:
-        for (n = 0; n < N; n++) {
+        for (int n = 0; n < N; n++) {
             double x = 2 * ((n / (double)(N - 1)) - .5);
 
             if (x > 0.25 && x <= 0.5) {
@@ -197,7 +186,7 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.75;
         break;
     case WFUNC_POISSON:
-        for (n = 0; n < N; n++) {
+        for (int n = 0; n < N; n++) {
             double x = 2 * ((n / (double)(N - 1)) - .5);
 
             if (x >= 0 && x <= .5) {
@@ -211,7 +200,7 @@ static inline void generate_window_func(float *lut, int N, int win_func,
         *overlap = 0.75;
         break;
     case WFUNC_BOHMAN:
-        for (n = 0; n < N; n++) {
+        for (int n = 0; n < N; n++) {
             double x = 2 * ((n / (double)(N - 1))) - 1.;
 
             lut[n] = (1 - fabs(x)) * cos(M_PI*fabs(x)) + 1./M_PI*sin(M_PI*fabs(x));
@@ -221,7 +210,7 @@ static inline void generate_window_func(float *lut, int N, int win_func,
     case WFUNC_KAISER:
     {
         double scale = 1.0 / av_bessel_i0(12.);
-        for (n = 0; n < N; n++) {
+        for (int n = 0; n < N; n++) {
             double x = 2.0 / (double)(N - 1);
             lut[n] = av_bessel_i0(12. * sqrt(1. - SQR(n * x - 1.))) * scale;
         }
---
+++ libavfilter/window_func_opt.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Paul B Mahol
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVFILTER_WINDOW_FUNC_OPT_H
+#define AVFILTER_WINDOW_FUNC_OPT_H
+
+enum WindowFunc     { WFUNC_RECT, WFUNC_HANNING, WFUNC_HAMMING, WFUNC_BLACKMAN,
+                      WFUNC_BARTLETT, WFUNC_WELCH, WFUNC_FLATTOP,
+                      WFUNC_BHARRIS, WFUNC_BNUTTALL, WFUNC_SINE, WFUNC_NUTTALL,
+                      WFUNC_BHANN, WFUNC_LANCZOS, WFUNC_GAUSS, WFUNC_TUKEY,
+                      WFUNC_DOLPH, WFUNC_CAUCHY, WFUNC_PARZEN, WFUNC_POISSON,
+                      WFUNC_BOHMAN, WFUNC_KAISER,
+                      NB_WFUNC };
+
+#define WIN_FUNC_OPTION(win_func_opt_name, win_func_offset, flag, default_window_func)                              \
+    { win_func_opt_name, "set window function", win_func_offset, AV_OPT_TYPE_INT, {.i64 = default_window_func}, 0, NB_WFUNC-1, flag, .unit = "win_func" }, \
+        { "rect",     "Rectangular",      0, AV_OPT_TYPE_CONST, {.i64=WFUNC_RECT},     0, 0, flag, .unit = "win_func" }, \
+        { "bartlett", "Bartlett",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BARTLETT}, 0, 0, flag, .unit = "win_func" }, \
+        { "hann",     "Hann",             0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HANNING},  0, 0, flag, .unit = "win_func" }, \
+        { "hanning",  "Hanning",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HANNING},  0, 0, flag, .unit = "win_func" }, \
+        { "hamming",  "Hamming",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_HAMMING},  0, 0, flag, .unit = "win_func" }, \
+        { "blackman", "Blackman",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BLACKMAN}, 0, 0, flag, .unit = "win_func" }, \
+        { "welch",    "Welch",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_WELCH},    0, 0, flag, .unit = "win_func" }, \
+        { "flattop",  "Flat-top",         0, AV_OPT_TYPE_CONST, {.i64=WFUNC_FLATTOP},  0, 0, flag, .unit = "win_func" }, \
+        { "bharris",  "Blackman-Harris",  0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BHARRIS},  0, 0, flag, .unit = "win_func" }, \
+        { "bnuttall", "Blackman-Nuttall", 0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BNUTTALL}, 0, 0, flag, .unit = "win_func" }, \
+        { "bhann",    "Bartlett-Hann",    0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BHANN},    0, 0, flag, .unit = "win_func" }, \
+        { "sine",     "Sine",             0, AV_OPT_TYPE_CONST, {.i64=WFUNC_SINE},     0, 0, flag, .unit = "win_func" }, \
+        { "nuttall",  "Nuttall",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_NUTTALL},  0, 0, flag, .unit = "win_func" }, \
+        { "lanczos",  "Lanczos",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_LANCZOS},  0, 0, flag, .unit = "win_func" }, \
+        { "gauss",    "Gauss",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_GAUSS},    0, 0, flag, .unit = "win_func" }, \
+        { "tukey",    "Tukey",            0, AV_OPT_TYPE_CONST, {.i64=WFUNC_TUKEY},    0, 0, flag, .unit = "win_func" }, \
+        { "dolph",    "Dolph-Chebyshev",  0, AV_OPT_TYPE_CONST, {.i64=WFUNC_DOLPH},    0, 0, flag, .unit = "win_func" }, \
+        { "cauchy",   "Cauchy",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_CAUCHY},   0, 0, flag, .unit = "win_func" }, \
+        { "parzen",   "Parzen",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_PARZEN},   0, 0, flag, .unit = "win_func" }, \
+        { "poisson",  "Poisson",          0, AV_OPT_TYPE_CONST, {.i64=WFUNC_POISSON},  0, 0, flag, .unit = "win_func" }, \
+        { "bohman",   "Bohman",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_BOHMAN},   0, 0, flag, .unit = "win_func" }, \
+        { "kaiser",   "Kaiser",           0, AV_OPT_TYPE_CONST, {.i64=WFUNC_KAISER},   0, 0, flag, .unit = "win_func" }
+
+#endif /* AVFILTER_WINDOW_FUNC_OPT_H */
+
