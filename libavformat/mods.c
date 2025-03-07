eec0781d12287cf73375182c2d43f5d6850ae962
Paul B Mahol <onemda@gmail.com>
2025-03-07 14:00:14 +0000

    avformat/mods: set start_time

--- libavformat/mods.c
+++ libavformat/mods.c
@@ -52,6 +52,7 @@ static int mods_read_header(AVFormatContext *s)
 
     avio_skip(pb, 8);
 
+    st->start_time           = 0;
     st->nb_frames            = avio_rl32(pb);
     st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
     st->codecpar->codec_id   = AV_CODEC_ID_MOBICLIP;
