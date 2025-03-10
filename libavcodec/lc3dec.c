710a5fcc3bd9505dcd9a673f020c917a2ec127cb
Paul B Mahol <onemda@gmail.com>
2025-03-10 10:07:05 +0000

    avcodec/lc3dec: use macro to set supported sample formats

--- libavcodec/lc3dec.c
+++ libavcodec/lc3dec.c
@@ -5388,6 +5388,5 @@ const FFCodec ff_lc3_decoder = {
     .close          = decode_close,
     .p.capabilities = AV_CODEC_CAP_DR1 | AV_CODEC_CAP_SLICE_THREADS,
     .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
-    .p.sample_fmts  = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_FLTP,
-                                                      AV_SAMPLE_FMT_NONE },
+    CODEC_SAMPLEFMTS(AV_SAMPLE_FMT_FLTP),
 };
