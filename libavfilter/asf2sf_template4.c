fbe5989e026b1c9170e0eb5b1198f172b65ca6ca
Paul B Mahol <onemda@gmail.com>
2025-03-19 13:41:56 +0000

    avfilter/asf2sf_template4: process full planar differently

--- libavfilter/asf2sf_template4.c
+++ libavfilter/asf2sf_template4.c
@@ -138,86 +138,99 @@ static int fn(sf2sf)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
     AVFrame *restrict in = td->in;
     const int nb_channels = in->ch_layout.nb_channels;
     const int nb_samples = in->nb_samples;
-    const int start = (nb_samples * jobnr) / nb_jobs;
-    const int end = (nb_samples * (jobnr+1)) / nb_jobs;
 
-    if (nb_channels == 1) {
-        const stype *const restrict src = fn_src_ptr(in, 0, 0);
-        dtype *restrict dst = fn_dst_ptr(out, 0, 0);
+    if (SRC_p == 1 && DST_p == 1) {
+        const int start = (nb_channels * jobnr) / nb_jobs;
+        const int end = (nb_channels * (jobnr+1)) / nb_jobs;
 
-        fn(sf2sf_loop)(dst, src, end-start, 1);
-    } else if (nb_channels == 2) {
-        const stype *const restrict src0 = fn_src_ptr(in, 0, start);
-        const stype *const restrict src1 = fn_src_ptr(in, 1, start);
-        dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
-        dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+        for (int ch = start; ch < end; ch++) {
+            const stype *const restrict src = fn_src_ptr(in, ch, 0);
+            dtype *restrict dst = fn_dst_ptr(out, ch, 0);
 
-        fn(sf2sf_loop2)(dst0, dst1, src0, src1, end-start);
-    } else if (nb_channels == 3) {
-        const stype *const restrict src0 = fn_src_ptr(in, 0, start);
-        const stype *const restrict src1 = fn_src_ptr(in, 1, start);
-        const stype *const restrict src2 = fn_src_ptr(in, 2, start);
-        dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
-        dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
-        dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
-
-        fn(sf2sf_loop3)(dst0, dst1, dst2, src0, src1, src2, end-start);
-    } else if (nb_channels == 4) {
-        const stype *const restrict src0 = fn_src_ptr(in, 0, start);
-        const stype *const restrict src1 = fn_src_ptr(in, 1, start);
-        const stype *const restrict src2 = fn_src_ptr(in, 2, start);
-        const stype *const restrict src3 = fn_src_ptr(in, 3, start);
-        dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
-        dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
-        dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
-        dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
-
-        fn(sf2sf_loop4)(dst0, dst1, dst2, dst3,
-                        src0, src1, src2, src3, end-start);
-    } else if (nb_channels == 6) {
-        const stype *const restrict src0 = fn_src_ptr(in, 0, start);
-        const stype *const restrict src1 = fn_src_ptr(in, 1, start);
-        const stype *const restrict src2 = fn_src_ptr(in, 2, start);
-        const stype *const restrict src3 = fn_src_ptr(in, 3, start);
-        const stype *const restrict src4 = fn_src_ptr(in, 4, start);
-        const stype *const restrict src5 = fn_src_ptr(in, 5, start);
-        dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
-        dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
-        dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
-        dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
-        dtype *restrict dst4 = fn_dst_ptr(out, 4, start);
-        dtype *restrict dst5 = fn_dst_ptr(out, 5, start);
-
-        fn(sf2sf_loop6)(dst0, dst1, dst2, dst3, dst4, dst5,
-                        src0, src1, src2, src3, src4, src5,
-                        end-start);
-    } else if (nb_channels == 8) {
-        const stype *const restrict src0 = fn_src_ptr(in, 0, start);
-        const stype *const restrict src1 = fn_src_ptr(in, 1, start);
-        const stype *const restrict src2 = fn_src_ptr(in, 2, start);
-        const stype *const restrict src3 = fn_src_ptr(in, 3, start);
-        const stype *const restrict src4 = fn_src_ptr(in, 4, start);
-        const stype *const restrict src5 = fn_src_ptr(in, 5, start);
-        const stype *const restrict src6 = fn_src_ptr(in, 6, start);
-        const stype *const restrict src7 = fn_src_ptr(in, 7, start);
-        dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
-        dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
-        dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
-        dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
-        dtype *restrict dst4 = fn_dst_ptr(out, 4, start);
-        dtype *restrict dst5 = fn_dst_ptr(out, 5, start);
-        dtype *restrict dst6 = fn_dst_ptr(out, 6, start);
-        dtype *restrict dst7 = fn_dst_ptr(out, 7, start);
-
-        fn(sf2sf_loop8)(dst0, dst1, dst2, dst3, dst4, dst5, dst6, dst7,
-                        src0, src1, src2, src3, src4, src5, src6, src7,
-                        end-start);
+            fn(sf2sf_loop)(dst, src, nb_samples, nb_channels);
+        }
     } else {
-        for (int ch = 0; ch < nb_channels; ch++) {
-            const stype *const restrict src = fn_src_ptr(in, ch, start);
-            dtype *restrict dst = fn_dst_ptr(out, ch, start);
+        const int start = (nb_samples * jobnr) / nb_jobs;
+        const int end = (nb_samples * (jobnr+1)) / nb_jobs;
 
-            fn(sf2sf_loop)(dst, src, end-start, nb_channels);
+        if (nb_channels == 1) {
+            const stype *const restrict src = fn_src_ptr(in, 0, 0);
+            dtype *restrict dst = fn_dst_ptr(out, 0, 0);
+
+            fn(sf2sf_loop)(dst, src, end-start, 1);
+        } else if (nb_channels == 2) {
+            const stype *const restrict src0 = fn_src_ptr(in, 0, start);
+            const stype *const restrict src1 = fn_src_ptr(in, 1, start);
+            dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
+            dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+
+            fn(sf2sf_loop2)(dst0, dst1, src0, src1, end-start);
+        } else if (nb_channels == 3) {
+            const stype *const restrict src0 = fn_src_ptr(in, 0, start);
+            const stype *const restrict src1 = fn_src_ptr(in, 1, start);
+            const stype *const restrict src2 = fn_src_ptr(in, 2, start);
+            dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
+            dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+            dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
+
+            fn(sf2sf_loop3)(dst0, dst1, dst2, src0, src1, src2, end-start);
+        } else if (nb_channels == 4) {
+            const stype *const restrict src0 = fn_src_ptr(in, 0, start);
+            const stype *const restrict src1 = fn_src_ptr(in, 1, start);
+            const stype *const restrict src2 = fn_src_ptr(in, 2, start);
+            const stype *const restrict src3 = fn_src_ptr(in, 3, start);
+            dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
+            dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+            dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
+            dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
+
+            fn(sf2sf_loop4)(dst0, dst1, dst2, dst3,
+                            src0, src1, src2, src3, end-start);
+        } else if (nb_channels == 6) {
+            const stype *const restrict src0 = fn_src_ptr(in, 0, start);
+            const stype *const restrict src1 = fn_src_ptr(in, 1, start);
+            const stype *const restrict src2 = fn_src_ptr(in, 2, start);
+            const stype *const restrict src3 = fn_src_ptr(in, 3, start);
+            const stype *const restrict src4 = fn_src_ptr(in, 4, start);
+            const stype *const restrict src5 = fn_src_ptr(in, 5, start);
+            dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
+            dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+            dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
+            dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
+            dtype *restrict dst4 = fn_dst_ptr(out, 4, start);
+            dtype *restrict dst5 = fn_dst_ptr(out, 5, start);
+
+            fn(sf2sf_loop6)(dst0, dst1, dst2, dst3, dst4, dst5,
+                            src0, src1, src2, src3, src4, src5,
+                            end-start);
+        } else if (nb_channels == 8) {
+            const stype *const restrict src0 = fn_src_ptr(in, 0, start);
+            const stype *const restrict src1 = fn_src_ptr(in, 1, start);
+            const stype *const restrict src2 = fn_src_ptr(in, 2, start);
+            const stype *const restrict src3 = fn_src_ptr(in, 3, start);
+            const stype *const restrict src4 = fn_src_ptr(in, 4, start);
+            const stype *const restrict src5 = fn_src_ptr(in, 5, start);
+            const stype *const restrict src6 = fn_src_ptr(in, 6, start);
+            const stype *const restrict src7 = fn_src_ptr(in, 7, start);
+            dtype *restrict dst0 = fn_dst_ptr(out, 0, start);
+            dtype *restrict dst1 = fn_dst_ptr(out, 1, start);
+            dtype *restrict dst2 = fn_dst_ptr(out, 2, start);
+            dtype *restrict dst3 = fn_dst_ptr(out, 3, start);
+            dtype *restrict dst4 = fn_dst_ptr(out, 4, start);
+            dtype *restrict dst5 = fn_dst_ptr(out, 5, start);
+            dtype *restrict dst6 = fn_dst_ptr(out, 6, start);
+            dtype *restrict dst7 = fn_dst_ptr(out, 7, start);
+
+            fn(sf2sf_loop8)(dst0, dst1, dst2, dst3, dst4, dst5, dst6, dst7,
+                            src0, src1, src2, src3, src4, src5, src6, src7,
+                            end-start);
+        } else {
+            for (int ch = 0; ch < nb_channels; ch++) {
+                const stype *const restrict src = fn_src_ptr(in, ch, start);
+                dtype *restrict dst = fn_dst_ptr(out, ch, start);
+
+                fn(sf2sf_loop)(dst, src, end-start, nb_channels);
+            }
         }
     }
 
