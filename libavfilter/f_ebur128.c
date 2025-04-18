d097389b3d821cb5f488f23b9d6d7f577365dc01
Paul B Mahol <onemda@gmail.com>
2025-04-18 09:32:42 +0000

    avfilter/f_ebur128: simplify FIND_PEAK() macro

--- libavfilter/f_ebur128.c
+++ libavfilter/f_ebur128.c
@@ -780,11 +780,9 @@ static void process_ebur128(EBUR128Context *ebur128, const uint8_t **csamples, c
     }
 
 #define FIND_PEAK(global, sp, ptype) do {                        \
-    int ch;                                                      \
-    double maxpeak;                                              \
-    maxpeak = 0.0;                                               \
+    double maxpeak = 0.0;                                        \
     if (ebur128->peak_mode & PEAK_MODE_ ## ptype ## _PEAKS) {    \
-        for (ch = 0; ch < nb_channels; ch++)                     \
+        for (int ch = 0; ch < nb_channels; ch++)                 \
             maxpeak = FFMAX(maxpeak, sp[ch]);                    \
         global = DBFS(maxpeak);                                  \
     }                                                            \
