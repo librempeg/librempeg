e88081010ae8bd17e434d6d5b152b753caad3cd0
Paul B Mahol <onemda@gmail.com>
2025-04-29 13:22:11 +0000

    avfilter/vf_dblur: add frame threading

--- libavfilter/vf_dblur.c
+++ libavfilter/vf_dblur.c
@@ -201,11 +201,18 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     if (av_frame_is_writable(in)) {
         out = in;
     } else {
-        out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
+        int ret;
+
+        out = av_frame_alloc();
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
         }
+
+        ret = ff_filter_get_buffer(ctx, out);
+        if (ret < 0)
+            return ret;
+
         av_frame_copy_props(out, in);
     }
 
@@ -303,7 +310,8 @@ const FFFilter ff_vf_dblur = {
     .p.name        = "dblur",
     .p.description = NULL_IF_CONFIG_SMALL("Apply Directional Blur filter."),
     .p.priv_class  = &dblur_class,
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC |
+                     AVFILTER_FLAG_FRAME_THREADS,
     .priv_size     = sizeof(DBlurContext),
     .uninit        = uninit,
     FILTER_INPUTS(dblur_inputs),
