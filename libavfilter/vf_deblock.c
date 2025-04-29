d1159af2cca74d733d70b1d42eb9bc5def47948d
Paul B Mahol <onemda@gmail.com>
2025-04-29 20:16:55 +0000

    avfilter/vf_deblock: add frame threading

--- libavfilter/vf_deblock.c
+++ libavfilter/vf_deblock.c
@@ -310,11 +310,21 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     if (av_frame_is_writable(in)) {
         out = in;
     } else {
-        out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
+        int ret;
+
+        out = av_frame_alloc();
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
         }
+
+        ret = ff_filter_get_buffer(ctx, out);
+        if (ret < 0) {
+            av_frame_free(&out);
+            av_frame_free(&in);
+            return ret;
+        }
+
         av_frame_copy_props(out, in);
     }
 
@@ -415,7 +425,8 @@ const FFFilter ff_vf_deblock = {
     .p.name        = "deblock",
     .p.description = NULL_IF_CONFIG_SMALL("Deblock video."),
     .p.priv_class  = &deblock_class,
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC |
+                     AVFILTER_FLAG_FRAME_THREADS,
     .priv_size     = sizeof(DeblockContext),
     FILTER_INPUTS(inputs),
     FILTER_OUTPUTS(outputs),
