fed86ff1f053ffb72636d59319d8b50d63c80a73
Paul B Mahol <onemda@gmail.com>
2025-05-10 17:08:38 +0000

    avfilter: add upmix audio filter
    Using alpha-Sliding Windowed Infinite Fourier Transform.

---
+++ libavfilter/af_upmix.c
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2026 Paul B Mahol
+ *
+ * This file is part of Librempeg
+ *
+ * Librempeg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Librempeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Librempeg; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <float.h>
+
+#include "libavutil/channel_layout.h"
+#include "libavutil/ffmath.h"
+#include "libavutil/mem.h"
+#include "libavutil/opt.h"
+
+#include "audio.h"
+#include "avfilter.h"
+#include "filters.h"
+#include "formats.h"
+
+typedef struct ThreadData {
+    AVFrame *frame;
+    int idx;
+} ThreadData;
+
+typedef struct AUpmixContext {
+    const AVClass *class;
+
+    AVChannelLayout out_ch_layout;
+
+    int channels;
+    int n;
+
+    int aswift_size;
+    float gain;
+    float smooth;
+
+    void *state;
+    void *x_pos;
+    void *y_pos;
+
+    int (*do_upmix_in)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
+    int (*do_angle)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
+    int (*do_upmix)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
+    int (*do_upmix_out)(AVFilterContext *ctx, AVFrame *out, const int ch, const int n);
+    int (*upmix_init)(AVFilterContext *ctx);
+    void (*upmix_uninit)(AVFilterContext *ctx);
+} AUpmixContext;
+
+static int query_formats(const AVFilterContext *ctx,
+                         AVFilterFormatsConfig **cfg_in,
+                         AVFilterFormatsConfig **cfg_out)
+{
+    static const enum AVSampleFormat formats[] = {
+        AV_SAMPLE_FMT_FLTP, AV_SAMPLE_FMT_DBLP,
+        AV_SAMPLE_FMT_NONE,
+    };
+    const AUpmixContext *s = ctx->priv;
+    AVFilterChannelLayouts *layouts;
+    int ret;
+
+    ret = ff_set_common_formats_from_list2(ctx, cfg_in, cfg_out, formats);
+    if (ret)
+        return ret;
+
+    layouts = NULL;
+    ret = ff_add_channel_layout(&layouts, &s->out_ch_layout);
+    if (ret)
+        return ret;
+
+    ret = ff_channel_layouts_ref(layouts, &cfg_out[0]->channel_layouts);
+    if (ret)
+        return ret;
+
+    layouts = NULL;
+    ret = ff_add_channel_layout(&layouts, &(AVChannelLayout)AV_CHANNEL_LAYOUT_STEREO);
+    if (ret)
+        return ret;
+
+    return ff_channel_layouts_ref(layouts, &cfg_in[0]->channel_layouts);
+}
+
+enum SurroundChannel {
+    SC_FL = 1, SC_FR, SC_FC, SC_LF, SC_BL, SC_BR, SC_BC, SC_SL, SC_SR,
+    SC_TC, SC_TFC, SC_TFL, SC_TFR, SC_TBC, SC_TBL, SC_TBR,
+    SC_LF2, SC_TSL, SC_TSR, SC_BFC, SC_BFL, SC_BFR, SC_FLC, SC_FRC,
+    SC_NB,
+};
+
+static const float sc_ch_pos[SC_NB][5] = {
+    [SC_FL]  = { -1.f,  1.f, 0.f, 1.f, 0.f },
+    [SC_FR]  = {  1.f,  1.f, 0.f, 0.f, 1.f },
+    [SC_FC]  = {  0.f,  1.f, 0.f, .5f, .5f },
+    [SC_SL]  = { -1.f,  0.f, 0.f, 1.f, 0.f },
+    [SC_SR]  = {  1.f,  0.f, 0.f, 0.f, 1.f },
+    [SC_BL]  = { -1.f, -1.f, 0.f, 1.f, 0.f },
+    [SC_BR]  = {  1.f, -1.f, 0.f, 0.f, 1.f },
+    [SC_BC]  = {  0.f, -1.f, 0.f, .5f, .5f },
+    [SC_LF]  = {  0.f,  0.f, 0.f, .5f, .5f },
+    [SC_LF2] = {  0.f,  0.f, 0.f, .5f, .5f },
+    [SC_FLC] = {-0.4f,  1.f, 0.f, 1.f, 0.f },
+    [SC_FRC] = { 0.4f,  1.f, 0.f, 0.f, 1.f },
+    [SC_TFL] = {-1.0f,  1.f, 1.f, 1.f, 0.f },
+    [SC_TFR] = { 1.0f,  1.f, 1.f, 0.f, 1.f },
+    [SC_TSL] = {-1.0f,  0.f, 1.f, 1.f, 0.f },
+    [SC_TSR] = { 1.0f,  0.f, 1.f, 0.f, 1.f },
+    [SC_TBL] = {-1.0f, -1.f, 1.f, 1.f, 0.f },
+    [SC_TBR] = { 1.0f, -1.f, 1.f, 0.f, 1.f },
+};
+
+static const int sc_map[64] = {
+    [AV_CHAN_FRONT_LEFT      ] = SC_FL,
+    [AV_CHAN_FRONT_RIGHT     ] = SC_FR,
+    [AV_CHAN_FRONT_CENTER    ] = SC_FC,
+    [AV_CHAN_LOW_FREQUENCY   ] = SC_LF,
+    [AV_CHAN_BACK_LEFT       ] = SC_BL,
+    [AV_CHAN_BACK_RIGHT      ] = SC_BR,
+    [AV_CHAN_BACK_CENTER     ] = SC_BC,
+    [AV_CHAN_SIDE_LEFT       ] = SC_SL,
+    [AV_CHAN_SIDE_RIGHT      ] = SC_SR,
+    [AV_CHAN_TOP_CENTER      ] = SC_TC,
+    [AV_CHAN_TOP_FRONT_CENTER] = SC_TFC,
+    [AV_CHAN_TOP_FRONT_LEFT  ] = SC_TFL,
+    [AV_CHAN_TOP_FRONT_RIGHT ] = SC_TFR,
+    [AV_CHAN_TOP_BACK_CENTER ] = SC_TBC,
+    [AV_CHAN_TOP_BACK_LEFT   ] = SC_TBL,
+    [AV_CHAN_TOP_BACK_RIGHT  ] = SC_TBR,
+    [AV_CHAN_LOW_FREQUENCY_2 ] = SC_LF2,
+    [AV_CHAN_TOP_SIDE_LEFT   ] = SC_TSL,
+    [AV_CHAN_TOP_SIDE_RIGHT  ] = SC_TSR,
+    [AV_CHAN_BOTTOM_FRONT_CENTER] = SC_BFC,
+    [AV_CHAN_BOTTOM_FRONT_LEFT  ] = SC_BFL,
+    [AV_CHAN_BOTTOM_FRONT_RIGHT ] = SC_BFR,
+    [AV_CHAN_FRONT_LEFT_OF_CENTER] = SC_FLC,
+    [AV_CHAN_FRONT_RIGHT_OF_CENTER] = SC_FRC,
+};
+
+#define DEPTH 32
+#include "upmix_template.c"
+
+#undef DEPTH
+#define DEPTH 64
+#include "upmix_template.c"
+
+static int config_output(AVFilterLink *outlink)
+{
+    AVFilterContext *ctx = outlink->src;
+    AUpmixContext *s = ctx->priv;
+
+    switch (outlink->format) {
+    case AV_SAMPLE_FMT_FLTP:
+        s->do_angle = angle_fltp;
+        s->do_upmix = upmix_fltp;
+        s->do_upmix_in = upmix_in_fltp;
+        s->do_upmix_out = upmix_out_fltp;
+        s->upmix_init = upmix_init_fltp;
+        s->upmix_uninit = upmix_uninit_fltp;
+        break;
+    case AV_SAMPLE_FMT_DBLP:
+        s->do_angle = angle_dblp;
+        s->do_upmix = upmix_dblp;
+        s->do_upmix_in = upmix_in_dblp;
+        s->do_upmix_out = upmix_out_dblp;
+        s->upmix_init = upmix_init_dblp;
+        s->upmix_uninit = upmix_uninit_dblp;
+        break;
+    default:
+        AVERROR_BUG;
+    }
+
+    return s->upmix_init(ctx);
+}
+
+static int filter_frame(AVFilterLink *inlink, AVFrame *in)
+{
+    AVFilterContext *ctx = inlink->dst;
+    AVFilterLink *outlink = ctx->outputs[0];
+    const int nb_out_channels = outlink->ch_layout.nb_channels;
+    AUpmixContext *s = ctx->priv;
+    const int aswift_size = s->aswift_size;
+    const int nb_threads = FFMIN(aswift_size, ff_filter_get_nb_threads(ctx));
+    AVFrame *out;
+
+    out = ff_get_audio_buffer(outlink, in->nb_samples);
+    if (!out) {
+        av_frame_free(&in);
+        return AVERROR(ENOMEM);
+    }
+    av_frame_copy_props(out, in);
+
+    for (int n = 0; n < in->nb_samples; n++) {
+        ThreadData td;
+
+        td.frame = in;
+        td.idx = n;
+
+        ff_filter_execute(ctx, s->do_upmix_in, &td, NULL, nb_threads);
+        ff_filter_execute(ctx, s->do_angle, NULL, NULL, nb_threads);
+        ff_filter_execute(ctx, s->do_upmix, NULL, NULL, nb_threads);
+
+        for (int ch = 0; ch < nb_out_channels; ch++)
+            s->do_upmix_out(ctx, out, ch, n);
+    }
+
+    av_frame_free(&in);
+    return ff_filter_frame(outlink, out);
+}
+
+static av_cold void uninit(AVFilterContext *ctx)
+{
+    AUpmixContext *s = ctx->priv;
+
+    if (s->upmix_uninit)
+        s->upmix_uninit(ctx);
+}
+
+static const AVFilterPad upmix_inputs[] = {
+    {
+        .name         = "default",
+        .type         = AVMEDIA_TYPE_AUDIO,
+        .filter_frame = filter_frame,
+    },
+};
+
+static const AVFilterPad upmix_outputs[] = {
+    {
+        .name         = "default",
+        .type         = AVMEDIA_TYPE_AUDIO,
+        .config_props = config_output,
+    },
+};
+
+#define OFFSET(x) offsetof(AUpmixContext, x)
+#define FLAGS AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
+#define TFLAGS AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_RUNTIME_PARAM
+
+static const AVOption upmix_options[] = {
+    { "chl_out", "set output channel layout", OFFSET(out_ch_layout), AV_OPT_TYPE_CHLAYOUT, {.str="5.1"}, 0, 0, FLAGS },
+    { "size", "set swift transform size", OFFSET(aswift_size), AV_OPT_TYPE_INT, {.i64=128}, 16, 4096, FLAGS },
+    { "smooth", "set swift smooth value", OFFSET(smooth), AV_OPT_TYPE_FLOAT, {.dbl=0.001}, 0, 1, TFLAGS },
+    { "gain", "set output gain value", OFFSET(gain), AV_OPT_TYPE_FLOAT, {.dbl=0.001}, 0, 32, TFLAGS },
+    { NULL }
+};
+
+AVFILTER_DEFINE_CLASS(upmix);
+
+const FFFilter ff_af_upmix = {
+    .p.name          = "upmix",
+    .p.description   = NULL_IF_CONFIG_SMALL("Audio Upmixer."),
+    .p.priv_class    = &upmix_class,
+    .p.flags         = AVFILTER_FLAG_SLICE_THREADS,
+    .priv_size       = sizeof(AUpmixContext),
+    .uninit          = uninit,
+    FILTER_INPUTS(upmix_inputs),
+    FILTER_OUTPUTS(upmix_outputs),
+    FILTER_QUERY_FUNC2(query_formats),
+};
---
+++ libavfilter/upmix_template.c
@@ -0,0 +1,349 @@
+/*
+ * Copyright (c) 2026 Paul B Mahol
+ *
+ * This file is part of Librempeg
+ *
+ * Librempeg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Librempeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Librempeg; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#undef ftype
+#undef ATAN2
+#undef FABS
+#undef FSQRT
+#undef FEXP
+#undef FEXP10
+#undef EPS
+#undef CLIP
+#undef SAMPLE_FORMAT
+#if DEPTH == 32
+#define ftype float
+#define ATAN2 atan2f
+#define FABS fabsf
+#define FSQRT sqrtf
+#define FEXP expf
+#define FEXP10 ff_exp10f
+#define CLIP av_clipf
+#define SAMPLE_FORMAT fltp
+#define EPS FLT_EPSILON
+#else
+#define ftype double
+#define ATAN2 atan2
+#define FABS fabs
+#define FSQRT sqrt
+#define FEXP exp
+#define FEXP10 ff_exp10
+#define CLIP av_clipd
+#define SAMPLE_FORMAT dblp
+#define EPS DBL_EPSILON
+#endif
+
+#define F(x) ((ftype)(x))
+
+#define fn3(a,b)   a##_##b
+#define fn2(a,b)   fn3(a,b)
+#define fn(a)      fn2(a, SAMPLE_FORMAT)
+
+#define HYPOT(x, y) FSQRT((x)*(x) + (y)*(y))
+
+typedef struct fn(ctype) { ftype re, im; } fn(ctype);
+
+typedef struct fn(aswift_plan) {
+    int size;
+    ftype a_fast, a_slow;
+    fn(ctype) *wf, *ws;
+
+    fn(ctype) *p_fast;
+    fn(ctype) *p_slow;
+    fn(ctype) *x_fast;
+    fn(ctype) *x_slow;
+} fn(aswift_plan);
+
+av_always_inline static fn(ctype) fn(make_complex)(const ftype re, const ftype im)
+{
+    return (fn(ctype)){ re, im };
+}
+
+av_always_inline static fn(ctype) fn(aswift_add)(const fn(ctype) x, const fn(ctype) y)
+{
+    return fn(make_complex)(x.re + y.re, x.im + y.im);
+}
+
+av_always_inline static fn(ctype) fn(aswift_mul)(const fn(ctype) x, const fn(ctype) y)
+{
+    return fn(make_complex)(x.re * y.re - x.im * y.im, x.re * y.im + x.im * y.re);
+}
+
+av_always_inline static fn(ctype) fn(aswift_mul_real)(const fn(ctype) x, const ftype y)
+{
+    return fn(make_complex)(x.re * y, x.im * y);
+}
+
+av_always_inline static fn(ctype) fn(polar)(const ftype r, const ftype t)
+{
+    return fn(make_complex)(r * cos(t), r * sin(t));
+}
+
+typedef struct fn(StateContext) {
+    fn(aswift_plan) plan;
+    fn(ctype) *aswift, *iaswift;
+} fn(StateContext);
+
+static void fn(upmix_uninit)(AVFilterContext *ctx)
+{
+    AUpmixContext *s = ctx->priv;
+
+    if (s->state) {
+        fn(StateContext) *state = s->state;
+
+        for (int ch = 0; ch < s->channels; ch++) {
+            fn(StateContext) *stc = &state[ch];
+
+            av_freep(&stc->aswift);
+            av_freep(&stc->iaswift);
+
+            av_freep(&stc->plan.wf);
+            av_freep(&stc->plan.ws);
+            av_freep(&stc->plan.p_fast);
+            av_freep(&stc->plan.p_slow);
+            av_freep(&stc->plan.x_fast);
+            av_freep(&stc->plan.x_slow);
+        }
+    }
+
+    av_freep(&s->state);
+    av_freep(&s->x_pos);
+    av_freep(&s->y_pos);
+}
+
+static int fn(upmix_init)(AVFilterContext *ctx)
+{
+    AUpmixContext *s = ctx->priv;
+    const int channels = FFMAX(ctx->inputs[0]->ch_layout.nb_channels,
+                               ctx->outputs[0]->ch_layout.nb_channels);
+    fn(StateContext) *state;
+
+    s->x_pos = av_calloc(s->aswift_size, sizeof(ftype));
+    if (!s->x_pos)
+        return AVERROR(ENOMEM);
+
+    s->y_pos = av_calloc(s->aswift_size, sizeof(ftype));
+    if (!s->y_pos)
+        return AVERROR(ENOMEM);
+
+    s->state = av_calloc(channels, sizeof(*state));
+    if (!s->state)
+        return AVERROR(ENOMEM);
+    s->channels = channels;
+    state = s->state;
+
+    for (int ch = 0; ch < channels; ch++) {
+        fn(StateContext) *stc = &state[ch];
+
+        stc->aswift = av_calloc(s->aswift_size, sizeof(*stc->aswift));
+        if (!stc->aswift)
+            return AVERROR(ENOMEM);
+
+        stc->iaswift = av_calloc(s->aswift_size, sizeof(*stc->iaswift));
+        if (!stc->iaswift)
+            return AVERROR(ENOMEM);
+
+        stc->plan.size = s->aswift_size;
+        stc->plan.wf = av_calloc(stc->plan.size, sizeof(*stc->plan.wf));
+        if (!stc->plan.wf)
+            return AVERROR(ENOMEM);
+
+        stc->plan.ws = av_calloc(stc->plan.size, sizeof(*stc->plan.ws));
+        if (!stc->plan.ws)
+            return AVERROR(ENOMEM);
+
+        stc->plan.a_fast = FEXP(-F(1.0) / 10);
+        stc->plan.a_slow = FEXP(-F(1.0) / 50);
+
+        stc->plan.x_fast = av_calloc(s->aswift_size, sizeof(*stc->plan.x_fast));
+        if (!stc->plan.x_fast)
+            return AVERROR(ENOMEM);
+
+        stc->plan.x_slow = av_calloc(s->aswift_size, sizeof(*stc->plan.x_slow));
+        if (!stc->plan.x_slow)
+            return AVERROR(ENOMEM);
+
+        stc->plan.p_fast = av_calloc(s->aswift_size, sizeof(*stc->plan.p_fast));
+        if (!stc->plan.p_fast)
+            return AVERROR(ENOMEM);
+
+        stc->plan.p_slow = av_calloc(s->aswift_size, sizeof(*stc->plan.p_slow));
+        if (!stc->plan.p_slow)
+            return AVERROR(ENOMEM);
+
+        for (int n = 0; n < stc->plan.size; n++) {
+            stc->plan.wf[n] = fn(polar)(F(1.0), (n+1) * F(-2.0)*F(M_PI) / stc->plan.size);
+            stc->plan.wf[n] = fn(aswift_mul_real)(stc->plan.wf[n], stc->plan.a_fast);
+            stc->plan.ws[n] = fn(polar)(F(1.0), (n+1) * F(-2.0)*F(M_PI) / stc->plan.size);
+            stc->plan.ws[n] = fn(aswift_mul_real)(stc->plan.ws[n], stc->plan.a_slow);
+        }
+    }
+
+    return 0;
+}
+
+static int fn(angle)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+{
+    AUpmixContext *s = ctx->priv;
+    const int N = s->aswift_size;
+    const int start = (N * jobnr) / nb_jobs;
+    const int end = (N * (jobnr+1)) / nb_jobs;
+    fn(StateContext) *state = s->state;
+    fn(StateContext) *stc0 = &state[0];
+    fn(StateContext) *stc1 = &state[1];
+    fn(ctype) *aswift0 = stc0->aswift;
+    fn(ctype) *aswift1 = stc1->aswift;
+    const ftype smooth = s->smooth;
+    const ftype asmooth = F(1.0) - smooth;
+    ftype *x_pos = s->x_pos;
+    ftype *y_pos = s->y_pos;
+
+    for (int n = start; n < end; n++) {
+        ftype l_re = aswift0[n].re, r_re = aswift1[n].re;
+        ftype l_im = aswift0[n].im, r_im = aswift1[n].im;
+        ftype l_mag = HYPOT(l_re, l_im);
+        ftype r_mag = HYPOT(r_re, r_im);
+        ftype x = (r_mag-l_mag)/(r_mag+l_mag+EPS);
+        ftype re, im, a, y;
+
+        re = l_re * r_re + l_im * r_im;
+        im = r_re * l_im - r_im * l_re;
+        a = ATAN2(im, re);
+        y = F(1.0) - FABS(a * F(M_2_PI));
+        x = CLIP(x, F(-1.0), F(1.0));
+        y = CLIP(y, F(-1.0), F(1.0));
+
+        x = x_pos[n] * asmooth + x * smooth;
+        y = y_pos[n] * asmooth + y * smooth;
+        x_pos[n] = isnormal(x) ? x : F(0.0);
+        y_pos[n] = isnormal(y) ? y : F(0.0);
+    }
+
+    return 0;
+}
+
+static int fn(upmix)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+{
+    AUpmixContext *s = ctx->priv;
+    const int nb_out_channels = s->out_ch_layout.nb_channels;
+    const int N = s->aswift_size;
+    const int start = (N * jobnr) / nb_jobs;
+    const int end = (N * (jobnr+1)) / nb_jobs;
+
+    for (int ch = 0; ch < nb_out_channels; ch++) {
+        const int chan = av_channel_layout_channel_from_index(&s->out_ch_layout, ch);
+        const int sc_chan = sc_map[chan];
+        fn(StateContext) *state = s->state;
+        fn(StateContext) *stc0 = &state[0];
+        fn(StateContext) *stc1 = &state[1];
+        fn(StateContext) *stc = &state[ch];
+        const fn(ctype) *srcl = stc0->aswift;
+        const fn(ctype) *srcr = stc1->aswift;
+        const ftype ch_x = sc_ch_pos[sc_chan][0];
+        const ftype ch_y = sc_ch_pos[sc_chan][1];
+        const ftype sign0 = sc_ch_pos[sc_chan][3];
+        const ftype sign1 = sc_ch_pos[sc_chan][4];
+        fn(ctype) *dst = stc->iaswift;
+        const ftype *x = s->x_pos;
+        const ftype *y = s->y_pos;
+
+        if (chan == AV_CHAN_NONE)
+            continue;
+
+        for (int n = start; n < end; n++) {
+            const ftype xx = (FABS(ch_x - x[n]) + FABS(ch_y - y[n])) * F(-0.5);
+            const ftype xf = FEXP10(xx);
+            const fn(ctype) l = srcl[n];
+            const fn(ctype) r = srcr[n];
+            fn(ctype) ret;
+
+            ret.re = sign0 * xf * l.re + xf * r.re * sign1;
+            ret.im = sign0 * xf * l.im + xf * r.im * sign1;
+
+            dst[n] = ret;
+        }
+    }
+
+    return 0;
+}
+
+static int fn(upmix_in)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+{
+    AUpmixContext *s = ctx->priv;
+    ThreadData *td = arg;
+    AVFrame *in = td->frame;
+    const int idx = td->idx;
+    const int nb_in_channels = in->ch_layout.nb_channels;
+    const int N = s->aswift_size;
+    const int start = (N * jobnr) / nb_jobs;
+    const int end = (N * (jobnr+1)) / nb_jobs;
+    fn(StateContext) *state = s->state;
+
+    for (int ch = 0; ch < nb_in_channels; ch++) {
+        fn(StateContext) *stc = &state[ch];
+        const ftype *src = (const ftype *)in->extended_data[ch];
+        fn(aswift_plan) *plan = &stc->plan;
+        fn(ctype) *out = stc->aswift;
+        const fn(ctype) *wf = plan->wf;
+        const fn(ctype) *ws = plan->ws;
+        fn(ctype) *fast = plan->x_fast;
+        fn(ctype) *slow = plan->x_slow;
+        fn(ctype) *pfast = plan->p_fast;
+        fn(ctype) *pslow = plan->p_slow;
+        fn(ctype) csample = fn(polar)(src[idx], F(0.0));
+
+        for (int n = start; n < end; n++) {
+            fast[n] = fn(aswift_add)(fn(aswift_mul)(pfast[n], wf[n]), csample);
+            slow[n] = fn(aswift_add)(fn(aswift_mul)(pslow[n], ws[n]), csample);
+
+            out[n].re = slow[n].re - fast[n].re;
+            out[n].im = slow[n].im - fast[n].im;
+
+            pfast[n].re = isnormal(fast[n].re) ? fast[n].re : F(0.0);
+            pfast[n].im = isnormal(fast[n].im) ? fast[n].im : F(0.0);
+            pslow[n].re = isnormal(slow[n].re) ? slow[n].re : F(0.0);
+            pslow[n].im = isnormal(slow[n].im) ? slow[n].im : F(0.0);
+        }
+    }
+
+    return 0;
+}
+
+static int fn(upmix_out)(AVFilterContext *ctx, AVFrame *out, const int ch,
+                         const int idx)
+{
+    AUpmixContext *s = ctx->priv;
+    const ftype gain = s->gain;
+    fn(StateContext) *state = s->state;
+    fn(StateContext) *stc = &state[ch];
+    ftype *dst = (ftype *)out->extended_data[ch];
+    fn(aswift_plan) *plan = &stc->plan;
+    fn(ctype) *in = stc->iaswift;
+    const int N = plan->size;
+    ftype acc = F(0.0);
+
+    for (int n = 0; n < N; n += 2) {
+        acc += in[n+0].re;
+        acc -= in[n+1].re;
+    }
+
+    dst[idx] = acc * gain / N;
+
+    return 0;
+}
