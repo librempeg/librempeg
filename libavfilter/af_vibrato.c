d518fa8378f7342371a0f0610c2c26d943c8887d
Paul B Mahol <onemda@gmail.com>
2025-05-25 21:08:08 +0000

    avfilter/af_vibrato: rewrite and cleanup
    Add fltp sample format support.
    Add timeline support.
    Add slice threading.

--- libavfilter/af_vibrato.c
+++ libavfilter/af_vibrato.c
@@ -31,41 +31,42 @@ typedef struct VibratoContext {
     double depth;
     int channels;
 
-    double **buf;
-    int buf_index;
+    AVFrame *buf, *in;
+    int *buf_index;
     int buf_size;
 
-    double *wave_table;
-    int wave_table_index;
+    void *wave_table;
+    int *wave_table_index;
     int wave_table_size;
+
+    int (*filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
 } VibratoContext;
 
 #define OFFSET(x) offsetof(VibratoContext, x)
 #define FLAGS AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
+#define TFLAGS AV_OPT_FLAG_AUDIO_PARAM|AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_RUNTIME_PARAM
 
 static const AVOption vibrato_options[] = {
     { "f", "set frequency in hertz",    OFFSET(freq),    AV_OPT_TYPE_DOUBLE,   {.dbl = 5.0},   0.1,   20000.0, FLAGS },
-    { "d", "set depth as percentage",   OFFSET(depth),   AV_OPT_TYPE_DOUBLE,   {.dbl = 0.5},   0.00,  1.0,     FLAGS },
+    { "d", "set depth as percentage",   OFFSET(depth),   AV_OPT_TYPE_DOUBLE,   {.dbl = 0.5},   0.00,  1.0,     TFLAGS },
     { NULL }
 };
 
 AVFILTER_DEFINE_CLASS(vibrato);
 
+#define DEPTH 32
+#include "vibrato_template.c"
+
+#undef DEPTH
+#define DEPTH 64
+#include "vibrato_template.c"
+
 static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 {
     AVFilterContext *ctx = inlink->dst;
-    VibratoContext *s = ctx->priv;
-    const int wave_table_size = s->wave_table_size;
-    const double *wave_table = s->wave_table;
     AVFilterLink *outlink = ctx->outputs[0];
-    const int channels = s->channels;
-    const int buf_size = s->buf_size;
-    const double depth = s->depth;
-    int wave_table_index = s->wave_table_index;
-    int buf_index = s->buf_index;
+    VibratoContext *s = ctx->priv;
     AVFrame *out;
-    const double *src;
-    double *dst;
 
     if (av_frame_is_writable(in)) {
         out = in;
@@ -78,40 +79,11 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         av_frame_copy_props(out, in);
     }
 
-    for (int n = 0; n < in->nb_samples; n++) {
-        int samp1_index, samp2_index;
-        double integer, decimal;
-        decimal = modf(depth * wave_table[wave_table_index], &integer);
+    s->in = in;
+    ff_filter_execute(ctx, s->filter_channels, out, NULL,
+                      FFMIN(s->channels, ff_filter_get_nb_threads(ctx)));
 
-        wave_table_index++;
-        if (wave_table_index >= wave_table_size)
-            wave_table_index -= wave_table_size;
-
-        samp1_index = buf_index + integer;
-        if (samp1_index >= buf_size)
-            samp1_index -= buf_size;
-        samp2_index = samp1_index + 1;
-        if (samp2_index >= buf_size)
-            samp2_index -= buf_size;
-
-        for (int c = 0; c < channels; c++) {
-            double *buf, this_samp;
-
-            src = (const double *)in->extended_data[c];
-            dst = (double *)out->extended_data[c];
-            buf = s->buf[c];
-
-            this_samp = src[n];
-            dst[n] = buf[samp1_index] + (decimal * (buf[samp2_index] - buf[samp1_index]));
-            buf[buf_index] = this_samp;
-        }
-        buf_index++;
-        if (buf_index >= buf_size)
-            buf_index -= buf_size;
-    }
-
-    s->wave_table_index = wave_table_index;
-    s->buf_index = buf_index;
+    s->in = NULL;
     if (in != out)
         av_frame_free(&in);
 
@@ -121,38 +93,43 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 static av_cold void uninit(AVFilterContext *ctx)
 {
     VibratoContext *s = ctx->priv;
-    int c;
 
+    av_freep(&s->wave_table_index);
     av_freep(&s->wave_table);
-    for (c = 0; c < s->channels; c++)
-        av_freep(&s->buf[c]);
-    av_freep(&s->buf);
+    av_freep(&s->buf_index);
+    av_frame_free(&s->buf);
 }
 
 static int config_input(AVFilterLink *inlink)
 {
-    int c;
     AVFilterContext *ctx = inlink->dst;
     VibratoContext *s = ctx->priv;
 
-    s->buf = av_calloc(inlink->ch_layout.nb_channels, sizeof(*s->buf));
-    if (!s->buf)
-        return AVERROR(ENOMEM);
+    switch (inlink->format) {
+    case AV_SAMPLE_FMT_DBLP:
+        s->filter_channels = filter_channels_dblp;
+        break;
+    case AV_SAMPLE_FMT_FLTP:
+        s->filter_channels = filter_channels_fltp;
+        break;
+    default:
+        return AVERROR_BUG;
+    }
+
     s->channels = inlink->ch_layout.nb_channels;
     s->buf_size = lrint(inlink->sample_rate * 0.005 + 0.5);
-    for (c = 0; c < s->channels; c++) {
-        s->buf[c] = av_malloc_array(s->buf_size, sizeof(*s->buf[c]));
-        if (!s->buf[c])
-            return AVERROR(ENOMEM);
-    }
-    s->buf_index = 0;
+    s->buf_index = av_calloc(s->channels, sizeof(s->buf_index));
+    s->buf = ff_get_audio_buffer(ctx->outputs[0], s->buf_size);
+    if (!s->buf || !s->buf_index)
+        return AVERROR(ENOMEM);
 
     s->wave_table_size = lrint(inlink->sample_rate / s->freq + 0.5);
-    s->wave_table = av_malloc_array(s->wave_table_size, sizeof(*s->wave_table));
-    if (!s->wave_table)
+    s->wave_table_index = av_calloc(s->channels, sizeof(s->wave_table_index));
+    s->wave_table = av_calloc(s->wave_table_size, av_get_bytes_per_sample(inlink->format));
+    if (!s->wave_table || !s->wave_table_index)
         return AVERROR(ENOMEM);
-    ff_generate_wave_table(WAVE_SIN, AV_SAMPLE_FMT_DBL, s->wave_table, s->wave_table_size, 0.0, s->buf_size - 1, 3.0 * M_PI_2);
-    s->wave_table_index = 0;
+    ff_generate_wave_table(WAVE_SIN, av_get_packed_sample_fmt(inlink->format), s->wave_table,
+                           s->wave_table_size, 0.0, s->buf_size - 1, 3.0 * M_PI_2);
 
     return 0;
 }
@@ -174,6 +151,8 @@ const FFFilter ff_af_vibrato = {
     .uninit        = uninit,
     FILTER_INPUTS(vibrato_inputs),
     FILTER_OUTPUTS(ff_audio_default_filterpad),
-    FILTER_SINGLE_SAMPLEFMT(AV_SAMPLE_FMT_DBLP),
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
+    FILTER_SAMPLEFMTS(AV_SAMPLE_FMT_FLTP, AV_SAMPLE_FMT_DBLP),
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC |
+                     AVFILTER_FLAG_SLICE_THREADS,
+    .process_command = ff_filter_process_command,
 };
---
+++ libavfilter/vibrato_template.c
@@ -0,0 +1,89 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#undef ftype
+#undef FMOD
+#undef SAMPLE_FORMAT
+#if DEPTH == 32
+#define ftype float
+#define FMOD modff
+#define SAMPLE_FORMAT fltp
+#else
+#define ftype double
+#define FMOD modf
+#define SAMPLE_FORMAT dblp
+#endif
+
+#define F(x) ((ftype)(x))
+
+#define fn3(a,b)   a##_##b
+#define fn2(a,b)   fn3(a,b)
+#define fn(a)      fn2(a, SAMPLE_FORMAT)
+
+static int fn(filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+{
+    VibratoContext *s = ctx->priv;
+    AVFrame *in = s->in;
+    AVFrame *out = arg;
+    const int channels = s->channels;
+    const int start = (channels * jobnr) / nb_jobs;
+    const int end = (channels * (jobnr+1)) / nb_jobs;
+    const int nb_samples = in->nb_samples;
+    const int buf_size = s->buf_size;
+    const ftype depth = s->depth;
+    const int wave_table_size = s->wave_table_size;
+    const ftype *wave_table = s->wave_table;
+
+    for (int ch = start; ch < end; ch++) {
+        const ftype *src = (const ftype *)in->extended_data[ch];
+        ftype *buf = (ftype *)s->buf->extended_data[ch];
+        ftype *dst = (ftype *)out->extended_data[ch];
+        int wave_table_index = s->wave_table_index[ch];
+        int buf_index = s->buf_index[ch];
+        int samp1_index, samp2_index;
+        ftype integer, decimal, this_samp;
+
+        for (int n = 0; n < nb_samples; n++) {
+            decimal = FMOD(depth * wave_table[wave_table_index], &integer);
+
+            wave_table_index++;
+            if (wave_table_index >= wave_table_size)
+                wave_table_index -= wave_table_size;
+
+            samp1_index = buf_index + integer;
+            if (samp1_index >= buf_size)
+                samp1_index -= buf_size;
+            samp2_index = samp1_index + 1;
+            if (samp2_index >= buf_size)
+                samp2_index -= buf_size;
+
+            this_samp = src[n];
+            dst[n] = buf[samp1_index] + (decimal * (buf[samp2_index] - buf[samp1_index]));
+            buf[buf_index] = this_samp;
+
+            buf_index++;
+            if (buf_index >= buf_size)
+                buf_index -= buf_size;
+        }
+
+        s->wave_table_index[ch] = wave_table_index;
+        s->buf_index[ch] = buf_index;
+    }
+
+    return 0;
+}
