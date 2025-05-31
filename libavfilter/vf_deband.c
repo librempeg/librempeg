4854241eb881b5a013d01a26c9e685490c3f706f
Paul B Mahol <onemda@gmail.com>
2025-05-31 17:44:56 +0000

    avfilter/vf_deband: add frame threading

--- libavfilter/vf_deband.c
+++ libavfilter/vf_deband.c
@@ -416,14 +416,23 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFilterContext *ctx = inlink->dst;
     AVFilterLink *outlink = ctx->outputs[0];
     DebandContext *s = ctx->priv;
-    AVFrame *out;
     ThreadData td;
+    AVFrame *out;
+    int ret;
 
-    out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
+    out = av_frame_alloc();
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
     }
+
+    ret = ff_filter_get_buffer(ctx, out);
+    if (ret < 0) {
+        av_frame_free(&out);
+        av_frame_free(&in);
+        return ret;
+    }
+
     av_frame_copy_props(out, in);
 
     td.in = in; td.out = out;
@@ -467,7 +476,8 @@ const FFFilter ff_vf_deband = {
     .p.name        = "deband",
     .p.description = NULL_IF_CONFIG_SMALL("Debands video."),
     .p.priv_class  = &deband_class,
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC | AVFILTER_FLAG_SLICE_THREADS,
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC | AVFILTER_FLAG_SLICE_THREADS |
+                     AVFILTER_FLAG_FRAME_THREADS,
     .priv_size     = sizeof(DebandContext),
     .uninit        = uninit,
     FILTER_INPUTS(deband_inputs),
