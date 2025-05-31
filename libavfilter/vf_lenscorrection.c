afcc6ba91cb9414e715545354928cc3661f8e7ea
Paul B Mahol <onemda@gmail.com>
2025-05-31 18:45:42 +0000

    avfilter/vf_lenscorrection: add frame threading

--- libavfilter/vf_lenscorrection.c
+++ libavfilter/vf_lenscorrection.c
@@ -300,12 +300,21 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFilterContext *ctx = inlink->dst;
     AVFilterLink *outlink = ctx->outputs[0];
     LenscorrectionCtx *rect = ctx->priv;
-    AVFrame *out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
     ThreadData td;
+    AVFrame *out;
+    int ret;
 
+    out = av_frame_alloc();
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
+    }
+
+    ret = ff_filter_get_buffer(ctx, out);
+    if (ret < 0) {
+        av_frame_free(&out);
+        av_frame_free(&in);
+        return ret;
     }
 
     av_frame_copy_props(out, in);
@@ -353,7 +362,8 @@ const FFFilter ff_vf_lenscorrection = {
     .p.name        = "lenscorrection",
     .p.description = NULL_IF_CONFIG_SMALL("Rectify the image by correcting for lens distortion."),
     .p.priv_class  = &lenscorrection_class,
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC | AVFILTER_FLAG_SLICE_THREADS,
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC | AVFILTER_FLAG_SLICE_THREADS |
+                     AVFILTER_FLAG_FRAME_THREADS,
     .priv_size     = sizeof(LenscorrectionCtx),
     FILTER_INPUTS(lenscorrection_inputs),
     FILTER_OUTPUTS(lenscorrection_outputs),
