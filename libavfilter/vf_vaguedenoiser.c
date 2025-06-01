90a39cfe308bd56996f821d5b955670ee4769834
Paul B Mahol <onemda@gmail.com>
2025-06-01 18:33:51 +0000

    avfilter/vf_vaguedenoiser: add frame threading

--- libavfilter/vf_vaguedenoiser.c
+++ libavfilter/vf_vaguedenoiser.c
@@ -536,10 +536,19 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     if (av_frame_is_writable(in)) {
         out = in;
     } else {
-        out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
+        int ret;
+
+        out = av_frame_alloc();
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
+        }
+
+        ret = ff_filter_get_buffer(ctx, out);
+        if (ret < 0) {
+            av_frame_free(&out);
+            av_frame_free(&in);
+            return ret;
         }
 
         av_frame_copy_props(out, in);
@@ -595,7 +604,8 @@ const FFFilter ff_vf_vaguedenoiser = {
     .p.name        = "vaguedenoiser",
     .p.description = NULL_IF_CONFIG_SMALL("Apply a Wavelet based Denoiser."),
     .p.priv_class  = &vaguedenoiser_class,
-    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
+    .p.flags       = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC |
+                     AVFILTER_FLAG_FRAME_THREADS,
     .priv_size     = sizeof(VagueDenoiserContext),
     .init          = init,
     .uninit        = uninit,
