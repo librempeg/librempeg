a99f7ecd8b92982b79f5f71679d8442648d9d06a
Paul B Mahol <onemda@gmail.com>
2025-06-02 10:07:51 +0000

    avfilter/vf_zoompan: remove swscale dependency

--- libavfilter/vf_zoompan.c
+++ libavfilter/vf_zoompan.c
@@ -24,7 +24,6 @@
 #include "avfilter.h"
 #include "filters.h"
 #include "video.h"
-#include "libswscale/swscale.h"
 
 static const char *const var_names[] = {
     "in_w",   "iw",
@@ -74,7 +73,7 @@ enum var_name {
     VARS_NB
 };
 
-typedef struct ZPcontext {
+typedef struct ZPContext {
     const AVClass *class;
     char *zoom_expr_str;
     char *x_expr_str;
@@ -83,11 +82,12 @@ typedef struct ZPcontext {
 
     AVExpr *zoom_expr, *x_expr, *y_expr;
 
+    int nb_planes;
+    int log2_chroma_w, log2_chroma_h;
     int w, h;
     double x, y;
     double prev_zoom;
     int prev_nb_frames;
-    struct SwsContext *sws;
     int64_t frame_count;
     const AVPixFmtDescriptor *desc;
     AVFrame *in;
@@ -123,6 +123,7 @@ static av_cold int init(AVFilterContext *ctx)
 
 static int config_output(AVFilterLink *outlink)
 {
+    const AVPixFmtDescriptor *pixdesc = av_pix_fmt_desc_get(outlink->format);
     AVFilterContext *ctx = outlink->src;
     FilterLink *l = ff_filter_link(outlink);
     ZPContext *s = ctx->priv;
@@ -133,6 +134,9 @@ static int config_output(AVFilterLink *outlink)
     outlink->time_base = av_inv_q(s->framerate);
     l->frame_rate = s->framerate;
     s->desc = av_pix_fmt_desc_get(outlink->format);
+    s->nb_planes = av_pix_fmt_count_planes(outlink->format);
+    s->log2_chroma_w = pixdesc->log2_chroma_w;
+    s->log2_chroma_h = pixdesc->log2_chroma_h;
     s->finished = 1;
 
     ret = av_expr_parse(&s->zoom_expr, s->zoom_expr_str, var_names, NULL, NULL, NULL, NULL, 0, ctx);
@@ -158,10 +162,8 @@ static int output_single_frame(AVFilterContext *ctx, AVFrame *in, double *var_va
     FilterLink *outl = ff_filter_link(outlink);
     AVFilterLink *inlink = ctx->inputs[0];
     int64_t pts = s->frame_count;
-    int k, x, y, w, h, ret = 0;
-    uint8_t *input[4];
-    int px[4], py[4];
     AVFrame *out;
+    int ret = 0;
 
     var_values[VAR_PX]    = s->x;
     var_values[VAR_PY]    = s->y;
@@ -178,20 +180,16 @@ static int output_single_frame(AVFilterContext *ctx, AVFrame *in, double *var_va
 
     *zoom = av_clipd(*zoom, 1, 10);
     var_values[VAR_ZOOM] = *zoom;
-    w = in->width * (1.0 / *zoom);
-    h = in->height * (1.0 / *zoom);
 
     *dx = av_expr_eval(s->x_expr, var_values, NULL);
 
-    x = *dx = av_clipd(*dx, 0, FFMAX(in->width - w, 0));
+    *dx = av_clipd(*dx, 0, in->width-1);
     var_values[VAR_X] = *dx;
-    x &= ~((1 << s->desc->log2_chroma_w) - 1);
 
     *dy = av_expr_eval(s->y_expr, var_values, NULL);
 
-    y = *dy = av_clipd(*dy, 0, FFMAX(in->height - h, 0));
+    *dy = av_clipd(*dy, 0, in->height-1);
     var_values[VAR_Y] = *dy;
-    y &= ~((1 << s->desc->log2_chroma_h) - 1);
 
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
     if (!out) {
@@ -199,40 +197,58 @@ static int output_single_frame(AVFilterContext *ctx, AVFrame *in, double *var_va
         return ret;
     }
 
-    px[1] = px[2] = AV_CEIL_RSHIFT(x, s->desc->log2_chroma_w);
-    px[0] = px[3] = x;
+    for (int p = 0; p < s->nb_planes; p++) {
+        const int log2_chroma_h = (p > 0 && p < 3) ? s->log2_chroma_h : 0;
+        const int log2_chroma_w = (p > 0 && p < 3) ? s->log2_chroma_w : 0;
+        const int h = AV_CEIL_RSHIFT(outlink->h, log2_chroma_h);
+        const int w = AV_CEIL_RSHIFT(outlink->w, log2_chroma_w);
+        const ptrdiff_t dst_linesize = out->linesize[p];
+        const ptrdiff_t src_linesize = in->linesize[p];
+        const int ih = AV_CEIL_RSHIFT(in->height, log2_chroma_h);
+        const int iw = AV_CEIL_RSHIFT(in->width, log2_chroma_w);
+        const float fw = iw * (1.0 / *zoom);
+        const float fh = ih * (1.0 / *zoom);
+        const uint8_t *src = in->data[p];
+        uint8_t *dst = out->data[p];
+        const float fdx = (*dx) / (1 << log2_chroma_w);
+        const float fdy = (*dy) / (1 << log2_chroma_h);
 
-    py[1] = py[2] = AV_CEIL_RSHIFT(y, s->desc->log2_chroma_h);
-    py[0] = py[3] = y;
+        for (int y = 0; y < h; y++) {
+            const float yh = y / (float)h;
+            const float py = fdy + fh*yh;
+            const float fy = floorf(py);
+            const float ffy = py - fy;
+            const int iy = fy;
 
-    s->sws = sws_alloc_context();
-    if (!s->sws) {
-        ret = AVERROR(ENOMEM);
-        goto error;
+            for (int x = 0; x < w; x++) {
+                const float xw = x / (float)w;
+                const float px = fdx + fw*xw;
+                const float fx = floorf(px);
+                const float ffx = px - fx;
+                const int ix = fx;
+                ptrdiff_t pos0 = ix + src_linesize * iy;
+                ptrdiff_t pos1 = ix + src_linesize * iy + 1;
+                ptrdiff_t pos2 = ix + src_linesize * (iy+1);
+                ptrdiff_t pos3 = ix + src_linesize * (iy+1) + 1;
+                const int v0 = src[pos0];
+                const int v1 = src[pos1];
+                const int v2 = src[pos2];
+                const int v3 = src[pos3];
+
+                dst[x] = lrintf(v0 * (1.f - ffx) * (1.f - ffy) +
+                                v1 * (      ffx) * (1.f - ffy)+
+                                v2 * (1.f - ffx) * (      ffy)+
+                                v3 * (      ffx) * (      ffy));
+            }
+
+            dst += dst_linesize;
+        }
     }
-
-    for (k = 0; in->data[k]; k++)
-        input[k] = in->data[k] + py[k] * in->linesize[k] + px[k];
-
-    av_opt_set_int(s->sws, "srcw", w, 0);
-    av_opt_set_int(s->sws, "srch", h, 0);
-    av_opt_set_int(s->sws, "src_format", in->format, 0);
-    av_opt_set_int(s->sws, "dstw", outlink->w, 0);
-    av_opt_set_int(s->sws, "dsth", outlink->h, 0);
-    av_opt_set_int(s->sws, "dst_format", outlink->format, 0);
-    av_opt_set_int(s->sws, "sws_flags", SWS_BICUBIC, 0);
-
-    if ((ret = sws_init_context(s->sws, NULL, NULL)) < 0)
-        goto error;
-
-    sws_scale(s->sws, (const uint8_t *const *)&input, in->linesize, 0, h, out->data, out->linesize);
 
     out->pts = pts;
     s->frame_count++;
 
     ret = ff_filter_frame(outlink, out);
-    sws_freeContext(s->sws);
-    s->sws = NULL;
     s->current_frame++;
 
     if (s->current_frame >= s->nb_frames) {
@@ -248,11 +264,6 @@ static int output_single_frame(AVFilterContext *ctx, AVFrame *in, double *var_va
         av_frame_free(&s->in);
         s->finished = 1;
     }
-    return ret;
-error:
-    sws_freeContext(s->sws);
-    s->sws = NULL;
-    av_frame_free(&out);
     return ret;
 }
 
@@ -345,8 +356,6 @@ static av_cold void uninit(AVFilterContext *ctx)
 {
     ZPContext *s = ctx->priv;
 
-    sws_freeContext(s->sws);
-    s->sws = NULL;
     av_expr_free(s->x_expr);
     av_expr_free(s->y_expr);
     av_expr_free(s->zoom_expr);
