11d518629585fff287e2e9154ee12003255202f2
Paul B Mahol <onemda@gmail.com>
2025-06-13 16:56:34 +0000

    avcodec: unbreak shared build

--- libavutil/float_dsp.c
+++ libavutil/float_dsp.c
@@ -106,16 +106,6 @@ static void vector_dmul_add_c(double *dst, const double *src0, const double *src
         dst[i] = src0[i] * src1[i] + src2[i];
 }
 
-void ff_vector_fmul_reverse_c(float *dst, const float *src0,
-                              const float *src1, int len)
-{
-    int i;
-
-    src1 += len-1;
-    for (i = 0; i < len; i++)
-        dst[i] = src0[i] * src1[-i];
-}
-
 static void butterflies_float_c(float *restrict v1, float *restrict v2,
                                 int len)
 {
