366d469acd00c0d0ff532d906ec9c7eff150bf37
Paul B Mahol <onemda@gmail.com>
2025-06-18 12:21:23 +0000

    avfilter/split: consume all frames before forwarding inlink status

--- libavfilter/split.c
+++ libavfilter/split.c
@@ -96,8 +96,7 @@ static int activate(AVFilterContext *ctx)
         }
 
         av_frame_free(&in);
-        if (ret < 0)
-            return ret;
+        return ret;
     }
 
     if (ff_inlink_acknowledge_status(inlink, &status, &pts)) {
