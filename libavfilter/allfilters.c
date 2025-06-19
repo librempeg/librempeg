HarshK23/FFmpeg

avfilter: add audio overlay filter
Signed-off-by: Harshit Karwal <karwalharshit@gmail.com>

libavfilter/allfilters.c
@@ -67,6 +67,7 @@ extern const AVFilter ff_af_anlmdn;
 extern const AVFilter ff_af_anlmf;
 extern const AVFilter ff_af_anlms;
 extern const AVFilter ff_af_anull;
+extern const AVFilter ff_af_aoverlay;
 extern const AVFilter ff_af_apad;
 extern const AVFilter ff_af_aperms;
 extern const AVFilter ff_af_aphaser;

librempeg/librempeg

Signed-off-by: Paul B Mahol <onemda@gmail.com>

libavfilter/allfilters.c
@@ -91,6 +91,7 @@ extern const FFFilter ff_af_anoformat;
 extern const FFFilter ff_af_anotch;
 extern const FFFilter ff_af_anrmse;
 extern const FFFilter ff_af_anull;
+extern const FFFilter ff_af_aoverlay;
 extern const FFFilter ff_af_apad;
 extern const FFFilter ff_af_aperms;
 extern const FFFilter ff_af_aphaser;