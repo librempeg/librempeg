c0ccc76264403792a0c5434bff1e294c4c4c7a93
Paul B Mahol <onemda@gmail.com>
2025-06-25 06:29:20 +0000

    avfilter/af_compand: limit max delay to 2 seconds

--- libavfilter/af_compand.c
+++ libavfilter/af_compand.c
@@ -89,7 +89,7 @@ static const AVOption compand_options[] = {
     { "opoints", "set output points of transfer function", OFFSET(out_points), AV_OPT_TYPE_FLOAT|AR, { .arr = &def_out_points }, -900, 900, AT },
     { "soft-knee", "set soft-knee", OFFSET(curve_dB), AV_OPT_TYPE_DOUBLE, { .dbl = 0.01 }, 0.01, 900, AT },
     { "gain", "set output gain", OFFSET(gain_dB), AV_OPT_TYPE_DOUBLE, { .dbl = 0 }, -900, 900, AT },
-    { "delay", "set delay for samples before sending them to volume adjuster", OFFSET(delay), AV_OPT_TYPE_DOUBLE, { .dbl = 0 }, 0, 20, A },
+    { "delay", "set delay for samples before sending them to volume adjuster", OFFSET(delay), AV_OPT_TYPE_DOUBLE, { .dbl = 0 }, 0, 2, A },
     { "sidechain", "enable sidechain input", OFFSET(sidechain), AV_OPT_TYPE_BOOL, {.i64=0}, 0, 1, A },
     { NULL }
 };
