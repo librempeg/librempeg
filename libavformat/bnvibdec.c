f652b176240ef087e9b5ff685114d2357d8b8d24
Paul B Mahol <onemda@gmail.com>
2025-07-15 14:43:16 +0000

    avformat/bnvibdec: remove unused headers

--- libavformat/bnvibdec.c
+++ libavformat/bnvibdec.c
@@ -19,9 +19,6 @@
  */
 
 #include "libavutil/intreadwrite.h"
-#include "libavutil/intfloat.h"
-#include "libavutil/dict.h"
-#include "libavutil/mem.h"
 #include "avformat.h"
 #include "demux.h"
 #include "internal.h"
