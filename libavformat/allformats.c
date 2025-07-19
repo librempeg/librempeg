46479844451402190b3099a45a7c205a411a5b2b
Paul B Mahol <onemda@gmail.com>
2025-07-19 07:57:18 +0000

    avformat: add SWAV demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -501,6 +501,7 @@ extern const FFInputFormat  ff_sup_demuxer;
 extern const FFOutputFormat ff_sup_muxer;
 extern const FFInputFormat  ff_svag_demuxer;
 extern const FFInputFormat  ff_svs_demuxer;
+extern const FFInputFormat  ff_swav_demuxer;
 extern const FFInputFormat  ff_swf_demuxer;
 extern const FFOutputFormat ff_swf_muxer;
 extern const FFInputFormat  ff_tac_demuxer;
