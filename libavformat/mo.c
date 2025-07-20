5915420345613bd77484cefe2377c51f50f5edb6
Paul B Mahol <onemda@gmail.com>
2025-07-20 09:15:26 +0000

    avformat/mo: add multi-track audio support

--- libavformat/mo.c
+++ libavformat/mo.c
@@ -27,6 +27,9 @@
 
 typedef struct MODemuxContext {
     AVPacket *audio_pkt;
+    int multi_audio;
+    int nb_audio_tracks;
+    int current_audio_track;
 } MODemuxContext;
 
 static int mo_probe(const AVProbeData *p)
@@ -79,28 +82,89 @@ static int mo_read_header(AVFormatContext *s)
 
     ast->start_time = 0;
     ast->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    ast->codecpar->sample_rate = avio_rl32(pb);
-    ast->codecpar->ch_layout.nb_channels = avio_rl32(pb);
 
     switch (audio_codec) {
     case 0x3341:
     case 0x3241:
+        ast->codecpar->sample_rate = avio_rl32(pb);
+        ast->codecpar->ch_layout.nb_channels = avio_rl32(pb);
         ast->codecpar->codec_id = AV_CODEC_ID_FASTAUDIO;
         ast->codecpar->block_align = 40 * ast->codecpar->ch_layout.nb_channels;
         ffstream(ast)->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+        avpriv_set_pts_info(ast, 64, 1, ast->codecpar->sample_rate);
         break;
     case 0x3941:
     case 0x3841:
+        ast->codecpar->sample_rate = avio_rl32(pb);
+        ast->codecpar->ch_layout.nb_channels = avio_rl32(pb);
         ast->codecpar->codec_id = AV_CODEC_ID_ADPCM_IMA_MO;
         ast->codecpar->block_align = 132 * ast->codecpar->ch_layout.nb_channels;
         ffstream(ast)->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+        avpriv_set_pts_info(ast, 64, 1, ast->codecpar->sample_rate);
+        break;
+    case 0x4D41:
+        m->multi_audio = 1;
+        m->nb_audio_tracks = avio_rl32(pb);
+        if (m->nb_audio_tracks < 1 ||
+            m->nb_audio_tracks > 8)
+            return AVERROR_INVALIDDATA;
+
+        audio_codec = avio_rl16(pb);
+        switch (audio_codec) {
+        case 0x3341:
+        case 0x3241:
+            ast->codecpar->codec_id = AV_CODEC_ID_FASTAUDIO;
+            ast->codecpar->block_align = 40 * ast->codecpar->ch_layout.nb_channels;
+            break;
+        case 0x3941:
+        case 0x3841:
+            ast->codecpar->codec_id = AV_CODEC_ID_ADPCM_IMA_MO;
+            ast->codecpar->block_align = 132 * ast->codecpar->ch_layout.nb_channels;
+            break;
+        default:
+            avpriv_request_sample(s, "codec 0x%X", audio_codec);
+            return AVERROR_PATCHWELCOME;
+        }
+
+        ast->codecpar->sample_rate = avio_rl32(pb);
+        ast->codecpar->ch_layout.nb_channels = avio_rl32(pb);
+        ffstream(ast)->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+        avpriv_set_pts_info(ast, 64, 1, ast->codecpar->sample_rate);
+
+        for (int n = 1; n < m->nb_audio_tracks; n++) {
+            ast = avformat_new_stream(s, NULL);
+            if (!ast)
+                return AVERROR(ENOMEM);
+
+            audio_codec = avio_rl16(pb);
+            switch (audio_codec) {
+            case 0x3341:
+            case 0x3241:
+                ast->codecpar->codec_id = AV_CODEC_ID_FASTAUDIO;
+                ast->codecpar->block_align = 40 * ast->codecpar->ch_layout.nb_channels;
+                break;
+            case 0x3941:
+            case 0x3841:
+                ast->codecpar->codec_id = AV_CODEC_ID_ADPCM_IMA_MO;
+                ast->codecpar->block_align = 132 * ast->codecpar->ch_layout.nb_channels;
+                break;
+            default:
+                avpriv_request_sample(s, "codec 0x%X", audio_codec);
+                return AVERROR_PATCHWELCOME;
+            }
+
+            ast->start_time = 0;
+            ast->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+            ast->codecpar->sample_rate = avio_rl32(pb);
+            ast->codecpar->ch_layout.nb_channels = avio_rl32(pb);
+            ffstream(ast)->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+            avpriv_set_pts_info(ast, 64, 1, ast->codecpar->sample_rate);
+        }
         break;
     default:
         avpriv_request_sample(s, "codec 0x%X", audio_codec);
         return AVERROR_PATCHWELCOME;
     }
-
-    avpriv_set_pts_info(ast, 64, 1, ast->codecpar->sample_rate);
 
     avio_seek(pb, offset, SEEK_SET);
 
@@ -116,8 +180,47 @@ static int mo_read_packet(AVFormatContext *s, AVPacket *pkt)
     int ret;
 
     if (m->audio_pkt->size > 0) {
-        av_packet_move_ref(pkt, m->audio_pkt);
-        pkt->stream_index = 1;
+        if (m->multi_audio) {
+            uint32_t header_size = 4 * m->nb_audio_tracks;
+            uint32_t offset, packet_size, offset_add;
+
+            if (m->audio_pkt->size <= header_size)
+                return AVERROR_INVALIDDATA;
+
+            offset_add = AV_RL32(m->audio_pkt->data+4*m->current_audio_track);
+            if (offset_add >= m->audio_pkt->size - header_size)
+                return AVERROR_INVALIDDATA;
+            offset = header_size + offset_add;
+
+            if (m->current_audio_track + 1 < m->nb_audio_tracks) {
+                packet_size = AV_RL32(m->audio_pkt->data+4*(m->current_audio_track+1));
+                if (packet_size <= offset)
+                    return AVERROR_INVALIDDATA;
+                packet_size -= offset;
+            } else {
+                packet_size = AV_RL32(m->audio_pkt->data+4*m->current_audio_track);
+                if (packet_size >= m->audio_pkt->size - header_size)
+                    return AVERROR_INVALIDDATA;
+                packet_size = m->audio_pkt->size - packet_size - header_size;
+            }
+
+            if (m->audio_pkt->size - packet_size < offset)
+                return AVERROR_INVALIDDATA;
+
+            if ((ret = av_new_packet(pkt, packet_size)) < 0)
+                return ret;
+            memcpy(pkt->data, m->audio_pkt->data + offset, packet_size);
+
+            pkt->stream_index = 1 + m->current_audio_track;
+            m->current_audio_track++;
+            if (m->current_audio_track >= m->nb_audio_tracks) {
+                m->current_audio_track = 0;
+                av_packet_unref(m->audio_pkt);
+            }
+        } else {
+            av_packet_move_ref(pkt, m->audio_pkt);
+            pkt->stream_index = 1;
+        }
         return 0;
     }
 
@@ -153,6 +256,7 @@ static int mo_read_seek(AVFormatContext *s, int stream_index,
 {
     MODemuxContext *m = s->priv_data;
 
+    m->current_audio_track = 0;
     av_packet_unref(m->audio_pkt);
 
     return -1;
