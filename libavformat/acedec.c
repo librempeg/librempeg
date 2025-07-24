0d762ce761b82cd6c7c2c75fb4bc0868b2f52e4c
Paul B Mahol <onemda@gmail.com>
2025-07-24 11:48:23 +0000

    avformat/acedec: add support for XMA2 in version 2 of format

--- libavformat/acedec.c
+++ libavformat/acedec.c
@@ -30,79 +30,154 @@ static int ace_probe(const AVProbeData *p)
 
     if (AV_RB32(p->buf) != MKBETAG('A','A','C',' '))
         return 0;
+
     if (p->buf_size < 0x44)
         return 0;
-    asc = AV_RB32(p->buf + 0x40);
-    if (asc < 0x44 || asc > p->buf_size - 4)
+
+    switch (AV_RB32(p->buf + 0x28)) {
+    case 2:
+        break;
+    case 3:
+        asc = AV_RB32(p->buf + 0x40);
+        if (asc < 0x44 || asc > p->buf_size - 4)
+            return 0;
+        if (AV_RB32(p->buf + asc) != MKBETAG('A','S','C',' '))
+            return 0;
+        break;
+    default:
         return 0;
-    if (AV_RB32(p->buf + asc) != MKBETAG('A','S','C',' '))
-        return 0;
+    }
 
     return AVPROBE_SCORE_MAX / 2 + 1;
 }
 
 static int ace_read_header(AVFormatContext *s)
 {
+    int streams = 0, version, ret, codec, rate = 0, nb_channels = 0;
+    uint32_t chunk, chunk_pos, size = 0;
     AVIOContext *pb = s->pb;
     AVCodecParameters *par;
-    int ret, codec, rate, nb_channels;
-    uint32_t asc_pos, size;
+    int64_t duration = 0;
     AVStream *st;
 
-    avio_skip(pb, 0x40);
-    asc_pos = avio_rb32(pb);
-    if (asc_pos < 0x44)
-        return AVERROR_INVALIDDATA;
-    avio_skip(pb, asc_pos - 0x44);
-    if (avio_rb32(pb) != MKBETAG('A','S','C',' '))
-        return AVERROR_INVALIDDATA;
-    avio_skip(pb, 0xec);
-    codec = avio_rb32(pb);
-    nb_channels = avio_rb32(pb);
-    if (nb_channels <= 0 || nb_channels > 8)
-        return AVERROR_INVALIDDATA;
-    size = avio_rb32(pb);
+    avio_skip(pb, 0x28);
+    version = avio_rb32(pb);
+    switch (version) {
+    case 2:
+        chunk_pos = avio_rb32(pb);
+        avio_skip(pb, chunk_pos - avio_tell(pb));
+        chunk = avio_rb32(pb);
+        if (chunk == MKBETAG('A','S','C',' ')) {
+            avio_skip(pb, 0xcc);
+            chunk = avio_rb32(pb);
+            if (chunk != MKBETAG('W','A','V','E'))
+                return AVERROR_INVALIDDATA;
+            avio_skip(pb, 12);
+
+            chunk = avio_rb16(pb);
+            switch (chunk) {
+            case 0x0400:
+                avio_skip(pb, 2);
+                streams = avio_rb16(pb);
+                codec = avio_rb16(pb);
+                avio_skip(pb, 8);
+                size = avio_rb32(pb);
+                rate = avio_rb32(pb);
+                avio_skip(pb, 16);
+                duration = avio_rb32(pb);
+                avio_skip(pb, 20);
+                chunk_pos += avio_rb32(pb);
+
+                avio_skip(pb, 16);
+                for (int i = 0; i < streams; i++) {
+                    nb_channels += avio_r8(pb);
+                    avio_skip(pb, 0x2f);
+                }
+
+                avio_skip(pb, chunk_pos - avio_tell(pb));
+                break;
+            default:
+                break;
+            }
+        } else {
+            return AVERROR_INVALIDDATA;
+        }
+        break;
+    case 3:
+        avio_skip(pb, 0x14);
+        chunk_pos = avio_rb32(pb);
+        if (chunk_pos < 0x44)
+            return AVERROR_INVALIDDATA;
+        avio_skip(pb, chunk_pos - 0x44);
+        if (avio_rb32(pb) != MKBETAG('A','S','C',' '))
+            return AVERROR_INVALIDDATA;
+        avio_skip(pb, 0xec);
+        codec = avio_rb32(pb);
+        nb_channels = avio_rb32(pb);
+        size = avio_rb32(pb);
+        rate = avio_rb32(pb);
+        avio_skip(pb, 16);
+        break;
+    default:
+        avpriv_request_sample(s, "version %d", version);
+        return AVERROR_PATCHWELCOME;
+    }
+
     if (size == 0)
         return AVERROR_INVALIDDATA;
-    rate = avio_rb32(pb);
+
+    if (nb_channels <= 0 || nb_channels > 8)
+        return AVERROR_INVALIDDATA;
+
     if (rate <= 0)
         return AVERROR_INVALIDDATA;
-    avio_skip(pb, 16);
 
     st = avformat_new_stream(s, NULL);
     if (!st)
         return AVERROR(ENOMEM);
 
+    st->start_time = 0;
     par = st->codecpar;
+    par->codec_type  = AVMEDIA_TYPE_AUDIO;
+    par->ch_layout.nb_channels = nb_channels;
+    par->sample_rate = rate;
+
     switch (codec) {
     case 4:
     case 5:
     case 6:
         par->codec_id = AV_CODEC_ID_ATRAC3;
+        par->block_align = (codec == 4 ? 0x60 : codec == 5 ? 0x98 : 0xC0) * nb_channels;
+        st->duration = (size / par->block_align) * 1024LL;
+
+        ret = ff_alloc_extradata(par, 14);
+        if (ret < 0)
+            return ret;
+
+        AV_WL16(par->extradata, 1);
+        AV_WL16(par->extradata+2, 2048 * nb_channels);
+        AV_WL16(par->extradata+4, 0);
+        AV_WL16(par->extradata+6, codec == 4 ? 1 : 0);
+        AV_WL16(par->extradata+8, codec == 4 ? 1 : 0);
+        AV_WL16(par->extradata+10, 1);
+        AV_WL16(par->extradata+12, 0);
+        break;
+    case 0x165:
+        if (duration > 0)
+            st->duration = duration;
+        par->codec_id = AV_CODEC_ID_XMA2;
+        par->block_align = 0x800;
+        ret = ff_alloc_extradata(par, 34);
+        if (ret < 0)
+            return ret;
+        memset(par->extradata, 0, par->extradata_size);
+        AV_WL16(par->extradata, streams);
+        ffstream(st)->need_parsing = AVSTREAM_PARSE_FULL;
         break;
     default:
         avpriv_request_sample(s, "codec 0x%04X", codec);
         return AVERROR_PATCHWELCOME;
     }
-
-    st->start_time = 0;
-    par->codec_type  = AVMEDIA_TYPE_AUDIO;
-    par->ch_layout.nb_channels = nb_channels;
-    par->sample_rate = rate;
-    par->block_align = (codec == 4 ? 0x60 : codec == 5 ? 0x98 : 0xC0) * nb_channels;
-    st->duration     = (size / par->block_align) * 1024LL;
-
-    ret = ff_alloc_extradata(par, 14);
-    if (ret < 0)
-        return ret;
-
-    AV_WL16(st->codecpar->extradata, 1);
-    AV_WL16(st->codecpar->extradata+2, 2048 * nb_channels);
-    AV_WL16(st->codecpar->extradata+4, 0);
-    AV_WL16(st->codecpar->extradata+6, codec == 4 ? 1 : 0);
-    AV_WL16(st->codecpar->extradata+8, codec == 4 ? 1 : 0);
-    AV_WL16(st->codecpar->extradata+10, 1);
-    AV_WL16(st->codecpar->extradata+12, 0);
 
     avpriv_set_pts_info(st, 64, 1, par->sample_rate);
 
@@ -111,9 +186,14 @@ static int ace_read_header(AVFormatContext *s)
 
 static int ace_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    AVCodecParameters *par = s->streams[0]->codecpar;
+    AVIOContext *pb = s->pb;
+    int ret;
 
-    return av_get_packet(s->pb, pkt, par->block_align);
+    ret = av_get_packet(pb, pkt, s->streams[0]->codecpar->block_align);
+    pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
+    pkt->stream_index = 0;
+
+    return ret;
 }
 
 const FFInputFormat ff_ace_demuxer = {
