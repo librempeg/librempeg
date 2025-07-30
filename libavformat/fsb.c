05cd9d5d3b13ad39f971a90780607b603f82d54b
Paul B Mahol <onemda@gmail.com>
2025-07-30 18:50:23 +0000

    avformat/fsb: add >1 nb_streams support for version 5

--- libavformat/fsb.c
+++ libavformat/fsb.c
@@ -21,10 +21,16 @@
 
 #include "libavutil/avassert.h"
 #include "libavutil/intreadwrite.h"
+#include "libavutil/mem.h"
 #include "avformat.h"
 #include "avio.h"
 #include "demux.h"
 #include "internal.h"
+
+typedef struct FSBStream {
+    int64_t start_offset;
+    int64_t stop_offset;
+} FSBStream;
 
 static int fsb_probe(const AVProbeData *p)
 {
@@ -43,7 +49,8 @@ static int fsb_read_header(AVFormatContext *s)
     int minor_version;
     int64_t offset;
     AVCodecParameters *par;
-    AVStream *st = avformat_new_stream(s, NULL);
+    FSBStream *fst;
+    AVStream *st;
     FFStream *sti;
     int ret;
 
@@ -56,16 +63,26 @@ static int fsb_read_header(AVFormatContext *s)
 
     minor_version = avio_rl32(pb);
 
-    if (!st)
-        return AVERROR(ENOMEM);
-    st->start_time = 0;
-    sti = ffstream(st);
-    par = st->codecpar;
-    par->codec_type  = AVMEDIA_TYPE_AUDIO;
-    par->codec_tag   = 0;
-
     if (version == 1) {
+        st = avformat_new_stream(s, NULL);
+        if (!st)
+            return AVERROR(ENOMEM);
+
+        fst = av_mallocz(sizeof(FSBStream));
+        if (!fst)
+            return AVERROR(ENOMEM);
+        st->priv_data = fst;
+
+        st->start_time = 0;
+        sti = ffstream(st);
+        par = st->codecpar;
+        par->codec_type  = AVMEDIA_TYPE_AUDIO;
+        par->codec_tag   = 0;
+
         offset = 0x80;
+        fst->start_offset = offset;
+        fst->stop_offset = INT64_MAX;
+
         avio_skip(pb, 40);
         st->duration = avio_rl32(pb);
         avio_skip(pb, 4);
@@ -88,8 +105,28 @@ static int fsb_read_header(AVFormatContext *s)
                 avio_skip(pb, 14);
             }
         }
+
+        avpriv_set_pts_info(st, 64, 1, par->sample_rate);
     } else if (version == 3) {
+        st = avformat_new_stream(s, NULL);
+        if (!st)
+            return AVERROR(ENOMEM);
+
+        fst = av_mallocz(sizeof(FSBStream));
+        if (!fst)
+            return AVERROR(ENOMEM);
+        st->priv_data = fst;
+
+        st->start_time = 0;
+        sti = ffstream(st);
+        par = st->codecpar;
+        par->codec_type  = AVMEDIA_TYPE_AUDIO;
+        par->codec_tag   = 0;
+
         offset = avio_rl32(pb) + 0x18;
+        fst->start_offset = offset;
+        fst->stop_offset = INT64_MAX;
+
         avio_skip(pb, 44);
         st->duration = avio_rl32(pb);
         avio_skip(pb, 12);
@@ -143,10 +180,30 @@ static int fsb_read_header(AVFormatContext *s)
             avpriv_request_sample(s, "format 0x%X", format);
             return AVERROR_PATCHWELCOME;
         }
+
+        avpriv_set_pts_info(st, 64, 1, par->sample_rate);
     } else if (version == 4) {
         uint32_t flags;
 
+        st = avformat_new_stream(s, NULL);
+        if (!st)
+            return AVERROR(ENOMEM);
+
+        fst = av_mallocz(sizeof(FSBStream));
+        if (!fst)
+            return AVERROR(ENOMEM);
+        st->priv_data = fst;
+
+        st->start_time = 0;
+        sti = ffstream(st);
+        par = st->codecpar;
+        par->codec_type  = AVMEDIA_TYPE_AUDIO;
+        par->codec_tag   = 0;
+
         offset = avio_rl32(pb) + 0x30;
+        fst->start_offset = offset;
+        fst->stop_offset = INT64_MAX;
+
         avio_skip(pb, 8);
         flags = avio_rl32(pb);
         avio_skip(pb, 68);
@@ -222,8 +279,11 @@ static int fsb_read_header(AVFormatContext *s)
             par->block_align = 1024 * par->ch_layout.nb_channels;
             break;
         }
+
+        avpriv_set_pts_info(st, 64, 1, par->sample_rate);
     } else if (version == 5) {
         uint64_t sample_mode;
+        int64_t start_offset;
         int sample_header_size;
         int sample_data_size;
         int name_table_size;
@@ -244,203 +304,236 @@ static int fsb_read_header(AVFormatContext *s)
 
         avio_seek(pb, base_hsize, SEEK_SET);
 
-        sample_mode = avio_rl64(pb);
-        st->duration = ((sample_mode >> 34) & 0x3FFFFFFF);
-        offset = ((sample_mode >> 7) & 0x07FFFFFF) << 5;
-        offset += base_hsize;
-        offset += sample_header_size;
-        offset += name_table_size;
+        for (int si = 0; si < nb_streams; si++) {
+            FSBStream *fst;
 
-        switch ((sample_mode >> 5) & 0x03) {
-        case 0:  channels = 1; break;
-        case 1:  channels = 2; break;
-        case 2:  channels = 6; break;
-        case 3:  channels = 8; break;
-        default: return AVERROR_INVALIDDATA;
-        }
+            if (avio_feof(pb))
+                return AVERROR_EOF;
 
-        switch ((sample_mode >> 1) & 0x0f) {
-        case 0:  sample_rate = 4000;  break;
-        case 1:  sample_rate = 8000;  break;
-        case 2:  sample_rate = 11000; break;
-        case 3:  sample_rate = 11025; break;
-        case 4:  sample_rate = 16000; break;
-        case 5:  sample_rate = 22050; break;
-        case 6:  sample_rate = 24000; break;
-        case 7:  sample_rate = 32000; break;
-        case 8:  sample_rate = 44100; break;
-        case 9:  sample_rate = 48000; break;
-        case 10: sample_rate = 96000; break;
-        default: return AVERROR_INVALIDDATA;
-        }
+            st = avformat_new_stream(s, NULL);
+            if (!st)
+                return AVERROR(ENOMEM);
 
-        if (sample_mode & 0x01) {
-            uint32_t extraflag, extraflag_type, extraflag_size, extraflag_end;
+            fst = av_mallocz(sizeof(FSBStream));
+            if (!fst)
+                return AVERROR(ENOMEM);
 
-            do {
-                extraflag = avio_rl32(pb);
-                extraflag_type = (extraflag >> 25) & 0x7F;
-                extraflag_size = (extraflag >> 1) & 0xFFFFFF;
-                extraflag_end  = extraflag & 0x01;
+            st->start_time = 0;
+            st->priv_data = fst;
+            sti = ffstream(st);
+            par = st->codecpar;
+            par->codec_type  = AVMEDIA_TYPE_AUDIO;
+            par->codec_tag   = 0;
 
-                if (avio_feof(pb))
-                    break;
+            sample_mode = avio_rl64(pb);
+            st->duration = ((sample_mode >> 34) & 0x3FFFFFFF);
+            start_offset = ((sample_mode >> 7) & 0x07FFFFFF) << 5;
+            start_offset += base_hsize;
+            start_offset += sample_header_size;
+            start_offset += name_table_size;
+            if (si == 0)
+                offset = start_offset;
+            fst->start_offset = start_offset;
 
-                switch (extraflag_type) {
-                case 0x01:
-                    channels = avio_r8(pb);
-                    if (channels == 0)
-                        return AVERROR_INVALIDDATA;
-                    break;
-                case 0x02:
-                    sample_rate = avio_rl32(pb);
-                    if (sample_rate <= 0)
-                        return AVERROR_INVALIDDATA;
-                    break;
-                case 0x05:
-                    avio_skip(pb, 4);
-                    break;
-                case 0x07:
-                    switch (codec) {
-                    case 0x06:
-                        ret = ff_alloc_extradata(par, 32 * channels);
-                        if (ret < 0)
-                            return ret;
+            if (si > 0) {
+                FSBStream *fst = s->streams[si-1]->priv_data;
 
-                        for (int ch = 0; ch < channels; ch++) {
-                            for (int n = 0; n < 16; n++)
-                                AV_WL16(par->extradata + n * 2, avio_rb16(pb));
-                            avio_skip(pb, 0xe);
+                fst->stop_offset = start_offset;
+            }
+
+            if (si == nb_streams-1)
+                fst->stop_offset = offset + sample_data_size;
+
+            switch ((sample_mode >> 5) & 0x03) {
+            case 0:  channels = 1; break;
+            case 1:  channels = 2; break;
+            case 2:  channels = 6; break;
+            case 3:  channels = 8; break;
+            default: return AVERROR_INVALIDDATA;
+            }
+
+            switch ((sample_mode >> 1) & 0x0f) {
+            case 0:  sample_rate = 4000;  break;
+            case 1:  sample_rate = 8000;  break;
+            case 2:  sample_rate = 11000; break;
+            case 3:  sample_rate = 11025; break;
+            case 4:  sample_rate = 16000; break;
+            case 5:  sample_rate = 22050; break;
+            case 6:  sample_rate = 24000; break;
+            case 7:  sample_rate = 32000; break;
+            case 8:  sample_rate = 44100; break;
+            case 9:  sample_rate = 48000; break;
+            case 10: sample_rate = 96000; break;
+            default: return AVERROR_INVALIDDATA;
+            }
+
+            if (sample_mode & 0x01) {
+                uint32_t extraflag, extraflag_type, extraflag_size, extraflag_end;
+
+                do {
+                    extraflag = avio_rl32(pb);
+                    extraflag_type = (extraflag >> 25) & 0x7F;
+                    extraflag_size = (extraflag >> 1) & 0xFFFFFF;
+                    extraflag_end  = extraflag & 0x01;
+
+                    if (avio_feof(pb))
+                        break;
+
+                    switch (extraflag_type) {
+                    case 0x01:
+                        channels = avio_r8(pb);
+                        if (channels == 0)
+                            return AVERROR_INVALIDDATA;
+                        break;
+                    case 0x02:
+                        sample_rate = avio_rl32(pb);
+                        if (sample_rate <= 0)
+                            return AVERROR_INVALIDDATA;
+                        break;
+                    case 0x07:
+                        switch (codec) {
+                        case 0x06:
+                            {
+                                const int blocks = extraflag_size / 46;
+
+                                ret = ff_alloc_extradata(par, 32 * blocks);
+                                if (ret < 0)
+                                    return ret;
+
+                                for (int ch = 0; ch < blocks; ch++) {
+                                    avio_read(pb, par->extradata + ch * 32, 32);
+                                    avio_skip(pb, 14);
+                                }
+                            }
+                            break;
                         }
                         break;
-                    }
-                    break;
-                case 0x09:
-                    switch (codec) {
-                    case 0x0D:
-                        ret = ff_alloc_extradata(par, 12);
-                        if (ret < 0)
-                            return ret;
-                        memset(par->extradata, 0, par->extradata_size);
+                    case 0x09:
+                        switch (codec) {
+                        case 0x0D:
+                            ret = ff_alloc_extradata(par, 12);
+                            if (ret < 0)
+                                return ret;
+                            memset(par->extradata, 0, par->extradata_size);
 
-                        avio_read(pb, par->extradata + 4, 4);
-                        extraflag_size -= 4;
-                        if (par->extradata[4] != 0xFE && extraflag_size >= 4) {
-                            par->block_align = AV_RL16(par->extradata + 4);
                             avio_read(pb, par->extradata + 4, 4);
                             extraflag_size -= 4;
-                        } else {
-                            avio_read(pb, par->extradata + 8, 4);
-                            extraflag_size -= 4;
-                        }
+                            if (par->extradata[4] != 0xFE && extraflag_size >= 4) {
+                                par->block_align = AV_RL16(par->extradata + 4);
+                                avio_read(pb, par->extradata + 4, 4);
+                                extraflag_size -= 4;
+                            } else {
+                                avio_read(pb, par->extradata + 8, 4);
+                                extraflag_size -= 4;
+                            }
 
+                            break;
+                        }
+                        avio_skip(pb, extraflag_size);
+                        break;
+                    case 0x0e:
+                        channels = channels * avio_rl32(pb);
+                        break;
+                    default:
+                        avio_skip(pb, extraflag_size);
                         break;
                     }
-                    avio_skip(pb, extraflag_size);
-                    break;
-                case 0x0e:
-                    channels = channels * avio_rl32(pb);
-                    break;
-                default:
-                    avio_skip(pb, extraflag_size);
-                    break;
-                }
-            } while (extraflag_end != 0);
-        }
+                } while (extraflag_end != 0);
+            }
 
-        par->ch_layout.nb_channels = channels;
-        par->sample_rate = sample_rate;
+            par->ch_layout.nb_channels = channels;
+            par->sample_rate = sample_rate;
 
-        switch (codec) {
-        case 0x01:
-            par->codec_id = AV_CODEC_ID_PCM_U8;
-            par->block_align = 1024 * channels;
-            break;
-        case 0x02:
-            par->codec_id = AV_CODEC_ID_PCM_S16LE;
-            par->block_align = 512 * channels;
-            break;
-        case 0x05:
-            par->codec_id = AV_CODEC_ID_PCM_F32LE;
-            par->block_align = 256 * channels;
-            break;
-        case 0x06:
-            par->codec_id = AV_CODEC_ID_ADPCM_THP_LE;
-            par->block_align = 0x8 * channels;
-            break;
-        case 0x0A:
-            par->codec_id = AV_CODEC_ID_XMA2;
-            par->block_align = 2048;
-            ret = ff_alloc_extradata(par, 34);
-            if (ret < 0)
-                return ret;
-            memset(par->extradata, 0, 34);
-            AV_WL16(par->extradata, 1);
-            sti->need_parsing = AVSTREAM_PARSE_FULL;
-            break;
-        case 0x0B:
-            par->codec_id = AV_CODEC_ID_MP3;
-            par->block_align = 1024;
-            sti->need_parsing = AVSTREAM_PARSE_FULL;
-            break;
-        case 0x0D:
-            par->codec_id = AV_CODEC_ID_ATRAC9;
-            if (par->block_align == 0)
+            switch (codec) {
+            case 0x01:
+                par->codec_id = AV_CODEC_ID_PCM_U8;
+                par->block_align = 1024 * channels;
+                break;
+            case 0x02:
+                par->codec_id = AV_CODEC_ID_PCM_S16LE;
+                par->block_align = 512 * channels;
+                break;
+            case 0x05:
+                par->codec_id = AV_CODEC_ID_PCM_F32LE;
+                par->block_align = 256 * channels;
+                break;
+            case 0x06:
+                par->codec_id = AV_CODEC_ID_ADPCM_THP;
+                par->block_align = 0x8 * channels;
+                break;
+            case 0x0A:
+                par->codec_id = AV_CODEC_ID_XMA2;
+                par->block_align = 2048;
+                ret = ff_alloc_extradata(par, 34);
+                if (ret < 0)
+                    return ret;
+                memset(par->extradata, 0, 34);
+                AV_WL16(par->extradata, 1);
+                sti->need_parsing = AVSTREAM_PARSE_FULL;
+                break;
+            case 0x0B:
+                par->codec_id = AV_CODEC_ID_MP3;
                 par->block_align = 1024;
-            sti->need_parsing = AVSTREAM_PARSE_FULL;
-            break;
-        case 0x10:
-            par->codec_id = AV_CODEC_ID_ADPCM_FMOD;
-            par->block_align = 0x8C * channels;
-            break;
-        default:
-            avpriv_request_sample(s, "codec 0x%X", codec);
-            return AVERROR_PATCHWELCOME;
+                sti->need_parsing = AVSTREAM_PARSE_FULL;
+                break;
+            case 0x0D:
+                par->codec_id = AV_CODEC_ID_ATRAC9;
+                if (par->block_align == 0)
+                    par->block_align = 1024;
+                sti->need_parsing = AVSTREAM_PARSE_FULL;
+                break;
+            case 0x10:
+                par->codec_id = AV_CODEC_ID_ADPCM_FMOD;
+                par->block_align = 0x8C * channels;
+                break;
+            default:
+                avpriv_request_sample(s, "codec 0x%X", codec);
+                return AVERROR_PATCHWELCOME;
+            }
+
+            avpriv_set_pts_info(st, 64, 1, par->sample_rate);
         }
-     } else {
+    } else {
         av_assert0(0);
     }
 
     avio_skip(pb, offset - avio_tell(pb));
-
-    avpriv_set_pts_info(st, 64, 1, par->sample_rate);
 
     return 0;
 }
 
 static int fsb_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    AVCodecParameters *par = s->streams[0]->codecpar;
+    AVIOContext *pb = s->pb;
+    int ret = AVERROR_EOF;
     int64_t pos;
-    int ret;
 
-    if (avio_feof(s->pb))
-        return AVERROR_EOF;
+    for (int n = 0; n < s->nb_streams; n++) {
+        AVStream *st = s->streams[n];
+        AVCodecParameters *par = st->codecpar;
+        FSBStream *fst = st->priv_data;
 
-    pos = avio_tell(s->pb);
-    if (par->codec_id == AV_CODEC_ID_ADPCM_THP &&
-        par->ch_layout.nb_channels > 1) {
-        int i, ch;
+        if (avio_feof(pb))
+            return AVERROR_EOF;
 
-        ret = av_new_packet(pkt, par->block_align);
-        if (ret < 0)
-            return ret;
-        for (i = 0; i < 4; i++) {
-            for (ch = 0; ch < par->ch_layout.nb_channels; ch++) {
-                pkt->data[ch * 8 + i * 2 + 0] = avio_r8(s->pb);
-                pkt->data[ch * 8 + i * 2 + 1] = avio_r8(s->pb);
-            }
+        pos = avio_tell(pb);
+        if (pos >= fst->start_offset && pos < fst->stop_offset) {
+            ret = av_get_packet(pb, pkt, par->block_align);
+
+            pkt->pos = pos;
+            pkt->stream_index = st->index;
+            if (par->codec_id == AV_CODEC_ID_XMA2 && pkt->size >= 1)
+                pkt->duration = (pkt->data[0] >> 2) * 512;
+
+            break;
+        } else if (pos >= fst->stop_offset && n+1 < s->nb_streams) {
+            AVStream *st_next = s->streams[n+1];
+            FSBStream *fst_next = st_next->priv_data;
+
+            if (fst_next->start_offset > pos)
+                avio_skip(pb, fst_next->start_offset - pos);
         }
-        ret = 0;
-    } else {
-        ret = av_get_packet(s->pb, pkt, par->block_align);
     }
-
-    if (par->codec_id == AV_CODEC_ID_XMA2 && pkt->size >= 1)
-        pkt->duration = (pkt->data[0] >> 2) * 512;
-
-    pkt->pos = pos;
-    pkt->stream_index = 0;
 
     return ret;
 }
@@ -450,7 +543,7 @@ const FFInputFormat ff_fsb_demuxer = {
     .p.long_name    = NULL_IF_CONFIG_SMALL("FMOD Sample Bank"),
     .p.extensions   = "fsb",
     .p.flags        = AVFMT_GENERIC_INDEX,
-    .read_probe  = fsb_probe,
-    .read_header = fsb_read_header,
-    .read_packet = fsb_read_packet,
+    .read_probe     = fsb_probe,
+    .read_header    = fsb_read_header,
+    .read_packet    = fsb_read_packet,
 };
