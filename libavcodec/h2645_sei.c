4b94b9f52bc1b3279ae2d418c4009f7000d4148b
Maryla Ustarroz-Calonge <maryla@google.com>
2025-08-05 12:12:41 +0200

    avcodec/itut35: rename some provider codes
    In general, a provider code corresponds to a company/organization, and
    the name should be that of the company, not of a given feature.
    The feature is signalled in the "provider oriented code".
    
    The exception is HDR Vivid, where the code 0x0004 indicates the HDR Vivid
    feature itself rather than CUVA (China UHD Video Industry Alliance),
    according to the UHD World Associations 'HDR Video Technology Part 2-1
    Application Guide to System Integration' document.
    
    ITU_T_T35_PROVIDER_CODE_CUVA -> ITU_T_T35_PROVIDER_CODE_HDR_VIVID
    ITU_T_T35_PROVIDER_CODE_LCEVC-> ITU_T_T35_PROVIDER_CODE_VNOVA
    ITU_T_T35_PROVIDER_CODE_SMTPE -> ITU_T_T35_PROVIDER_CODE_SAMSUNG
    
    Signed-off-by: Maryla Ustarroz-Calonge <maryla@google.com>
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/h2645_sei.c
+++ libavcodec/h2645_sei.c
@@ -167,7 +167,7 @@ static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
         }
     }
 #if CONFIG_HEVC_SEI
-    else if (country_code == ITU_T_T35_COUNTRY_CODE_CN && provider_code == ITU_T_T35_PROVIDER_CODE_CUVA) {
+    else if (country_code == ITU_T_T35_COUNTRY_CODE_CN && provider_code == ITU_T_T35_PROVIDER_CODE_HDR_VIVID) {
         const uint16_t cuva_provider_oriented_code = 0x0005;
         uint16_t provider_oriented_code;
 
@@ -181,7 +181,7 @@ static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
         if (provider_oriented_code == cuva_provider_oriented_code) {
             return decode_registered_user_data_dynamic_hdr_vivid(&h->dynamic_hdr_vivid, gb);
         }
-    } else if(country_code == ITU_T_T35_COUNTRY_CODE_US && provider_code == ITU_T_T35_PROVIDER_CODE_SMTPE) {
+    } else if(country_code == ITU_T_T35_COUNTRY_CODE_US && provider_code == ITU_T_T35_PROVIDER_CODE_SAMSUNG) {
         // A/341 Amendment - 2094-40
         const uint16_t smpte2094_40_provider_oriented_code = 0x0001;
         const uint8_t smpte2094_40_application_identifier = 0x04;
