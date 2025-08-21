5b6edc19f98723df09ba6a0dc347cd62e98b8265
Paul B Mahol <onemda@gmail.com>
2025-08-21 06:32:08 +0000

    avfilter/vf_negate: use ff_graph_frame_alloc/free()

--- libavfilter/vf_negate.c
+++ libavfilter/vf_negate.c
@@ -313,7 +313,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     } else {
         int ret;
 
-        out = av_frame_alloc();
+        out = ff_graph_frame_alloc(ctx);
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
@@ -334,7 +334,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_filter_execute(ctx, filter_slice, &td, NULL,
                       FFMIN(s->height[2], ff_filter_get_nb_threads(ctx)));
     if (out != in)
-        av_frame_free(&in);
+        ff_graph_frame_free(ctx, &in);
 
     return ff_filter_frame(outlink, out);
 }
