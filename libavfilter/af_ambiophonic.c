3b932e2da7056798cae37c0033bc14c950515ed4
Paul B Mahol <onemda@gmail.com>
2025-08-23 08:08:27 +0000

    avfilter/af_ambiophonic: use ff_graph_frame_free()

--- libavfilter/af_ambiophonic.c
+++ libavfilter/af_ambiophonic.c
@@ -152,7 +152,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
     s->do_filter(ctx, in, out);
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
