db598c5ef3032b95d8afce86e1e893d8aae0fa3a
Paul B Mahol <onemda@gmail.com>
2025-08-23 07:31:57 +0000

    avfilter/af_acentercut: use ff_graph_frame_free()

--- libavfilter/af_acentercut.c
+++ libavfilter/af_acentercut.c
@@ -183,7 +183,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     }
 
     s->in = NULL;
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
