acb95fa971a86ff47527efdbf41ccdd1275a591a
Paul B Mahol <onemda@gmail.com>
2025-08-23 11:19:16 +0000

    avfilter/vf_gblur: use ff_graph_frame_alloc/free()

--- libavfilter/vf_gblur.c
+++ libavfilter/vf_gblur.c
@@ -229,7 +229,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     } else {
         int ret;
 
-        out = av_frame_alloc();
+        out = ff_graph_frame_alloc(ctx);
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
@@ -309,7 +309,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     }
 
     if (out != in)
-        av_frame_free(&in);
+        ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
