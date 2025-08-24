0e2775e2bc6f17d8d3cca8dfccc4c45233662ff3
Paul B Mahol <onemda@gmail.com>
2025-08-24 09:45:01 +0000

    avfilter/af_harmonicbass: use ff_graph_frame_free()

--- libavfilter/af_harmonicbass.c
+++ libavfilter/af_harmonicbass.c
@@ -131,7 +131,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
     s->hb_stereo(ctx, out, in);
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
