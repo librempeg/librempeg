7cff6508a6cc88ff93ccd3d17025400a3cfca487
Paul B Mahol <onemda@gmail.com>
2025-08-24 09:04:15 +0000

    avfilter/vf_gamut: use ff_graph_frame_alloc/free()

--- libavfilter/vf_gamut.c
+++ libavfilter/vf_gamut.c
@@ -239,7 +239,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
     AVFrame *out;
     int ret;
 
-    out = av_frame_alloc();
+    out = ff_graph_frame_alloc(ctx);
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
@@ -259,6 +259,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
                                 FFMIN(out->height, ff_filter_get_nb_threads(ctx))))
         return ret;
 
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(ctx->outputs[0], out);
 }
 
