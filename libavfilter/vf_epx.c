8e67ae0b7733ce5d2914bfa7f3678ad74ad21f7e
Paul B Mahol <onemda@gmail.com>
2025-08-24 09:13:38 +0000

    avfilter/vf_epx: use ff_graph_frame_alloc/free()

--- libavfilter/vf_epx.c
+++ libavfilter/vf_epx.c
@@ -237,7 +237,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFrame *out;
     int ret;
 
-    out = av_frame_alloc();
+    out = ff_graph_frame_alloc(ctx);
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
@@ -256,7 +256,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_filter_execute(ctx, s->epx_slice, &td, NULL,
                       FFMIN(inlink->h, ff_filter_get_nb_threads(ctx)));
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
