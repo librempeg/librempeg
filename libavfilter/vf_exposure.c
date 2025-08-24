0978c7f0ad6c9f15812a278bcf5df810ed89b40b
Paul B Mahol <onemda@gmail.com>
2025-08-24 09:05:11 +0000

    avfilter/vf_exposure: use ff_graph_frame_alloc/free()

--- libavfilter/vf_exposure.c
+++ libavfilter/vf_exposure.c
@@ -94,7 +94,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     } else {
         int ret;
 
-        out = av_frame_alloc();
+        out = ff_graph_frame_alloc(ctx);
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
@@ -118,7 +118,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
                       FFMIN(out->height, ff_filter_get_nb_threads(ctx)));
 
     if (out != in)
-        av_frame_free(&in);
+        ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
