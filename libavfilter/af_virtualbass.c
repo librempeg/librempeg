a4fa50088cb942232d5a4eba872c5332c99564bf
Paul B Mahol <onemda@gmail.com>
2025-08-24 09:19:14 +0000

    avfilter/af_virtualbass: use ff_graph_frame_free()

--- libavfilter/af_virtualbass.c
+++ libavfilter/af_virtualbass.c
@@ -128,7 +128,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
     s->vb_stereo(ctx, out, in);
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
