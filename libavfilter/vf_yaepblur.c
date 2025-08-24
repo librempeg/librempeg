de18c83d4a2503c6280556ce4f011770f608a7bf
Paul B Mahol <onemda@gmail.com>
2025-08-24 08:49:35 +0000

    avfilter/vf_yaepblur: use ff_graph_frame_alloc/free()

--- libavfilter/vf_yaepblur.c
+++ libavfilter/vf_yaepblur.c
@@ -232,7 +232,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     } else {
         int ret;
 
-        out = av_frame_alloc();
+        out = ff_graph_frame_alloc(ctx);
         if (!out) {
             av_frame_free(&in);
             return AVERROR(ENOMEM);
@@ -275,7 +275,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     }
 
     if (out != in)
-        av_frame_free(&in);
+        ff_graph_frame_free(ctx, &in);
 
     return ff_filter_frame(outlink, out);
 }
