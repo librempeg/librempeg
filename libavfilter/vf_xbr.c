95141ad260ed29065ced0eac532e819a0e41ae8d
Paul B Mahol <onemda@gmail.com>
2025-08-24 08:50:41 +0000

    avfilter/vf_xbr: use ff_graph_frame_alloc/free()

--- libavfilter/vf_xbr.c
+++ libavfilter/vf_xbr.c
@@ -348,7 +348,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFrame *out;
     int ret;
 
-    out = av_frame_alloc();
+    out = ff_graph_frame_alloc(ctx);
     if (!out) {
         av_frame_free(&in);
         return AVERROR(ENOMEM);
@@ -369,7 +369,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_filter_execute(ctx, s->func, &td, NULL,
                       FFMIN(inlink->h, ff_filter_get_nb_threads(ctx)));
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
