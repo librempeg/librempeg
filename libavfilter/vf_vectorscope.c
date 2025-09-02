b2ce1a93724e5c064563a84cf4bbb31ef2a2cadd
Paul B Mahol <onemda@gmail.com>
2025-09-02 08:28:30 +0000

    avfilter/vf_vectorscope: use ff_graph_frame_free()

--- libavfilter/vf_vectorscope.c
+++ libavfilter/vf_vectorscope.c
@@ -1440,7 +1440,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         }
     }
 
-    av_frame_free(&in);
+    ff_graph_frame_free(ctx, &in);
     return ff_filter_frame(outlink, out);
 }
 
