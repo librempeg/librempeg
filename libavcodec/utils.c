bd63d5e0e77781988e74a084c5853ed4da1f9fa8
Paul B Mahol <onemda@gmail.com>
2025-09-21 23:10:30 +0000

    avcodec/utils: add PCM_SONIC case

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -483,6 +483,7 @@ int av_get_exact_bits_per_sample(enum AVCodecID codec_id)
     case AV_CODEC_ID_PCM_S8:
     case AV_CODEC_ID_PCM_S8_PLANAR:
     case AV_CODEC_ID_PCM_SGA:
+    case AV_CODEC_ID_PCM_SONIC:
     case AV_CODEC_ID_PCM_U8:
     case AV_CODEC_ID_SDX2_DPCM:
     case AV_CODEC_ID_CBD2_DPCM:
