40f04527ccabab08e338da0b20797d781121f62c
Paul B Mahol <onemda@gmail.com>
2025-10-03 13:10:13 +0000

    avformat/bnsf: fix excessive whitespaces

--- libavformat/bnsf.c
+++ libavformat/bnsf.c
@@ -60,12 +60,12 @@ static int bnsf_read_header(AVFormatContext *s)
         switch (chunk) {
         case MKTAG('s', 'f', 'm', 't'):
             avio_skip(pb, 2);
-            par->codec_type  = AVMEDIA_TYPE_AUDIO;
+            par->codec_type = AVMEDIA_TYPE_AUDIO;
             if (codec == MKBETAG('I','S','2','2'))
-                par->codec_id    = AV_CODEC_ID_G719;
+                par->codec_id = AV_CODEC_ID_G719;
             else if (codec == MKBETAG('I','S','1','4'))
-                par->codec_id    = AV_CODEC_ID_G722_1;
-            par->codec_tag   = 0;
+                par->codec_id = AV_CODEC_ID_G722_1;
+            par->codec_tag = 0;
             par->ch_layout.nb_channels = avio_rb16(pb);
             if (par->ch_layout.nb_channels == 0)
                 return AVERROR_INVALIDDATA;
@@ -73,7 +73,7 @@ static int bnsf_read_header(AVFormatContext *s)
             if (par->sample_rate <= 0)
                 return AVERROR_INVALIDDATA;
             st->start_time = 0;
-            st->duration     = avio_rb32(pb);
+            st->duration = avio_rb32(pb);
             avio_skip(pb, 4);
             par->block_align = avio_rb16(pb);
             if (par->block_align == 0)
