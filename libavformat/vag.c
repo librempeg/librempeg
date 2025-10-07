134f62ff7a1fd10a037303d525c17c33811ead1a
Paul B Mahol <onemda@gmail.com>
2025-10-07 13:42:44 +0000

    avformat/vag: extend support for more variants

--- libavformat/vag.c
+++ libavformat/vag.c
@@ -26,19 +26,42 @@
 #include "internal.h"
 #include "pcm.h"
 
-static int vag_probe(const AVProbeData *p)
+static int read_probe(const AVProbeData *p)
 {
-    if (memcmp(p->buf, "VAGp\0\0\0", 7) &&
-        AV_RB32(p->buf) != MKBETAG('V','A','G','2'))
+    if (memcmp(p->buf, "VAG", 3))
+        return 0;
+    if (p->buf_size < 20)
+        return 0;
+    if (p->buf[3] != '1' &&
+        p->buf[3] != '2' &&
+        p->buf[3] != 'i' &&
+        p->buf[3] != 'p')
+        return 0;
+    if ((int)AV_RB32(p->buf+16) <= 0)
         return 0;
 
-    return AVPROBE_SCORE_MAX;
+    return AVPROBE_SCORE_MAX/2;
 }
 
-static int vag_read_header(AVFormatContext *s)
+static int avio_seek_r8(AVIOContext *pb, int64_t offset)
 {
+    avio_seek(pb, offset, SEEK_SET);
+    return avio_r8(pb);
+}
+
+static uint32_t avio_seek_rl32(AVIOContext *pb, int64_t offset)
+{
+    avio_seek(pb, offset, SEEK_SET);
+    return avio_rl32(pb);
+}
+
+static int read_header(AVFormatContext *s)
+{
+    int ret, rate, channels = 0, stream_name_size = 16, align = 0, codec;
+    uint32_t type, version, reserved;
+    int64_t start_offset, duration = 0;
     AVIOContext *pb = s->pb;
-    uint32_t type;
+    char title[33];
     AVStream *st;
 
     st = avformat_new_stream(s, NULL);
@@ -46,34 +69,90 @@ static int vag_read_header(AVFormatContext *s)
         return AVERROR(ENOMEM);
 
     type = avio_rb32(pb);
-    st->start_time = 0;
-    st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->codec_id    = AV_CODEC_ID_ADPCM_PSX;
-    st->codecpar->ch_layout.nb_channels = 1 + (avio_rb32(pb) == 0x00000004);
-    avio_skip(pb, 4);
-    if (st->codecpar->ch_layout.nb_channels > 1) {
-        st->duration = avio_rb32(pb);
-    } else {
-        st->duration = avio_rb32(pb) / 16 * 28;
-    }
-    st->codecpar->sample_rate = avio_rb32(pb);
-    if (st->codecpar->sample_rate <= 0)
+    version = avio_rb32(pb);
+    reserved = avio_rb32(pb);
+    avio_rb32(pb);
+    rate = avio_rb32(pb);
+    codec = AV_CODEC_ID_ADPCM_PSX;
+    if (rate <= 0)
         return AVERROR_INVALIDDATA;
-    if (type == MKBETAG('V','A','G','p'))
-        avio_seek(pb, 0x1000, SEEK_SET);
-    if (type == MKBETAG('V','A','G','2')) {
-        st->codecpar->ch_layout.nb_channels = 2;
-        st->codecpar->block_align = 0x800 * st->codecpar->ch_layout.nb_channels;
-        avio_seek(pb, 0x40, SEEK_SET);
-    } else if (avio_rl32(pb) == MKTAG('V','A','G','p')) {
-        st->codecpar->block_align = 0x1000 * st->codecpar->ch_layout.nb_channels;
-        avio_seek(pb, 0, SEEK_SET);
-        st->duration = st->duration / 16 * 28;
-    } else {
-        st->codecpar->block_align = 16 * st->codecpar->ch_layout.nb_channels;
-        avio_seek(pb, st->codecpar->ch_layout.nb_channels > 1 ? 0x80 : 0x30, SEEK_SET);
+
+    switch (type) {
+    case MKBETAG('V','A','G','1'):
+        start_offset = 0x40;
+        align = 0x10;
+        channels = avio_seek_r8(pb, 0x1e);
+        if (channels == 0)
+            channels = 1;
+        break;
+    case MKBETAG('V','A','G','2'):
+        start_offset = 0x40;
+        align = 0x800;
+        channels = 2;
+        break;
+    case MKBETAG('V','A','G','i'):
+        start_offset = 0x800;
+        align = avio_seek_rl32(pb, 0x08);
+        channels = 2;
+        break;
+    case MKBETAG('V','A','G','p'):
+        switch (version) {
+        case 0x00020001:
+        case 0x00030000:
+            codec = AV_CODEC_ID_ADPCM_HEVAG;
+            start_offset = 0x30;
+            align = 0x10;
+            channels = avio_seek_r8(pb, 0x1e);
+            if (channels == 0)
+                channels = 1;
+            break;
+        case 0x02000000:
+        case 0x40000000:
+            start_offset = 0x40;
+            align = 0x10;
+            channels = 1;
+            break;
+        case 0x00000020:
+            if (reserved == 0x01010101) {
+                start_offset = 0x800;
+                align = 0x400;
+                channels = 2;
+                break;
+            }
+        default:
+            start_offset = 0x30;
+            align = 0x10;
+            channels = 1;
+            break;
+        }
+        break;
+    default:
+        return AVERROR_INVALIDDATA;
     }
+
+    if (channels <= 0 || align <= 0 || align > INT_MAX/channels)
+        return AVERROR_INVALIDDATA;
+
+    st->start_time = 0;
+    if (duration > 0)
+        st->duration = duration;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->codec_id = codec;
+    st->codecpar->block_align = align * channels;
+    st->codecpar->ch_layout.nb_channels = channels;
+    st->codecpar->bit_rate = 16LL * st->codecpar->ch_layout.nb_channels * 8 *
+                                    st->codecpar->sample_rate / 28;
+
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+    avio_seek(pb, 0x20, SEEK_SET);
+    if ((ret = avio_get_str(pb, stream_name_size, title, sizeof(title))) < 0)
+        return ret;
+    if (title[0] != '\0')
+        av_dict_set(&s->metadata, "title", title, 0);
+
+    avio_seek(pb, start_offset, SEEK_SET);
 
     return 0;
 }
@@ -83,7 +162,7 @@ const FFInputFormat ff_vag_demuxer = {
     .p.long_name    = NULL_IF_CONFIG_SMALL("Sony PS2 VAG"),
     .p.flags        = AVFMT_GENERIC_INDEX,
     .p.extensions   = "vag",
-    .read_probe     = vag_probe,
-    .read_header    = vag_read_header,
+    .read_probe     = read_probe,
+    .read_header    = read_header,
     .read_packet    = ff_pcm_read_packet,
 };
