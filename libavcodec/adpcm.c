ab3e32095f1531e3efa401a7384dd16f6feec0d8
Paul B Mahol <onemda@gmail.com>
2025-10-19 20:11:32 +0000

    avcodec/adpcm: allow >2 channels for adpcm_ima_wav_mono

--- libavcodec/adpcm.c
+++ libavcodec/adpcm.c
@@ -445,6 +445,7 @@ static av_cold int adpcm_decode_init(AVCodecContext * avctx)
         if (avctx->ch_layout.nb_channels <= 0)
             return AVERROR_INVALIDDATA;
         break;
+    case AV_CODEC_ID_ADPCM_IMA_WAV_MONO:
     case AV_CODEC_ID_ADPCM_IMA_DAT4:
     case AV_CODEC_ID_ADPCM_THP:
     case AV_CODEC_ID_ADPCM_THP_LE:
