16c822dd042fe12993dce285a0f5b1f16db084e2
Paul B Mahol <onemda@gmail.com>
2025-10-23 14:59:09 +0000

    avformat: add jstm demuxer

---
+++ libavformat/jstm.c
@@ -0,0 +1,94 @@
+/*
+ * JSTM demuxer
+ * Copyright (c) 2025 Paul B Mahol
+ *
+ * This file is part of Librempeg
+ *
+ * Librempeg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Librempeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Librempeg; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "libavutil/intreadwrite.h"
+#include "avformat.h"
+#include "demux.h"
+#include "internal.h"
+#include "pcm.h"
+
+static int read_probe(const AVProbeData *p)
+{
+    if (AV_RB32(p->buf) != MKBETAG('J','S','T','M'))
+        return 0;
+    if (p->buf_size < 12)
+        return 0;
+    if (AV_RL16(p->buf + 4) == 0)
+        return 0;
+    if (AV_RL16(p->buf + 4) != AV_RL16(p->buf + 6))
+        return 0;
+    if ((int)AV_RL32(p->buf + 8) <= 0)
+        return 0;
+
+    return AVPROBE_SCORE_MAX;
+}
+
+static int read_header(AVFormatContext *s)
+{
+    AVIOContext *pb = s->pb;
+    int channels, rate;
+    AVStream *st;
+
+    avio_skip(pb, 4);
+    channels = avio_rl16(pb);
+    avio_skip(pb, 2);
+    rate = avio_rl32(pb);
+    if (channels <= 0 || rate <= 0)
+        return AVERROR_INVALIDDATA;
+
+    st = avformat_new_stream(s, NULL);
+    if (!st)
+        return AVERROR(ENOMEM);
+
+    st->start_time = 0;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->ch_layout.nb_channels = channels;
+    st->codecpar->block_align = 2 * channels;
+    st->codecpar->codec_id = AV_CODEC_ID_PCM_S16LE;
+
+    avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+    avio_seek(pb, 0x20, SEEK_SET);
+
+    return 0;
+}
+
+static int read_packet(AVFormatContext *s, AVPacket *pkt)
+{
+    int ret;
+
+    ret = ff_pcm_read_packet(s, pkt);
+    for (int i = 0; i < pkt->size; i++)
+        pkt->data[i] ^= 0x5A;
+
+    return ret;
+}
+
+const FFInputFormat ff_jstm_demuxer = {
+    .p.name         = "jstm",
+    .p.long_name    = NULL_IF_CONFIG_SMALL("JSTM PS2"),
+    .p.extensions   = "jstm,stm",
+    .read_probe     = read_probe,
+    .read_header    = read_header,
+    .read_packet    = read_packet,
+    .read_seek      = ff_pcm_read_seek,
+};
