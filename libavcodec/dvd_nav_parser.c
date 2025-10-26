ac4d88d173f6da1096f867837610da54218ff196
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2025-10-26 09:33:25 +0100

    avcodec: Remove unnecessary parser.h inclusions
    It only contains declarations for some auxiliary functions for parsing
    that parsers that only work with complete packets don't need.
    
    Reviewed-by: James Almer <jamrial@gmail.com>
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/dvd_nav_parser.c
+++ libavcodec/dvd_nav_parser.c
@@ -19,8 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #include "avcodec.h"
-#include "get_bits.h"
-#include "parser.h"
+#include "libavutil/intreadwrite.h"
 
 #define PCI_SIZE  980
 #define DSI_SIZE 1018
--- libavcodec/vp9_parser.c
+++ libavcodec/vp9_parser.c
@@ -21,9 +21,9 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "libavutil/intreadwrite.h"
-#include "libavcodec/get_bits.h"
-#include "parser.h"
+#include "avcodec.h"
+#include "get_bits.h"
+#include "parser_internal.h"
 
 static int parse(AVCodecParserContext *ctx,
                  AVCodecContext *avctx,
