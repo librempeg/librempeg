565a75f10a9c2dc94894ea88830ab340bf343339
Paul B Mahol <onemda@gmail.com>
2025-11-01 08:47:09 +0000

    avformat/dcstr: refactor code

--- libavformat/dcstr.c
+++ libavformat/dcstr.c
@@ -24,7 +24,7 @@
 #include "demux.h"
 #include "internal.h"
 
-static int dcstr_probe(const AVProbeData *p)
+static int read_probe(const AVProbeData *p)
 {
     if ((int32_t)AV_RL32(p->buf) <= 0)
         return 0;
@@ -38,60 +38,61 @@ static int dcstr_probe(const AVProbeData *p)
     return AVPROBE_SCORE_MAX;
 }
 
-static int dcstr_read_header(AVFormatContext *s)
+static int read_header(AVFormatContext *s)
 {
+    int mult, align, channels, rate;
     AVIOContext *pb = s->pb;
-    unsigned codec, align;
-    int mult;
+    int64_t duration = 0;
+    unsigned codec;
     AVStream *st;
+
+    channels = avio_rl32(pb);
+    rate = avio_rl32(pb);
+    codec = avio_rl32(pb);
+    align = avio_rl32(pb);
+    avio_skip(pb, 4);
+    if (codec == 4)
+        duration = avio_rl32(pb);
+    else
+        avio_skip(pb, 4);
+    mult = avio_rl32(pb);
+    if (channels <= 0 || mult <= 0 || mult > INT_MAX/channels)
+        return AVERROR_INVALIDDATA;
+    channels *= mult;
+    if (rate <= 0 || align <= 0 || align > INT_MAX/channels)
+        return AVERROR_INVALIDDATA;
+
+    switch (codec) {
+    case  4: codec = AV_CODEC_ID_ADPCM_AICA;       break;
+    case  8: codec = AV_CODEC_ID_PCM_U8;           break;
+    case 16: codec = AV_CODEC_ID_PCM_S16LE_PLANAR; break;
+    default: avpriv_request_sample(s, "codec %X", codec);
+             return AVERROR_PATCHWELCOME;
+    }
 
     st = avformat_new_stream(s, NULL);
     if (!st)
         return AVERROR(ENOMEM);
 
-    st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->ch_layout.nb_channels = avio_rl32(pb);
-    if (st->codecpar->ch_layout.nb_channels <= 0)
-        return AVERROR_INVALIDDATA;
-    st->codecpar->sample_rate = avio_rl32(pb);
-    if (st->codecpar->sample_rate <= 0)
-        return AVERROR_INVALIDDATA;
-    codec                  = avio_rl32(pb);
-    align                  = avio_rl32(pb);
-    avio_skip(pb, 4);
     st->start_time = 0;
-    if (codec == 4)
-        st->duration = avio_rl32(pb);
-    else
-        avio_skip(pb, 4);
-    mult                   = avio_rl32(pb);
-    if (st->codecpar->ch_layout.nb_channels <= 0 || mult <= 0 ||
-        mult > INT_MAX / st->codecpar->ch_layout.nb_channels) {
-        av_log(s, AV_LOG_ERROR, "invalid number of channels %d x %d\n",
-               st->codecpar->ch_layout.nb_channels, mult);
-        return AVERROR_INVALIDDATA;
-    }
-    st->codecpar->ch_layout.nb_channels *= mult;
-    if (!align || align > INT_MAX / st->codecpar->ch_layout.nb_channels)
-        return AVERROR_INVALIDDATA;
-    st->codecpar->block_align = align * st->codecpar->ch_layout.nb_channels;
+    if (duration)
+        st->duration = duration;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->ch_layout.nb_channels = channels;
+    st->codecpar->codec_id = codec;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->block_align = align * channels;
 
-    switch (codec) {
-    case  4: st->codecpar->codec_id = AV_CODEC_ID_ADPCM_AICA;       break;
-    case 16: st->codecpar->codec_id = AV_CODEC_ID_PCM_S16LE_PLANAR; break;
-    default: avpriv_request_sample(s, "codec %X", codec);
-             return AVERROR_PATCHWELCOME;
-    }
-
-    avio_skip(pb, 0x800 - avio_tell(pb));
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+    avio_seek(pb, 0x800, SEEK_SET);
 
     return 0;
 }
 
-static int dcstr_read_packet(AVFormatContext *s, AVPacket *pkt)
+static int read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    AVCodecParameters *par    = s->streams[0]->codecpar;
+    AVCodecParameters *par = s->streams[0]->codecpar;
     return av_get_packet(s->pb, pkt, par->block_align);
 }
 
@@ -100,7 +101,7 @@ const FFInputFormat ff_dcstr_demuxer = {
     .p.long_name    = NULL_IF_CONFIG_SMALL("Sega DC STR"),
     .p.extensions   = "str",
     .p.flags        = AVFMT_GENERIC_INDEX | AVFMT_NO_BYTE_SEEK | AVFMT_NOBINSEARCH,
-    .read_probe     = dcstr_probe,
-    .read_header    = dcstr_read_header,
-    .read_packet    = dcstr_read_packet,
+    .read_probe     = read_probe,
+    .read_header    = read_header,
+    .read_packet    = read_packet,
 };
