librempeg/librempeg

commit 9876643749407ea34c8e44ca7d72fa22ec47119d

avcodec/tableprint_vlc: Fix build failure with hardcoded tables
Broken since 2bb95c262fcb2df239197971e81367967ee88186.

libavcodec/tableprint_vlc.h
@@ -29,6 +29,7 @@
 #define ff_tlog(a, ...) while(0)
 #define AVUTIL_MEM_H
 #define av_malloc(s) NULL
+#define av_mallocz(s) NULL
 #define av_malloc_array(a, b) NULL
 #define av_realloc_f(p, o, n) NULL
 #define av_free(p) while(0)

commit 8ff2b78667628082be4e0c6608660e87b64ea1a7

avcodec/tableprint_vlc: Remove unused av_malloc define
The only usage was removed.
Fixes: 9876643749407ea34c8e44ca7d72fa22ec47119d

libavcodec/tableprint_vlc.h
@@ -28,7 +28,6 @@
 #define ff_dlog(a, ...) while(0)
 #define ff_tlog(a, ...) while(0)
 #define AVUTIL_MEM_H
-#define av_malloc(s) NULL
 #define av_mallocz(s) NULL
 #define av_malloc_array(a, b) NULL
 #define av_realloc_f(p, o, n) NULL