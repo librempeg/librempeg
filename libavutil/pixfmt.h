149620f987a460a0fa8d52ed0f1c78767613518d
Lynne <dev@lynne.ee>
2025-11-06 18:52:17 +0100

    Revert "lavu/pixfmt: add grayscale 10 and 12-bit MSB padded variants"
    This reverts commit ed5b694ebeb4af5669f638d9045efe1f138bc538.

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -497,11 +497,6 @@ enum AVPixelFormat {
     AV_PIX_FMT_GBRP12MSBBE,    ///< planar GBR 4:4:4 36bpp, lowest bits zero, big-endian
     AV_PIX_FMT_GBRP12MSBLE,    ///< planar GBR 4:4:4 36bpp, lowest bits zero, little-endian
 
-    AV_PIX_FMT_GRAY10MSBBE,   ///<        Y        , 10bpp, lowest bits zero, big-endian
-    AV_PIX_FMT_GRAY10MSBLE,   ///<        Y        , 10bpp, lowest bits zero, little-endian
-    AV_PIX_FMT_GRAY12MSBBE,   ///<        Y        , 12bpp, lowest bits zero, big-endian
-    AV_PIX_FMT_GRAY12MSBLE,   ///<        Y        , 12bpp, lowest bits zero, little-endian
-
     AV_PIX_FMT_NB         ///< number of pixel formats, DO NOT USE THIS if you want to link with shared libav* because the number of formats might differ between versions
 };
 
@@ -554,8 +549,6 @@ enum AVPixelFormat {
 #define AV_PIX_FMT_YUV422P16 AV_PIX_FMT_NE(YUV422P16BE, YUV422P16LE)
 #define AV_PIX_FMT_YUV444P16 AV_PIX_FMT_NE(YUV444P16BE, YUV444P16LE)
 
-#define AV_PIX_FMT_GRAY10MSB AV_PIX_FMT_NE(GRAY10MSBBE, GRAY10MSBLE)
-#define AV_PIX_FMT_GRAY12MSB AV_PIX_FMT_NE(GRAY12MSBBE, GRAY12MSBLE)
 #define AV_PIX_FMT_YUV444P10MSB AV_PIX_FMT_NE(YUV444P10MSBBE, YUV444P10MSBLE)
 #define AV_PIX_FMT_YUV444P12MSB AV_PIX_FMT_NE(YUV444P12MSBBE, YUV444P12MSBLE)
 
