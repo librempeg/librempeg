0a9302293c5239466cd08dff00c0a0e9716a1307
Paul B Mahol <onemda@gmail.com>
2025-12-02 08:58:22 +0000

    avfilter/af_aquant: rework noise-shaper

--- libavfilter/af_aquant.c
+++ libavfilter/af_aquant.c
@@ -18,6 +18,8 @@
 
 #include "libavutil/channel_layout.h"
 #include "libavutil/common.h"
+#include "libavutil/ffmath.h"
+#include "libavutil/mem.h"
 #include "libavutil/opt.h"
 
 #include "audio.h"
@@ -31,9 +33,13 @@ typedef struct AudioQuantContext {
     int bits;
     int noise_shaper;
     int precision;
+    int channels;
 
-    AVFrame *frame[2];
+    AVFrame *in;
 
+    void *state;
+
+    int (*init_state)(AVFilterContext *ctx);
     int (*filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs);
 } AudioQuantContext;
 
@@ -70,119 +76,39 @@ static int query_formats(const AVFilterContext *ctx,
 
 static int activate(AVFilterContext *ctx)
 {
+    AVFilterLink *outlink = ctx->outputs[0];
+    AVFilterLink *inlink = ctx->inputs[0];
     AudioQuantContext *s = ctx->priv;
+    AVFrame *out, *in;
+    int ret;
 
-    FF_FILTER_FORWARD_STATUS_BACK_ALL(ctx->outputs[0], ctx);
-    if (s->noise_shaper)
-        FF_FILTER_FORWARD_STATUS_BACK_ALL(ctx->outputs[1], ctx);
+    FF_FILTER_FORWARD_STATUS_BACK_ALL(outlink, ctx);
 
-    if (!s->frame[0]) {
-        int ret = ff_inlink_consume_frame(ctx->inputs[0], &s->frame[0]);
-        if (ret < 0)
-            return ret;
-    }
+    ret = ff_inlink_consume_frame(inlink, &in);
+    if (ret < 0)
+        return ret;
 
-    if (s->noise_shaper && s->frame[0] && !s->frame[1]) {
-        int ret = ff_inlink_consume_samples(ctx->inputs[1],
-                                            s->frame[0]->nb_samples,
-                                            s->frame[0]->nb_samples,
-                                            &s->frame[1]);
-        if (ret < 0)
-            return ret;
-    }
-
-    if (!s->noise_shaper && s->frame[0]) {
-        AVFrame *out;
-
-        out = ff_get_audio_buffer(ctx->outputs[0], s->frame[0]->nb_samples);
+    if (ret > 0) {
+        out = ff_get_audio_buffer(outlink, in->nb_samples);
         if (!out) {
-            av_frame_free(&s->frame[0]);
+            av_frame_free(&in);
             return AVERROR(ENOMEM);
         }
-        av_frame_copy_props(out, s->frame[0]);
+        av_frame_copy_props(out, in);
 
+        s->in = in;
         ff_filter_execute(ctx, s->filter_channels, out, NULL,
-                          FFMIN(ctx->outputs[0]->ch_layout.nb_channels, ff_filter_get_nb_threads(ctx)));
+                          FFMIN(outlink->ch_layout.nb_channels, ff_filter_get_nb_threads(ctx)));
 
-        ff_graph_frame_free(ctx, &s->frame[0]);
+        ff_graph_frame_free(ctx, &in);
 
-        return ff_filter_frame(ctx->outputs[0], out);
-    } else if (s->frame[0] && s->frame[1]) {
-        AVFrame *out[2];
-        int ret;
-
-        out[0] = ff_get_audio_buffer(ctx->outputs[0], s->frame[0]->nb_samples);
-        out[1] = ff_get_audio_buffer(ctx->outputs[0], s->frame[0]->nb_samples);
-        if (!out[0] || !out[1]) {
-            av_frame_free(&out[0]);
-            av_frame_free(&out[1]);
-            av_frame_free(&s->frame[0]);
-            av_frame_free(&s->frame[1]);
-            return AVERROR(ENOMEM);
-        }
-        av_frame_copy_props(out[0], s->frame[0]);
-        av_frame_copy_props(out[1], s->frame[1]);
-
-        ff_filter_execute(ctx, s->filter_channels, out, NULL,
-                          FFMIN(ctx->outputs[0]->ch_layout.nb_channels, ff_filter_get_nb_threads(ctx)));
-
-        ff_graph_frame_free(ctx, &s->frame[0]);
-        ff_graph_frame_free(ctx, &s->frame[1]);
-
-        ret = ff_filter_frame(ctx->outputs[0], out[0]);
-        if (ret < 0) {
-            av_frame_free(&out[1]);
-            return ret;
-        }
-        return ff_filter_frame(ctx->outputs[1], out[1]);
-    } else if (s->noise_shaper && s->frame[0] && !s->frame[1]) {
-        AVFrame *out[2];
-        int ret;
-
-        out[0] = ff_get_audio_buffer(ctx->outputs[0], s->frame[0]->nb_samples);
-        out[1] = ff_get_audio_buffer(ctx->outputs[0], s->frame[0]->nb_samples);
-        if (!out[0] || !out[1]) {
-            av_frame_free(&out[0]);
-            av_frame_free(&out[1]);
-            av_frame_free(&s->frame[0]);
-            return AVERROR(ENOMEM);
-        }
-        av_frame_copy_props(out[0], s->frame[0]);
-
-        ff_filter_execute(ctx, s->filter_channels, out, NULL,
-                          FFMIN(ctx->outputs[0]->ch_layout.nb_channels, ff_filter_get_nb_threads(ctx)));
-
-        ff_graph_frame_free(ctx, &s->frame[0]);
-
-        ret = ff_filter_frame(ctx->outputs[0], out[0]);
-        if (ret < 0) {
-            av_frame_free(&out[1]);
-            return ret;
-        }
-        return ff_filter_frame(ctx->outputs[1], out[1]);
+        return ff_filter_frame(outlink, out);
     }
 
-    for (int i = 0; i < 1 + s->noise_shaper; i++) {
-        int64_t pts;
-        int status;
+    FF_FILTER_FORWARD_STATUS(inlink, outlink);
+    FF_FILTER_FORWARD_WANTED(outlink, inlink);
 
-        if (ff_inlink_acknowledge_status(ctx->inputs[i], &status, &pts)) {
-            ff_outlink_set_status(ctx->outputs[0], status, pts);
-            if (s->noise_shaper)
-                ff_outlink_set_status(ctx->outputs[1], status, pts);
-            return 0;
-        }
-    }
-
-    if (ff_outlink_frame_wanted(ctx->outputs[0])) {
-        for (int i = 0; i < 1 + s->noise_shaper; i++) {
-            if (s->frame[i])
-                continue;
-            ff_inlink_request_frame(ctx->inputs[i]);
-            return 0;
-        }
-    }
-    return 0;
+    return FFERROR_NOT_READY;
 }
 
 #define DEPTH 32
@@ -197,52 +123,29 @@ static int config_output(AVFilterLink *outlink)
     AVFilterContext *ctx = outlink->src;
     AudioQuantContext *s = ctx->priv;
 
-    if (FF_OUTLINK_IDX(outlink))
-        return 0;
+    s->channels = outlink->ch_layout.nb_channels;
 
     switch (outlink->format) {
     case AV_SAMPLE_FMT_DBLP:
+        s->init_state = init_state_double;
         s->filter_channels = s->noise_shaper ? shaper_channels_double : filter_channels_double;
         break;
     case AV_SAMPLE_FMT_FLTP:
+        s->init_state = init_state_float;
         s->filter_channels = s->noise_shaper ? shaper_channels_float : filter_channels_float;
         break;
     default:
         return AVERROR_BUG;
     }
 
-    return 0;
+    return s->init_state(ctx);
 }
 
-static av_cold int init(AVFilterContext *ctx)
+static av_cold void uninit(AVFilterContext *ctx)
 {
     AudioQuantContext *s = ctx->priv;
 
-    if (s->noise_shaper) {
-        AVFilterPad pad;
-        int ret;
-
-        pad = (AVFilterPad) {
-            .name = "filtered_error",
-            .type = AVMEDIA_TYPE_AUDIO,
-        };
-
-        ret = ff_append_inpad(ctx, &pad);
-        if (ret < 0)
-            return ret;
-
-        pad = (AVFilterPad) {
-            .name = "unfiltered_error",
-            .type = AVMEDIA_TYPE_AUDIO,
-            .config_props = config_output,
-        };
-
-        ret = ff_append_outpad(ctx, &pad);
-        if (ret < 0)
-            return ret;
-    }
-
-    return 0;
+    av_freep(&s->state);
 }
 
 static const AVFilterPad outputs[] = {
@@ -258,14 +161,12 @@ const FFFilter ff_af_aquant = {
     .p.description  = NULL_IF_CONFIG_SMALL("Apply Quantization with optional Noise-Shaping to audio stream."),
     .p.priv_class   = &aquant_class,
     .priv_size      = sizeof(AudioQuantContext),
-    .init           = init,
     .activate       = activate,
+    .uninit         = uninit,
     FILTER_INPUTS(ff_audio_default_filterpad),
     FILTER_OUTPUTS(outputs),
     FILTER_QUERY_FUNC2(query_formats),
-    .p.flags        = AVFILTER_FLAG_DYNAMIC_OUTPUTS |
-                      AVFILTER_FLAG_DYNAMIC_INPUTS |
-                      AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL |
+    .p.flags        = AVFILTER_FLAG_SUPPORT_TIMELINE_INTERNAL |
                       AVFILTER_FLAG_SLICE_THREADS,
     .process_command = ff_filter_process_command,
 };
--- libavfilter/aquant_template.c
+++ libavfilter/aquant_template.c
@@ -35,43 +35,116 @@
 #define fn2(a,b)   fn3(a,b)
 #define fn(a)      fn2(a, SAMPLE_FORMAT)
 
+typedef struct fn(StateContext) {
+    ftype a[3];
+    ftype b[3];
+    ftype c[4];
+    ftype quant_err;
+} fn(StateContext);
+
+static int fn(init_state)(AVFilterContext *ctx)
+{
+    AudioQuantContext *s = ctx->priv;
+    fn(StateContext) *state;
+
+    s->state = av_calloc(s->channels, sizeof(*state));
+    if (!s->state)
+        return AVERROR(ENOMEM);
+
+    state = s->state;
+    for (int ch = 0; ch < s->channels; ch++) {
+        fn(StateContext) *stc = &state[ch];
+        const ftype w0 = M_PI * 0.1;
+        const ftype cos_w0 = cos(w0);
+        const ftype sin_w0 = sin(w0);
+        ftype a[3], b[3], bb[3];
+        ftype alpha;
+
+        alpha = sin_w0 / 0.02;
+
+        a[0] =  1 + alpha;
+        a[1] = -2 * cos_w0;
+        a[2] =  1 - alpha;
+        b[0] =  1;
+        b[1] = -2 * cos_w0;
+        b[2] =  1;
+
+        a[1] /= a[0];
+        a[2] /= a[0];
+        b[0] /= a[0];
+        b[1] /= a[0];
+        b[2] /= a[0];
+        a[0] /= a[0];
+
+        bb[0] = b[0]/b[0];
+        bb[1] = b[1]/b[0];
+        bb[2] = b[2]/b[0];
+
+        bb[1] -= a[1];
+        bb[2] -= a[2];
+
+        stc->a[0] = a[0];
+        stc->a[1] = a[1];
+        stc->a[2] = a[2];
+        stc->b[0] = bb[0];
+        stc->b[1] = bb[1];
+        stc->b[2] = bb[2];
+    }
+
+    return 0;
+}
+
 static int fn(shaper_channels)(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
 {
     const int is_disabled = ff_filter_disabled(ctx);
     AudioQuantContext *s = ctx->priv;
-    AVFrame **frames = s->frame;
-    AVFrame **outs = arg;
-    AVFrame *out0 = outs[0];
-    AVFrame *out1 = outs[1];
-    const int start = (out0->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (out0->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
-    const int nb_samples = out0->nb_samples;
+    AVFrame *in = s->in;
+    AVFrame *out = arg;
+    const int start = (in->ch_layout.nb_channels * jobnr) / nb_jobs;
+    const int end = (in->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int nb_samples = in->nb_samples;
     const ftype factor = 1U << (s->bits-1);
     const ftype scale = F(1.0) / factor;
 
     for (int c = start; c < end; c++) {
-        const ftype *input = (const ftype *)frames[0]->extended_data[c];
-        const ftype *filter = frames[1] ? (const ftype *)frames[1]->extended_data[c] : NULL;
-        ftype *output = (ftype *)out0->extended_data[c];
-        ftype *error = (ftype *)out1->extended_data[c];
+        const ftype *input = (const ftype *)in->extended_data[c];
+        ftype *output = (ftype *)out->extended_data[c];
 
         if (is_disabled) {
             for (int n = 0; n < nb_samples; n++)
                 output[n] = input[n];
-        } else if (filter) {
-            for (int n = 0; n < nb_samples; n++) {
-                const ftype wanted = input[n] - filter[n];
-
-                output[n] = ROUND(wanted * factor) * scale;
-                error[n] = output[n] - wanted;
-            }
         } else {
-            for (int n = 0; n < nb_samples; n++) {
-                const ftype wanted = input[n];
+            fn(StateContext) *state = s->state;
+            fn(StateContext) *stc = &state[c];
+            ftype *fcache = stc->c;
+            const ftype *a = stc->a;
+            const ftype *b = stc->b;
+            const ftype a1 = -a[1];
+            const ftype a2 = -a[2];
+            const ftype b1 = b[1];
+            const ftype b2 = b[2];
+            ftype w1 = fcache[0];
+            ftype w2 = fcache[1];
+            ftype quant_err = stc->quant_err;
 
-                output[n] = ROUND(wanted * factor) * scale;
-                error[n] = output[n] - wanted;
+            for (int n = 0; n < nb_samples; n++) {
+                ftype filter_out, quant_out, quant_in, in = input[n];
+
+                filter_out = quant_err + w1;
+
+                w1 = b1 * quant_err + w2 + a1 * filter_out;
+                w2 = b2 * quant_err + a2 * filter_out;
+
+                quant_in = in + filter_out;
+                quant_out = ROUND(quant_in * factor) * scale;
+                quant_err = quant_out - quant_in;
+                output[n] = quant_out;
             }
+
+            stc->quant_err = isnormal(quant_err) ? quant_err : F(0.0);
+
+            fcache[0] = isnormal(w1) ? w1 : F(0.0);
+            fcache[1] = isnormal(w2) ? w2 : F(0.0);
         }
     }
 
@@ -82,7 +155,7 @@ static int fn(filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int n
 {
     const int is_disabled = ff_filter_disabled(ctx);
     AudioQuantContext *s = ctx->priv;
-    AVFrame **frames = s->frame;
+    AVFrame *in = s->in;
     AVFrame *out = arg;
     const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
     const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
@@ -91,7 +164,7 @@ static int fn(filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int n
     const ftype scale = F(1.0) / factor;
 
     for (int c = start; c < end; c++) {
-        const ftype *input = (const ftype *)frames[0]->extended_data[c];
+        const ftype *input = (const ftype *)in->extended_data[c];
         ftype *output = (ftype *)out->extended_data[c];
 
         if (is_disabled) {
