39d69bf862ab9407dd28275a4e23f9f1cf0992e7
Paul B Mahol <onemda@gmail.com>
2026-02-16 07:38:09 +0000

    avformat/csmp: remove unused headers

--- libavformat/csmp.c
+++ libavformat/csmp.c
@@ -19,9 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "libavutil/bswap.h"
 #include "libavutil/intreadwrite.h"
-#include "libavutil/mem.h"
 #include "avformat.h"
 #include "demux.h"
 #include "internal.h"
