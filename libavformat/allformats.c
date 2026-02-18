2a56a0897d7da9efaf3601d6a43d0756ae164916
modusc896d352 <RMP_acidkaneE3A@protonmail.com>
2026-02-18 01:07:10 -0300

    avformat: add SquareSoft PS2 DAT demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -570,6 +570,7 @@ extern const FFInputFormat  ff_spcm_demuxer;
 extern const FFOutputFormat ff_spx_muxer;
 extern const FFInputFormat  ff_spdif_demuxer;
 extern const FFOutputFormat ff_spdif_muxer;
+extern const FFInputFormat  ff_square_ps2fmv_demuxer;
 extern const FFInputFormat  ff_srt_demuxer;
 extern const FFOutputFormat ff_srt_muxer;
 extern const FFInputFormat  ff_ssm_demuxer;
