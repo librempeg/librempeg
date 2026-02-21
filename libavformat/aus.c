3fe88f84da676a675a929c259600eb433074834c
Paul B Mahol <onemda@gmail.com>
2026-02-21 13:26:22 +0000

    avformat/aus: refactor packet reading

--- libavformat/aus.c
+++ libavformat/aus.c
@@ -25,6 +25,7 @@
 #include "avformat.h"
 #include "demux.h"
 #include "internal.h"
+#include "pcm.h"
 
 static int read_probe(const AVProbeData *p)
 {
@@ -97,18 +98,6 @@ static int read_header(AVFormatContext *s)
     return 0;
 }
 
-static int read_packet(AVFormatContext *s, AVPacket *pkt)
-{
-    AVIOContext *pb = s->pb;
-    int ret;
-
-    ret = av_get_packet(pb, pkt, s->streams[0]->codecpar->block_align);
-    pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
-    pkt->stream_index = 0;
-
-    return ret;
-}
-
 const FFInputFormat ff_aus_demuxer = {
     .p.name         = "aus",
     .p.long_name    = NULL_IF_CONFIG_SMALL("AUS (Atomic Planet)"),
@@ -116,5 +105,5 @@ const FFInputFormat ff_aus_demuxer = {
     .p.extensions   = "aus",
     .read_probe     = read_probe,
     .read_header    = read_header,
-    .read_packet    = read_packet,
+    .read_packet    = ff_pcm_read_packet,
 };
