39c9136652fb4940fb48038510cb78669e677c77
Paul B Mahol <onemda@gmail.com>
2026-02-22 08:06:32 +0000

    avformat/aif: set start_time to 0

--- libavformat/aif.c
+++ libavformat/aif.c
@@ -58,6 +58,7 @@ static int read_header(AVFormatContext *s)
     if (!st)
         return AVERROR(ENOMEM);
 
+    st->start_time = 0;
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codecpar->codec_id = AV_CODEC_ID_ADPCM_IMA_XBOX;
     st->codecpar->ch_layout.nb_channels = nb_channels;
