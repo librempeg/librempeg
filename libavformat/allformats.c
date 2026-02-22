2404796848415fc25cbd26a3817cdb360f7204d7
Paul B Mahol <onemda@gmail.com>
2026-02-22 19:40:56 +0000

    avformat: add UBIBNM demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -624,6 +624,7 @@ extern const FFInputFormat  ff_twopfs_demuxer;
 extern const FFInputFormat  ff_txd_demuxer;
 extern const FFInputFormat  ff_tty_demuxer;
 extern const FFInputFormat  ff_ty_demuxer;
+extern const FFInputFormat  ff_ubibnm_demuxer;
 extern const FFInputFormat  ff_ueba_demuxer;
 extern const FFOutputFormat ff_uncodedframecrc_muxer;
 extern const FFInputFormat  ff_usm_demuxer;
