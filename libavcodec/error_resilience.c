7037bc19759b6a23fa604be81232245245c48ded
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-02-27 16:36:00 +0100

    avcodec/error_resilience: Initialize error resilience context earlier
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/error_resilience.c
+++ libavcodec/error_resilience.c
@@ -28,6 +28,7 @@
 #include <limits.h>
 
 #include "libavutil/avassert.h"
+#include "libavutil/attributes.h"
 #include "libavutil/mem.h"
 #include "libavutil/threadprogress.h"
 
@@ -38,6 +39,14 @@
 #include "mpegutils.h"
 #include "mpegvideo.h"
 #include "threadframe.h"
+
+av_cold void ff_er_init(ERContext *const s)
+{
+    MECmpContext mecc;
+
+    ff_me_cmp_init(&mecc, s->avctx);
+    s->sad = mecc.sad[0];
+}
 
 /**
  * @param stride the number of MVs to get to the next row
@@ -795,13 +804,6 @@ void ff_er_frame_start(ERContext *s)
 {
     if (!s->avctx->error_concealment)
         return;
-
-    if (!s->mecc_inited) {
-        MECmpContext mecc;
-        ff_me_cmp_init(&mecc, s->avctx);
-        s->sad = mecc.sad[0];
-        s->mecc_inited = 1;
-    }
 
     memset(s->error_status_table, ER_MB_ERROR | VP_START | ER_MB_END,
            s->mb_stride * s->mb_height * sizeof(uint8_t));
