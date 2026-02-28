4a0f39fb1abe0353af325f8ffe7b3e2100839921
Paul B Mahol <onemda@gmail.com>
2026-02-28 15:53:35 +0000

    avformat/xwc: add OGG/Vorbis support

--- libavformat/xwc.c
+++ libavformat/xwc.c
@@ -26,6 +26,10 @@
 #include "internal.h"
 #include "pcm.h"
 
+typedef struct XWCDemuxContext {
+    AVFormatContext *ogg_ctx;
+} XWCDemuxContext;
+
 static int read_probe(const AVProbeData *p)
 {
     if (p->buf_size < 16)
@@ -48,8 +52,11 @@ static int read_header(AVFormatContext *s)
 {
     int64_t start, extra_offset, chunk_offset, duration;
     uint32_t version, codec, seek_size, chunk_size;
+    extern const FFInputFormat ff_ogg_demuxer;
+    XWCDemuxContext *xwc = s->priv_data;
     AVIOContext *pb = s->pb;
     int nb_channels, rate;
+    FFStream *sti;
     AVStream *st;
     int ret;
 
@@ -92,6 +99,39 @@ static int read_header(AVFormatContext *s)
         start = 0x800;
         rate = 44100;
         break;
+    case MKBETAG('V','O','R','B'):
+        codec = AV_CODEC_ID_VORBIS;
+        start = 0x30;
+        rate = 44100;
+
+        if (!(xwc->ogg_ctx = avformat_alloc_context()))
+            return AVERROR(ENOMEM);
+
+        if ((ret = ff_copy_whiteblacklists(xwc->ogg_ctx, s)) < 0) {
+            avformat_free_context(xwc->ogg_ctx);
+            xwc->ogg_ctx = NULL;
+
+            return ret;
+        }
+
+        xwc->ogg_ctx->flags = AVFMT_FLAG_CUSTOM_IO | AVFMT_FLAG_GENPTS;
+        xwc->ogg_ctx->ctx_flags |= AVFMTCTX_UNSEEKABLE;
+        xwc->ogg_ctx->probesize = 0;
+        xwc->ogg_ctx->max_analyze_duration = 0;
+        xwc->ogg_ctx->interrupt_callback = s->interrupt_callback;
+        xwc->ogg_ctx->pb = pb;
+        xwc->ogg_ctx->io_open = NULL;
+
+        avio_seek(pb, start, SEEK_SET);
+
+        ret = avformat_open_input(&xwc->ogg_ctx, "", &ff_ogg_demuxer.p, NULL);
+        if (ret < 0)
+            return ret;
+
+        ret = avformat_find_stream_info(xwc->ogg_ctx, NULL);
+        if (ret < 0)
+            return ret;
+        break;
     default:
         avpriv_request_sample(s, "codec %08X", codec);
         return AVERROR_PATCHWELCOME;
@@ -120,7 +160,26 @@ static int read_header(AVFormatContext *s)
         memset(st->codecpar->extradata, 0, 34);
         AV_WL16(st->codecpar->extradata, 1);
         break;
+    case AV_CODEC_ID_VORBIS:
+        st->id = xwc->ogg_ctx->streams[0]->id;
+        st->duration = xwc->ogg_ctx->streams[0]->duration;
+        st->time_base = xwc->ogg_ctx->streams[0]->time_base;
+        st->start_time = xwc->ogg_ctx->streams[0]->start_time;
+        st->pts_wrap_bits = xwc->ogg_ctx->streams[0]->pts_wrap_bits;
+
+        ret = avcodec_parameters_copy(st->codecpar, xwc->ogg_ctx->streams[0]->codecpar);
+        if (ret < 0)
+            return ret;
+
+        ret = av_dict_copy(&st->metadata, xwc->ogg_ctx->streams[0]->metadata, 0);
+        if (ret < 0)
+            return ret;
+
+        sti = ffstream(st);
+        sti->request_probe = 0;
+        break;
     }
+
     ffstream(st)->need_parsing = AVSTREAM_PARSE_FULL_RAW;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
@@ -130,12 +189,51 @@ static int read_header(AVFormatContext *s)
     return 0;
 }
 
+static int read_packet(AVFormatContext *s, AVPacket *pkt)
+{
+    XWCDemuxContext *xwc = s->priv_data;
+    int ret;
+
+    if (xwc->ogg_ctx) {
+        ret = av_read_frame(xwc->ogg_ctx, pkt);
+        pkt->stream_index = 0;
+    } else {
+        ret = ff_pcm_read_packet(s, pkt);
+    }
+
+    return ret;
+}
+
+static int read_seek(AVFormatContext *s, int stream_index,
+                     int64_t ts, int flags)
+{
+    XWCDemuxContext *xwc = s->priv_data;
+
+    if (xwc->ogg_ctx) {
+        return av_seek_frame(xwc->ogg_ctx, 0, ts, flags);
+    } else {
+        return -1;
+    }
+}
+
+static int read_close(AVFormatContext *s)
+{
+    XWCDemuxContext *xwc = s->priv_data;
+
+    avformat_close_input(&xwc->ogg_ctx);
+
+    return 0;
+}
+
 const FFInputFormat ff_xwc_demuxer = {
     .p.name         = "xwc",
     .p.long_name    = NULL_IF_CONFIG_SMALL("Starbreeze XWC"),
     .p.flags        = AVFMT_GENERIC_INDEX,
     .p.extensions   = "xwc",
+    .priv_data_size = sizeof(XWCDemuxContext),
     .read_probe     = read_probe,
     .read_header    = read_header,
-    .read_packet    = ff_pcm_read_packet,
+    .read_packet    = read_packet,
+    .read_seek      = read_seek,
+    .read_close     = read_close,
 };
