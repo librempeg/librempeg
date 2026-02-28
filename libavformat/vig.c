daac613f0bad54e82411a3b858601a1fd58431a0
Paul B Mahol <onemda@gmail.com>
2026-02-28 16:29:14 +0000

    avformat/vig: move AVStream * allocation after checks

--- libavformat/vig.c
+++ libavformat/vig.c
@@ -47,41 +47,47 @@ static int read_probe(const AVProbeData *p)
 
 static int read_header(AVFormatContext *s)
 {
-    uint32_t data_offset, data_size, loop_start, loop_end, block_size;
+    uint32_t data_size, loop_start, loop_end;
+    int rate, nb_channels, align;
     AVIOContext *pb = s->pb;
+    int64_t start_offset;
+    AVStream *st;
 
-    AVStream *st = avformat_new_stream(s, NULL);
+    avio_skip(pb, 8);
+    start_offset = avio_rl32(pb);
+    data_size = avio_rl32(pb);
+    loop_start = avio_rl32(pb);
+    loop_end = avio_rl32(pb);
+    rate = avio_rl32(pb);
+    nb_channels = avio_rl32(pb);
+    avio_skip(pb, 4);
+    align = avio_rl32(pb);
+    if (align == 0)
+        align = 0x800;
+    if (rate <= 0 || nb_channels <= 0 || align <= 0 || align > INT_MAX/nb_channels)
+        return AVERROR_INVALIDDATA;
+
+    if (loop_end > 0) {
+        av_dict_set_int(&s->metadata, "loop_start", PSB2SMP(loop_start, nb_channels), 0);
+        av_dict_set_int(&s->metadata, "loop_end", PSB2SMP(loop_end + loop_start, nb_channels), 0);
+    }
+
+    st = avformat_new_stream(s, NULL);
     if (!st)
         return AVERROR(ENOMEM);
 
     st->start_time = 0;
+    st->duration = PSB2SMP(data_size, nb_channels);
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codecpar->codec_id = AV_CODEC_ID_ADPCM_PSX;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->ch_layout.nb_channels = nb_channels;
+    st->codecpar->block_align = align * nb_channels;
 
-    avio_skip(pb, 8);
-    data_offset = avio_rl32(pb);
-    data_size = avio_rl32(pb);
-
-    loop_start = avio_rl32(pb);
-    loop_end = avio_rl32(pb);
-
-    st->codecpar->sample_rate = avio_rl32(pb);
-    st->codecpar->ch_layout.nb_channels = avio_rl32(pb);
-    avio_skip(pb, 4);
-
-    st->duration = PSB2SMP(data_size, st->codecpar->ch_layout.nb_channels);
-    if (loop_end > 0) {
-        av_dict_set_int(&s->metadata, "loop_start", PSB2SMP(loop_start, st->codecpar->ch_layout.nb_channels), 0);
-        av_dict_set_int(&s->metadata, "loop_end", PSB2SMP(loop_end + loop_start, st->codecpar->ch_layout.nb_channels), 0);
-    }
-
-    block_size = avio_rl32(pb);
-    if (block_size == 0 || st->codecpar->ch_layout.nb_channels == 1)
-        block_size = 0x800;
-    st->codecpar->block_align = block_size * st->codecpar->ch_layout.nb_channels;
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
-    avio_seek(pb, data_offset, SEEK_SET);
+    avio_seek(pb, start_offset, SEEK_SET);
+
     return 0;
 }
 
