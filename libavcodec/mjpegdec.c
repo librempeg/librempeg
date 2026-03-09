00531f1943b871dd404cae462b07a23aa4e2a030
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-03-09 06:16:32 +0100

    avcodec: Remove FF_API_CODEC_PROPS
    Deprecated since 2025-01-05.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -2442,11 +2442,6 @@ redo_for_pal8:
             break;
         case SOF3:
             avctx->profile     = AV_PROFILE_MJPEG_HUFFMAN_LOSSLESS;
-#if FF_API_CODEC_PROPS
-FF_DISABLE_DEPRECATION_WARNINGS
-            avctx->properties |= FF_CODEC_PROPERTY_LOSSLESS;
-FF_ENABLE_DEPRECATION_WARNINGS
-#endif
             s->lossless    = 1;
             s->ls          = 0;
             s->progressive = 0;
@@ -2455,11 +2450,6 @@ FF_ENABLE_DEPRECATION_WARNINGS
             break;
         case SOF55:
             avctx->profile     = AV_PROFILE_MJPEG_JPEG_LS;
-#if FF_API_CODEC_PROPS
-FF_DISABLE_DEPRECATION_WARNINGS
-            avctx->properties |= FF_CODEC_PROPERTY_LOSSLESS;
-FF_ENABLE_DEPRECATION_WARNINGS
-#endif
             s->lossless    = 1;
             s->ls          = 1;
             s->progressive = 0;
