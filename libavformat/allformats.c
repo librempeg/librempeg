bdd3895cebb369237b827ea7514a0fea5121eb3a
Paul B Mahol <onemda@gmail.com>
2026-03-14 20:57:53 +0000

    avformat: add RCAVOC demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -508,6 +508,7 @@ extern const FFInputFormat  ff_qon_demuxer;
 extern const FFInputFormat  ff_r3d_demuxer;
 extern const FFInputFormat  ff_rawvideo_demuxer;
 extern const FFOutputFormat ff_rawvideo_muxer;
+extern const FFInputFormat  ff_rcavoc_demuxer;
 extern const FFInputFormat  ff_rcwt_demuxer;
 extern const FFOutputFormat ff_rcwt_muxer;
 extern const FFInputFormat  ff_realtext_demuxer;
