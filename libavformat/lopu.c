d4e80c54037e9039e48517df68142cac16931b80
Paul B Mahol <onemda@gmail.com>
2026-03-15 17:27:18 +0000

    avformat/lopu: improve probe()

--- libavformat/lopu.c
+++ libavformat/lopu.c
@@ -30,12 +30,17 @@ typedef struct LOPUDemuxContext {
 
 static int lopu_probe(const AVProbeData *p)
 {
-    if (AV_RL32(p->buf) == MKTAG('L','O','P','U') &&
-        AV_RL32(p->buf+8) > 0 &&
-        AV_RL16(p->buf+12) > 0)
-        return AVPROBE_SCORE_MAX/2;
+    if (AV_RL32(p->buf) != MKTAG('L','O','P','U'))
+        return 0;
 
-    return 0;
+    if (p->buf_size < 16)
+        return 0;
+    if ((int)AV_RL32(p->buf+8) <= 0)
+        return 0;
+    if ((int)AV_RL32(p->buf+12) <= 0)
+        return 0;
+
+    return AVPROBE_SCORE_MAX;
 }
 
 static int lopu_read_header(AVFormatContext *s)
