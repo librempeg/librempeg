310d45e5ad95c0d5ec4d4961d57a66861a1378bc
Paul B Mahol <onemda@gmail.com>
2026-04-07 07:40:58 +0000

    avformat/pdadec: remove extra parentheses

--- libavformat/pdadec.c
+++ libavformat/pdadec.c
@@ -82,7 +82,7 @@ static int read_header(AVFormatContext *s)
         par->block_align = avio_rl16(pb);
         if (par->block_align <= 0)
             return AVERROR_INVALIDDATA;
-        par->bit_rate = (8LL * par->block_align * par->sample_rate / av_get_audio_frame_duration2(par, par->block_align));
+        par->bit_rate = 8LL * par->block_align * par->sample_rate / av_get_audio_frame_duration2(par, par->block_align);
     } else {
         par->block_align = par->bits_per_coded_sample *
                            par->ch_layout.nb_channels / 8;
