ab2c393cdad810ac65b60315192b255030cbe1ca
Paul B Mahol <onemda@gmail.com>
2026-04-09 21:18:14 +0000

    avfilter/af_bs2b: use better call in query()

--- libavfilter/af_bs2b.c
+++ libavfilter/af_bs2b.c
@@ -113,11 +113,7 @@ static int query_formats(const AVFilterContext *ctx,
     if (ret < 0)
         return ret;
 
-    ret = ff_set_common_formats_from_list2(ctx, cfg_in, cfg_out, sample_fmts);
-    if (ret < 0)
-        return ret;
-
-    return 0;
+    return ff_set_sample_formats_from_list2(ctx, cfg_in, cfg_out, sample_fmts);
 }
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
