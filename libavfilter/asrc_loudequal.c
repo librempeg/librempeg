9ea15ea4b06e06689238e4e41505f9756bf5fe2b
Paul B Mahol <onemda@gmail.com>
2026-04-26 21:02:11 +0000

    avfilter/asrc_*: use better call in query()

--- libavfilter/asrc_loudequal.c
+++ libavfilter/asrc_loudequal.c
@@ -306,7 +306,7 @@ static int query_formats(const AVFilterContext *ctx,
     static const enum AVSampleFormat sample_fmts[] = { AV_SAMPLE_FMT_FLT,
                                                        AV_SAMPLE_FMT_NONE };
 
-    int ret = ff_set_common_formats_from_list2(ctx, cfg_in, cfg_out, sample_fmts);
+    int ret = ff_set_sample_formats_from_list2(ctx, cfg_in, cfg_out, sample_fmts);
     if (ret < 0)
         return ret;
 
