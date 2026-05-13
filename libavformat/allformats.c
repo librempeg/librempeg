01634c6c6a95dacebd78ea926632f07f2f76bce6
Paul B Mahol <onemda@gmail.com>
2026-05-13 20:22:40 +0000

    avformat: add wmw demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -709,6 +709,7 @@ extern const FFInputFormat  ff_webvtt_demuxer;
 extern const FFOutputFormat ff_webvtt_muxer;
 extern const FFInputFormat  ff_wiibgm_demuxer;
 extern const FFInputFormat  ff_wiibns_demuxer;
+extern const FFInputFormat  ff_wmw_demuxer;
 extern const FFInputFormat  ff_wsaud_demuxer;
 extern const FFOutputFormat ff_wsaud_muxer;
 extern const FFInputFormat  ff_wsd_demuxer;
