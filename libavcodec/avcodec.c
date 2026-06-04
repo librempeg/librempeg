fd9ade6684e675cca4b457819620dbbe2c2bffc6
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-06-04 23:39:48 +0200

    avcodec/codec: Remove deprecated AVCodec arrays
    They were deprecated in 3305767560a6303f474fffa3afb10c500059b455
    on 2024-09-08. Removing them allows to constify all FFCodecs;
    furthermore, it allows to use a union of a video-only and a audio-only
    structure to reduce sizeof(FFCodec).
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/avcodec.c
+++ libavcodec/avcodec.c
@@ -723,7 +723,7 @@ int avcodec_receive_frame(AVCodecContext *avctx, AVFrame *frame)
     do {                                                                    \
         if (codec->type != (allowed_type))                                  \
             return AVERROR(EINVAL);                                         \
-        const field_type *ptr = codec->field;                               \
+        const field_type *ptr = codec2->field;                              \
         *out_configs = ptr;                                                 \
         if (ptr) {                                                          \
             for (int i = 0;; i++) {                                         \
@@ -769,7 +769,6 @@ int ff_default_get_supported_config(const AVCodecContext *avctx,
     const FFCodec *codec2 = ffcodec(codec);
 
     switch (config) {
-FF_DISABLE_DEPRECATION_WARNINGS
     case AV_CODEC_CONFIG_PIX_FORMAT:
         WRAP_CONFIG(AVMEDIA_TYPE_VIDEO, pix_fmts, pix_fmt, enum AVPixelFormat, pix_fmt == AV_PIX_FMT_NONE);
     case AV_CODEC_CONFIG_FRAME_RATE:
@@ -780,7 +779,6 @@ FF_DISABLE_DEPRECATION_WARNINGS
         WRAP_CONFIG(AVMEDIA_TYPE_AUDIO, sample_fmts, sample_fmt, enum AVSampleFormat, sample_fmt == AV_SAMPLE_FMT_NONE);
     case AV_CODEC_CONFIG_CHANNEL_LAYOUT:
         WRAP_CONFIG(AVMEDIA_TYPE_AUDIO, ch_layouts, ch_layout, AVChannelLayout, ch_layout.nb_channels == 0);
-FF_ENABLE_DEPRECATION_WARNINGS
 
     case AV_CODEC_CONFIG_COLOR_RANGE:
         if (codec->type != AVMEDIA_TYPE_VIDEO)
