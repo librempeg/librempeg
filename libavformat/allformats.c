542edad03093f4063206133dcf539139b2625141
Paul B Mahol <onemda@gmail.com>
2026-07-08 18:22:34 +0000

    avformat: add GBTS demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -255,6 +255,7 @@ extern const FFInputFormat  ff_g726le_demuxer;
 extern const FFOutputFormat ff_g726le_muxer;
 extern const FFInputFormat  ff_g728_demuxer;
 extern const FFInputFormat  ff_g729_demuxer;
+extern const FFInputFormat  ff_gbts_demuxer;
 extern const FFInputFormat  ff_gccstr_demuxer;
 extern const FFInputFormat  ff_gcstr_demuxer;
 extern const FFInputFormat  ff_gcsw_demuxer;
