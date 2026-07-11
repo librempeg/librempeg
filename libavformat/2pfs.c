ffea756342a2d825eb2c616c549385ef7fbc2f3a
Paul B Mahol <onemda@gmail.com>
2026-07-11 09:08:30 +0000

    avformat/2pfs: extend support for IAP too

--- libavformat/2pfs.c
+++ libavformat/2pfs.c
@@ -34,6 +34,8 @@ static int read_probe(const AVProbeData *p)
     if (AV_RL16(p->buf + 4) < 1 ||
         AV_RL16(p->buf + 4) > 2)
         return 0;
+    if (p->buf[16] < 1 || p->buf[16] > 3)
+        return 0;
     if (p->buf[0x40] == 0)
         return 0;
     if (AV_RL16(p->buf + 4) == 1)
@@ -47,19 +49,54 @@ static int read_probe(const AVProbeData *p)
 
 static int read_header(AVFormatContext *s)
 {
-    int channels, rate, version;
+    int align, channels, rate, version, type, nb_streams;
+    int64_t stream_offset, offset;
     AVIOContext *pb = s->pb;
     AVStream *st;
 
     avio_skip(pb, 4);
     version = avio_rl16(pb);
-    avio_skip(pb, 0x3a);
-    channels = avio_r8(pb);
-    avio_skip(pb, 3);
-    if (version == 2)
-        avio_skip(pb, 4);
-    rate = avio_rl32(pb);
-    if (channels == 0 || rate <= 0 || channels > INT_MAX/0x1000)
+    avio_skip(pb, 10);
+    type = avio_r8(pb);
+    switch (type) {
+    case 1:
+        avio_skip(pb, 39);
+        stream_offset = avio_rl32(pb) + 0x40;
+        nb_streams = avio_rl32(pb);
+        if (nb_streams != 1)
+            return AVERROR_PATCHWELCOME;
+        channels = avio_r8(pb);
+        avio_skip(pb, 1);
+        if (version == 1) {
+            avio_skip(pb, 2);
+            rate = avio_rl32(pb);
+        } else if (version == 2) {
+            avio_skip(pb, 6);
+            rate = avio_rl32(pb);
+        }
+        align = 0x1000;
+        break;
+    case 2:
+    case 3:
+        avio_skip(pb, 56);
+        stream_offset = avio_rl32(pb) + 0x50;
+        avio_skip(pb, 8);
+        nb_streams = avio_rl32(pb);
+        offset = 0x60 + 0x20 * 0;
+        avio_seek(pb, offset, SEEK_SET);
+        channels = avio_r8(pb);
+        avio_skip(pb, 7);
+        rate = avio_rl32(pb);
+        rate = av_rescale_rnd(rate, 48000, 4096, AV_ROUND_UP);
+        stream_offset = avio_rl32(pb);
+        /*stream_size = avio_rl32(pb);*/
+        align = 0x1000;
+        break;
+    default:
+        return AVERROR_INVALIDDATA;
+    }
+
+    if (align <= 0 || channels <= 0 || rate <= 0 || channels > INT_MAX/align)
         return AVERROR_INVALIDDATA;
 
     st = avformat_new_stream(s, NULL);
@@ -70,14 +107,14 @@ static int read_header(AVFormatContext *s)
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codecpar->sample_rate = rate;
     st->codecpar->ch_layout.nb_channels = channels;
-    st->codecpar->block_align = 0x1000 * channels;
+    st->codecpar->block_align = align * channels;
     st->codecpar->codec_id = AV_CODEC_ID_ADPCM_PSX;
     st->codecpar->bit_rate = 16LL * st->codecpar->ch_layout.nb_channels * 8 *
                                     st->codecpar->sample_rate / 28;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
-    avio_seek(pb, 0x800, SEEK_SET);
+    avio_seek(pb, stream_offset, SEEK_SET);
 
     return 0;
 }
@@ -85,7 +122,7 @@ static int read_header(AVFormatContext *s)
 const FFInputFormat ff_twopfs_demuxer = {
     .p.name         = "2pfs",
     .p.long_name    = NULL_IF_CONFIG_SMALL("Konami PS2 2PFS"),
-    .p.extensions   = "sap",
+    .p.extensions   = "sap,iap",
     .p.flags        = AVFMT_GENERIC_INDEX,
     .read_probe     = read_probe,
     .read_header    = read_header,
