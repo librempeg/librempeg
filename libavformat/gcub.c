e2087d344779e9a95ab181230998e518e82b09e8
Paul B Mahol <onemda@gmail.com>
2026-07-28 17:36:43 +0000

    avformat/gcub: rewrite bit_rate into single line

--- libavformat/gcub.c
+++ libavformat/gcub.c
@@ -67,8 +67,7 @@ static int read_header(AVFormatContext *s)
     st->codecpar->ch_layout.nb_channels = channels;
     st->codecpar->sample_rate = rate;
     st->codecpar->block_align = align * channels;
-    st->codecpar->bit_rate = 8LL * st->codecpar->ch_layout.nb_channels * 8 *
-                                   st->codecpar->sample_rate / 14;
+    st->codecpar->bit_rate = 8LL * channels * 8 * rate / 14;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
