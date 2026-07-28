6f205392720cde5c24ecb8586f6c9236d402b485
Paul B Mahol <onemda@gmail.com>
2026-07-28 13:46:03 +0000

    avformat/bwav: move stream allocation after checks

--- libavformat/bwav.c
+++ libavformat/bwav.c
@@ -40,56 +40,65 @@ static int read_probe(const AVProbeData *p)
 
 static int read_header(AVFormatContext *s)
 {
+    int ret, codec, rate, nb_channels, align;
+    int64_t offset, duration;
     AVIOContext *pb = s->pb;
-    int ret, codec;
-    int64_t offset;
     AVStream *st;
 
     avio_skip(pb, 14);
-
-    st = avformat_new_stream(s, NULL);
-    if (!st)
-        return AVERROR(ENOMEM);
-
-    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->ch_layout.nb_channels = avio_rl16(pb);
-    if (st->codecpar->ch_layout.nb_channels <= 0)
-        return AVERROR_INVALIDDATA;
+    nb_channels = avio_rl16(pb);
     codec = avio_rl16(pb);
     avio_skip(pb, 2);
-    st->codecpar->sample_rate = avio_rl32(pb);
+    rate = avio_rl32(pb);
     avio_skip(pb, 4);
-    st->start_time = 0;
-    st->duration = avio_rl32(pb);
-    st->codecpar->block_align = 1024;
+    duration = avio_rl32(pb);
+
+    if (nb_channels > 1) {
+        avio_seek(pb, 0x8c, SEEK_SET);
+        align = avio_rl32(pb);
+    } else {
+        align = 1024;
+    }
 
     switch (codec) {
     case 0:
-        st->codecpar->codec_id = AV_CODEC_ID_PCM_S16LE_PLANAR;
+        codec = AV_CODEC_ID_PCM_S16LE_PLANAR;
         break;
     case 1:
-        st->codecpar->codec_id = AV_CODEC_ID_ADPCM_NDSP_LE;
+        codec = AV_CODEC_ID_ADPCM_NDSP_LE;
         break;
     default:
         avpriv_request_sample(s, "codec %d", codec);
         return AVERROR_PATCHWELCOME;
     }
 
+    if (align <= 0 || rate <= 0 || nb_channels <= 0 || nb_channels > INT_MAX/align)
+        return AVERROR_INVALIDDATA;
+
+    st = avformat_new_stream(s, NULL);
+    if (!st)
+        return AVERROR(ENOMEM);
+
+    st->start_time = 0;
+    st->duration = duration;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->codec_id = codec;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->ch_layout.nb_channels = nb_channels;
+    st->codecpar->block_align = align * nb_channels;
+
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
-    avio_seek(pb, 0x20, SEEK_SET);
-    if ((ret = ff_alloc_extradata(st->codecpar, 32 * st->codecpar->ch_layout.nb_channels)) < 0)
-        return ret;
-    for (int ch = 0; ch < st->codecpar->ch_layout.nb_channels; ch++) {
-        avio_read(pb, st->codecpar->extradata + ch * 32, 32);
-        avio_skip(pb, 0x2c);
-    }
+    if (st->codecpar->codec_id == AV_CODEC_ID_ADPCM_NDSP_LE) {
+        avio_seek(pb, 0x20, SEEK_SET);
+        if ((ret = ff_alloc_extradata(st->codecpar, 32 * st->codecpar->ch_layout.nb_channels)) < 0)
+            return ret;
 
-    avio_seek(pb, 0x8c, SEEK_SET);
-    if (st->codecpar->ch_layout.nb_channels > 1)
-        st->codecpar->block_align = avio_rl32(pb) * st->codecpar->ch_layout.nb_channels;
-    if (!st->codecpar->block_align)
-        return AVERROR_INVALIDDATA;
+        for (int ch = 0; ch < st->codecpar->ch_layout.nb_channels; ch++) {
+            avio_read(pb, st->codecpar->extradata + ch * 32, 32);
+            avio_skip(pb, 0x2c);
+        }
+    }
 
     avio_seek(pb, 0x44, SEEK_SET);
     offset = avio_rl32(pb);
