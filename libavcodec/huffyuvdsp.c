493eeede4e3521b2f8a47758fca1e2968fc15113
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2026-07-30 01:56:32 +0200

    avcodec/huffyuvdsp: Remove pix_fmt parameter from ff_huffyuvdsp_init()
    It is unused since the mmx add_hfyu_median_pred_int16 function
    has been removed.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/huffyuvdsp.c
+++ libavcodec/huffyuvdsp.c
@@ -81,15 +81,15 @@ static void add_hfyu_left_pred_bgr32_c(uint8_t *dst, const uint8_t *src,
     left[A] = a;
 }
 
-av_cold void ff_huffyuvdsp_init(HuffYUVDSPContext *c, enum AVPixelFormat pix_fmt)
+av_cold void ff_huffyuvdsp_init(HuffYUVDSPContext *c)
 {
     c->add_int16 = add_int16_c;
     c->add_hfyu_median_pred_int16 = add_hfyu_median_pred_int16_c;
     c->add_hfyu_left_pred_bgr32 = add_hfyu_left_pred_bgr32_c;
 
 #if ARCH_RISCV
-    ff_huffyuvdsp_init_riscv(c, pix_fmt);
+    ff_huffyuvdsp_init_riscv(c);
 #elif ARCH_X86
-    ff_huffyuvdsp_init_x86(c, pix_fmt);
+    ff_huffyuvdsp_init_x86(c);
 #endif
 }
