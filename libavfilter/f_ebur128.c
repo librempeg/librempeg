23069ca6200c54eba3296cacea5bb262014ad562
Ayoub Nabil <237098474+ayoubnabil@users.noreply.github.com>
2026-08-05 19:35:50 +0200

    avfilter/f_ebur128: fix top-back channel weighting
    ITU-R BS.1770 assigns a weight of 1.0 to height channels, but the
    filter included the top-back channels in the 1.41 surround mask.
    
    remove the top-back channels from that mask and add a FATE test for
    the resulting loudness measurement.
    
    fixes #23968.
    
    Signed-off-by: Ayoub Nabil <237098474+ayoubnabil@users.noreply.github.com>

--- libavfilter/f_ebur128.c
+++ libavfilter/f_ebur128.c
@@ -470,10 +470,11 @@ static int config_audio_out(AVFilterLink *outlink, EBUR128Context *ebur128)
 {
     const int nb_channels = outlink->ch_layout.nb_channels;
 
-#define BACK_MASK (AV_CH_BACK_LEFT    |AV_CH_BACK_CENTER    |AV_CH_BACK_RIGHT| \
-                   AV_CH_TOP_BACK_LEFT|AV_CH_TOP_BACK_CENTER|AV_CH_TOP_BACK_RIGHT| \
-                   AV_CH_SIDE_LEFT                          |AV_CH_SIDE_RIGHT| \
-                   AV_CH_SURROUND_DIRECT_LEFT               |AV_CH_SURROUND_DIRECT_RIGHT)
+    /* Height channels always use a weight of 1.0 in ITU-R BS.1770. */
+#define WEIGHT_1_41_MASK (AV_CH_BACK_LEFT             |AV_CH_BACK_CENTER          | \
+                          AV_CH_BACK_RIGHT            |AV_CH_SIDE_LEFT            | \
+                          AV_CH_SIDE_RIGHT            |AV_CH_SURROUND_DIRECT_LEFT | \
+                          AV_CH_SURROUND_DIRECT_RIGHT)
 
     ebur128->nb_channels  = nb_channels;
     ebur128->t0           = av_calloc(nb_channels, 4 * sizeof(*ebur128->t0));
@@ -535,7 +536,7 @@ static int config_audio_out(AVFilterLink *outlink, EBUR128Context *ebur128)
         if (chl == AV_CHAN_LOW_FREQUENCY || chl == AV_CHAN_LOW_FREQUENCY_2) {
             ebur128->ch_weighting[i] = 0;
             ebur128->nozero_ch_weighting = 0;
-        } else if (chl < 64 && (1ULL << chl) & BACK_MASK) {
+        } else if (chl < 64 && (1ULL << chl) & WEIGHT_1_41_MASK) {
             ebur128->ch_weighting[i] = 1.41;
         } else {
             ebur128->ch_weighting[i] = 1.0;
