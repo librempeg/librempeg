f5502f66b2d0a0758261ad86ae0077e9478cdbce
Paul B Mahol <onemda@gmail.com>
2026-08-12 13:58:58 +0000

    avfilter/af_afdelay: use ff_slice_pos()

--- libavfilter/af_afdelay.c
+++ libavfilter/af_afdelay.c
@@ -115,8 +115,8 @@ static int filter_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_jo
     ThreadData *td = arg;
     AVFrame *out = td->out;
     AVFrame *in = td->in;
-    const int start = (in->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (in->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(in->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(in->ch_layout.nb_channels, jobnr+1, nb_jobs);
 
     for (int ch = start; ch < end; ch++) {
         const uint8_t *src = in->extended_data[ch];
