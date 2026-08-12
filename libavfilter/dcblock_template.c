d3b8f46269cdfdd538389625fdaf80048dc8eb78
Paul B Mahol <onemda@gmail.com>
2026-08-12 13:29:36 +0000

    avfilter/dcblock_template: use ff_slice_pos()

--- libavfilter/dcblock_template.c
+++ libavfilter/dcblock_template.c
@@ -92,8 +92,8 @@ static int fn(filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int n
     ThreadData *td = arg;
     AVFrame *out = td->out;
     AVFrame *in = td->in;
-    const int start = (in->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (in->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(in->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(in->ch_layout.nb_channels, jobnr+1, nb_jobs);
     fn(StateContext) *stc = s->st;
 
     for (int ch = start; ch < end; ch++) {
