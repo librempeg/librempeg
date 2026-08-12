962e802b93a0f8e10e04e54d18d5c4dbf511e950
Paul B Mahol <onemda@gmail.com>
2026-08-12 14:05:40 +0000

    avfilter/af_aasrc: use ff_slice_pos()

--- libavfilter/af_aasrc.c
+++ libavfilter/af_aasrc.c
@@ -210,8 +210,8 @@ static int aasrc_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_job
 {
     AASRCContext *s = ctx->priv;
     AVFrame *out = arg;
-    const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(out->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(out->ch_layout.nb_channels, jobnr+1, nb_jobs);
 
     for (int ch = start; ch < end; ch++)
         s->do_aasrc(ctx, s->in, out, ch);
