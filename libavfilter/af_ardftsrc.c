b472ab651990a81b0d4f34ad056abd832edd2a06
Paul B Mahol <onemda@gmail.com>
2026-08-12 14:15:04 +0000

    avfilter/af_ardftsrc: add forgotten ff_slice_pos()

--- libavfilter/af_ardftsrc.c
+++ libavfilter/af_ardftsrc.c
@@ -322,8 +322,8 @@ static int src_out_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_j
 {
     AudioRDFTSRCContext *s = ctx->priv;
     AVFrame *out = arg;
-    const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(out->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(out->ch_layout.nb_channels, jobnr+1, nb_jobs);
 
     for (int ch = start; ch < end; ch++)
         s->do_src_out(ctx, out, ch, 0, 1);
