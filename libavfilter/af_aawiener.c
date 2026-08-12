1edb1c3d98b770ac23063d0a64a703ba27fc6d8d
Paul B Mahol <onemda@gmail.com>
2026-08-12 17:54:21 +0000

    avfilter/af_aawiener: use ff_slice_pos()

--- libavfilter/af_aawiener.c
+++ libavfilter/af_aawiener.c
@@ -94,8 +94,8 @@ static int awiener_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_j
 {
     AudioAWienerContext *s = ctx->priv;
     AVFrame *out = arg;
-    const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(out->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(out->ch_layout.nb_channels, jobnr+1, nb_jobs);
 
     for (int ch = start; ch < end; ch++)
         s->do_awiener(ctx, s->in, out, ch);
