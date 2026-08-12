f83ec007ce48341a4aec67fd756d75afd07e1e48
Paul B Mahol <onemda@gmail.com>
2026-08-12 13:27:53 +0000

    avfilter/alimiter_template: use ff_slice_pos()

--- libavfilter/alimiter_template.c
+++ libavfilter/alimiter_template.c
@@ -244,8 +244,8 @@ static int fn(filter_channels)(AVFilterContext *ctx, void *arg, int jobnr, int n
     AVFrame *sc = s->sc ? s->sc : s->in;
     AVFrame *in = s->in;
     AVFrame *out = arg;
-    const int start = (in->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (in->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(in->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(in->ch_layout.nb_channels, jobnr+1, nb_jobs);
     const int is_disabled = ff_filter_disabled(ctx);
     const int nb_samples = in->nb_samples;
     const ftype limit = s->limit;
