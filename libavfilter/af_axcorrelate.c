107aaf2e00cd1c910005691ca3b4a7d9a682cbfc
Paul B Mahol <onemda@gmail.com>
2026-08-13 07:20:36 +0000

    avfilter/af_axcorrelate: use ff_slice_pos()

--- libavfilter/af_axcorrelate.c
+++ libavfilter/af_axcorrelate.c
@@ -60,8 +60,8 @@ static int filter_channels(AVFilterContext *ctx, void *arg, int jobnr, int nb_jo
 {
     AVFrame *out = arg;
     AudioXCorrelateContext *s = ctx->priv;
-    const int start = (out->ch_layout.nb_channels * jobnr) / nb_jobs;
-    const int end = (out->ch_layout.nb_channels * (jobnr+1)) / nb_jobs;
+    const int start = ff_slice_pos(out->ch_layout.nb_channels, jobnr, nb_jobs);
+    const int end = ff_slice_pos(out->ch_layout.nb_channels, jobnr+1, nb_jobs);
 
     for (int ch = start; ch < end; ch++)
         s->xcorrelate(ctx, out, ch);
