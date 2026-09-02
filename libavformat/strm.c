2d2e8c94049316d35ca363a129b0112da6c3b0d5
Paul B Mahol <onemda@gmail.com>
2026-09-02 09:56:31 +0000

    avformat/strm: small cleanup

--- libavformat/strm.c
+++ libavformat/strm.c
@@ -54,13 +54,13 @@ static int read_probe(const AVProbeData *p)
 
 static int read_header(AVFormatContext *s)
 {
-    int codec, rate, nb_channels;
+    int codec, rate, nb_channels, align;
     AVIOContext *pb = s->pb;
     int64_t start, duration;
     AVStream *st;
 
     avio_skip(pb, 20);
-    start = avio_rl32(pb) + 24;
+    start = avio_rl32(pb) + 24LL;
     codec = avio_r8(pb);
     avio_skip(pb, 1);
     nb_channels = avio_r8(pb);
@@ -69,39 +69,39 @@ static int read_header(AVFormatContext *s)
     avio_skip(pb, 6);
     duration = avio_rl32(pb);
 
-    if (rate <= 0 || nb_channels <= 0)
+    switch (codec) {
+    case 0:
+        codec = AV_CODEC_ID_PCM_S8;
+        align = 1;
+        break;
+    case 1:
+        codec = AV_CODEC_ID_PCM_S16LE;
+        align = 2;
+        break;
+    case 2:
+        codec = AV_CODEC_ID_ADPCM_IMA_NDS;
+        avio_skip(pb, 0x30 - avio_tell(pb));
+        align = avio_rl32(pb);
+        break;
+    default:
+        avpriv_request_sample(s, "codec 0x%X", codec);
+        return AVERROR_PATCHWELCOME;
+    }
+
+    if (rate <= 0 || nb_channels <= 0 || align <= 0 || nb_channels >= INT_MAX/align)
         return AVERROR_INVALIDDATA;
 
     st = avformat_new_stream(s, NULL);
     if (!st)
         return AVERROR(ENOMEM);
 
-    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    st->codecpar->ch_layout.nb_channels = nb_channels;
     st->start_time = 0;
     st->duration = duration;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->codec_id = codec;
+    st->codecpar->ch_layout.nb_channels = nb_channels;
     st->codecpar->sample_rate = rate;
-
-    switch (codec) {
-    case 0:
-        st->codecpar->codec_id = AV_CODEC_ID_PCM_S8;
-        st->codecpar->block_align = nb_channels;
-        break;
-    case 1:
-        st->codecpar->codec_id = AV_CODEC_ID_PCM_S16LE;
-        st->codecpar->block_align = 2 * nb_channels;
-        break;
-    case 2:
-        st->codecpar->codec_id = AV_CODEC_ID_ADPCM_IMA_NDS;
-        avio_skip(pb, 0x30 - avio_tell(pb));
-        st->codecpar->block_align = avio_rl32(pb) * nb_channels;
-        if (st->codecpar->block_align <= 0)
-            return AVERROR_INVALIDDATA;
-        break;
-    default:
-        avpriv_request_sample(s, "codec 0x%X", codec);
-        return AVERROR_PATCHWELCOME;
-    }
+    st->codecpar->block_align = align * nb_channels;
 
     avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
 
