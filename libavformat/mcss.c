7b6710a6f811d1f235dbc4cf5c2f9d75b9e9847e
Paul B Mahol <onemda@gmail.com>
2026-09-02 08:49:13 +0000

    avformat: add MCSS demuxer

---
+++ libavformat/mcss.c
@@ -0,0 +1,92 @@
+/*
+ * Guerrilla MSS demuxer
+ * Copyright (c) 2026 Paul B Mahol
+ *
+ * This file is part of Librempeg
+ *
+ * Librempeg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Librempeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Librempeg; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "libavutil/intreadwrite.h"
+#include "libavutil/mem.h"
+#include "avformat.h"
+#include "demux.h"
+#include "internal.h"
+#include "pcm.h"
+
+static int read_probe(const AVProbeData *p)
+{
+    if (AV_RB32(p->buf) != MKBETAG('M','C','S','S'))
+        return 0;
+
+    if (p->buf_size < 28)
+        return 0;
+    if (AV_RL32(p->buf+8) == 0)
+        return 0;
+    if ((int)AV_RL32(p->buf+16) <= 0)
+        return 0;
+    if ((int)AV_RL16(p->buf+22) <= 0)
+        return 0;
+    if ((int)AV_RL32(p->buf+24) <= 0)
+        return 0;
+
+    return AVPROBE_SCORE_MAX;
+}
+
+static int read_header(AVFormatContext *s)
+{
+    int rate, channels, align;
+    AVIOContext *pb = s->pb;
+    int64_t start;
+    AVStream *st;
+
+    avio_skip(pb, 8);
+    start = avio_rl32(pb);
+    avio_skip(pb, 4);
+    rate = avio_rl32(pb);
+    avio_skip(pb, 2);
+    channels = avio_rl16(pb);
+    align = avio_rl32(pb);
+    if (align <= 0 || rate <= 0 || channels <= 0 || align >= INT_MAX/channels)
+        return AVERROR_INVALIDDATA;
+
+    st = avformat_new_stream(s, NULL);
+    if (!st)
+        return AVERROR(ENOMEM);
+
+    st->start_time = 0;
+    st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->codec_id = AV_CODEC_ID_ADPCM_PSX;
+    st->codecpar->ch_layout.nb_channels = channels;
+    st->codecpar->sample_rate = rate;
+    st->codecpar->block_align = align * channels;
+    st->codecpar->bit_rate = 16LL * channels * 8 * rate / 28;
+
+    avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+    avio_seek(pb, start, SEEK_SET);
+
+    return 0;
+}
+
+const FFInputFormat ff_mcss_demuxer = {
+    .p.name         = "mcss",
+    .p.long_name    = NULL_IF_CONFIG_SMALL("Guerrilla MSS"),
+    .p.flags        = AVFMT_GENERIC_INDEX,
+    .p.extensions   = "mss",
+    .read_probe     = read_probe,
+    .read_header    = read_header,
+    .read_packet    = ff_pcm_read_packet,
+};
