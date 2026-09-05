0778ec81418dc7450f3f618c0321084743d65df9
Paul B Mahol <onemda@gmail.com>
2026-09-05 17:43:30 +0000

    avformat: add WADP demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -749,6 +749,7 @@ extern const FFInputFormat  ff_vxds_demuxer;
 extern const FFInputFormat  ff_vxn_demuxer;
 extern const FFInputFormat  ff_w64_demuxer;
 extern const FFOutputFormat ff_w64_muxer;
+extern const FFInputFormat  ff_wadp_demuxer;
 extern const FFInputFormat  ff_wady_demuxer;
 extern const FFInputFormat  ff_waf_demuxer;
 extern const FFInputFormat  ff_wav2_demuxer;
