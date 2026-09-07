82442e2b229f88ef38e93de44ccb62b0a4197a32
Paul B Mahol <onemda@gmail.com>
2026-09-07 21:43:42 +0000

    avformat: add RAWX demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -566,6 +566,7 @@ extern const FFInputFormat  ff_raki_demuxer;
 extern const FFInputFormat  ff_raud_demuxer;
 extern const FFInputFormat  ff_rawvideo_demuxer;
 extern const FFOutputFormat ff_rawvideo_muxer;
+extern const FFInputFormat  ff_rawx_demuxer;
 extern const FFInputFormat  ff_rcavoc_demuxer;
 extern const FFInputFormat  ff_rcwt_demuxer;
 extern const FFOutputFormat ff_rcwt_muxer;
