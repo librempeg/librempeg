6ed5133c0dce860b1981fbd3fbbab68a3273c38d
Paul B Mahol <onemda@gmail.com>
2026-09-11 10:20:27 +0000

    avformat/dckcey: use ff_pcm_read_packet()

--- libavformat/dckcey.c
+++ libavformat/dckcey.c
@@ -24,6 +24,7 @@
 #include "avformat.h"
 #include "demux.h"
 #include "internal.h"
+#include "pcm.h"
 
 static int read_probe(const AVProbeData *p)
 {
@@ -71,19 +72,6 @@ static int read_header(AVFormatContext *s)
     return 0;
 }
 
-static int read_packet(AVFormatContext *s, AVPacket *pkt)
-{
-    AVCodecParameters *par = s->streams[0]->codecpar;
-    AVIOContext *pb = s->pb;
-    int ret;
-
-    ret = av_get_packet(pb, pkt, par->block_align);
-    pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
-    pkt->stream_index = 0;
-
-    return ret;
-}
-
 const FFInputFormat ff_dckcey_demuxer = {
     .p.name         = "dckcey",
     .p.long_name    = NULL_IF_CONFIG_SMALL("Konami Dreamcast KCEY"),
@@ -91,5 +79,5 @@ const FFInputFormat ff_dckcey_demuxer = {
     .p.extensions   = "kcey",
     .read_probe     = read_probe,
     .read_header    = read_header,
-    .read_packet    = read_packet,
+    .read_packet    = ff_pcm_read_packet,
 };
