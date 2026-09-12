09c0e8ad3e494081ff8f26d2b33b9fb552ca5828
Paul B Mahol <onemda@gmail.com>
2026-09-12 18:23:35 +0000

    avcodec/adpcm: improve quality of ADPCM_MS output

--- libavcodec/adpcm.c
+++ libavcodec/adpcm.c
@@ -896,7 +896,7 @@ static inline int16_t adpcm_ms_expand_nibble(ADPCMChannelStatus *c, int nibble)
 {
     int predictor;
 
-    predictor = (((c->sample1) * (c->coeff1)) + ((c->sample2) * (c->coeff2))) / 64;
+    predictor = (((c->sample1) * (c->coeff1)) + ((c->sample2) * (c->coeff2))) >> 6;
     predictor += ((nibble & 0x08)?(nibble - 0x10):(nibble)) * c->idelta;
 
     c->sample2 = c->sample1;
