c8dcc9fe771f962a8891227a6d74f1a336e1739f
Paul B Mahol <onemda@gmail.com>
2026-09-16 21:19:49 +0000

    avfilter/avf_showcwt: add stereoquad intensity mode

--- libavfilter/avf_showcwt.c
+++ libavfilter/avf_showcwt.c
@@ -101,6 +101,7 @@ enum DisplayMode {
     STEREOCOOL,
     MONOCORR,
     STEREOCORR,
+    STEREOQUAD,
     NB_MODE
 };
 
@@ -216,6 +217,7 @@ static const AVOption showcwt_options[] = {
     {  "stereocool","stereo cool intensity",0,AV_OPT_TYPE_CONST,{.i64=STEREOCOOL},0,0,TFLAGS, .unit="mode" },
     {  "monocorr",  "mono corr intensity",0,AV_OPT_TYPE_CONST,{.i64=MONOCORR},  0, 0, TFLAGS, .unit="mode" },
     {  "stereocorr","stereo corr intensity",0,AV_OPT_TYPE_CONST,{.i64=STEREOCORR},0,0,TFLAGS, .unit="mode" },
+    {  "stereoquad","stereo quad intensity",0,AV_OPT_TYPE_CONST,{.i64=STEREOQUAD},0,0,TFLAGS, .unit="mode" },
     { "slide", "set slide mode", OFFSET(slide), AV_OPT_TYPE_INT,  {.i64=0}, 0, NB_SLIDE-1, FLAGS, .unit="slide" },
     {  "replace", "replace", 0, AV_OPT_TYPE_CONST,{.i64=SLIDE_REPLACE},0, 0, FLAGS, .unit="slide" },
     {  "scroll",  "scroll",  0, AV_OPT_TYPE_CONST,{.i64=SLIDE_SCROLL}, 0, 0, FLAGS, .unit="slide" },
@@ -679,6 +681,43 @@ static int draw(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
 skip:
 
         switch (mode) {
+        case STEREOQUAD:
+            {
+                const AVComplexFloat *src2 = (nb_channels > 1) ? src + ihop_size: src;
+                float a, b, c, z, u, v, corr, re, im;
+
+                u = hypotf(src[0].re, src[0].im);
+                v = hypotf(src2[0].re, src2[0].im);
+                z = 0.5f * (u + v);
+                b = (1.f - (fminf(u, v) + FLT_EPSILON) / (fmaxf(u, v) + FLT_EPSILON));
+                c = (u < v) ? 1.f : -1.f;
+
+                re = src[0].re * src2[0].re + src[0].im * src2[0].im;
+                im = src2[0].re * src[0].im - src2[0].im * src[0].re;
+                corr = atan2f(im, re);
+                a = hypotf(re, im);
+
+                a = remap_log(s, a, weight, iscale, log_factor);
+                z = remap_log(s, z, weight, iscale, log_factor);
+
+                Y = z;
+                a = hypotf(z, a) * 0.2f;
+                z = cosf(corr);
+                U = a * c * b + 0.5f;
+                V = a * ((z > 0.f) ? 1.f - z : z) + 0.5f;
+
+                if (sono_size > 0) {
+                    dstY[0] = av_clip_uint8(lrintf(Y * 255.f));
+                    dstU[0] = av_clip_uint8(lrintf(U * 255.f));
+                    dstV[0] = av_clip_uint8(lrintf(V * 255.f));
+                    if (dstA)
+                        dstA[0] = dstY[0];
+                }
+
+                if (bar_size > 0)
+                    draw_bar(s, y, Y, U, V);
+            }
+            break;
         case STEREOCORR:
             {
                 const AVComplexFloat *src2 = (nb_channels > 1) ? src + ihop_size: src;
