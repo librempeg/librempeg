6330fc4100ad6d25f4efcca5db2c14c44dff9920
Paul B Mahol <onemda@gmail.com>
2026-09-19 12:27:29 +0000

    avformat/mgs2s: add support for different layout

--- libavformat/mgs2s.c
+++ libavformat/mgs2s.c
@@ -36,7 +36,7 @@ static int read_probe(const AVProbeData *p)
         return 0;
     if (AV_RB16(p->buf+6) <= 0)
         return 0;
-    if (p->buf[8]/2 == 0)
+    if (p->buf[8] == 0)
         return 0;
     if (AV_RL16(p->buf+10) != 17)
         return 0;
@@ -46,14 +46,26 @@ static int read_probe(const AVProbeData *p)
 
 static int read_header(AVFormatContext *s)
 {
-    int streams, rate, channels;
+    int align, streams, rate, channels;
     AVIOContext *pb = s->pb;
+    int64_t start;
     AVStream *st;
 
-    avio_skip(pb, 6);
+    start = avio_size(pb) - avio_rb32(pb);
+    start = FFABS(start);
+    start = FFMIN(start, 0x800);
+    align = (start == 0x10) ? 0x400 : 0x800;
+
+    avio_skip(pb, 2);
     rate = avio_rb16(pb);
-    streams = avio_r8(pb)/2;
-    channels = 2;
+    if (align == 0x800) {
+        streams = avio_r8(pb)/2;
+        channels = 2;
+    } else {
+        channels = avio_r8(pb);
+        streams = 1;
+        align *= channels;
+    }
     if (streams <= 0 || rate <= 0 || channels <= 0)
         return AVERROR_INVALIDDATA;
 
@@ -64,31 +76,38 @@ static int read_header(AVFormatContext *s)
 
         st->start_time = 0;
         st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-        st->codecpar->codec_id = AV_CODEC_ID_ADPCM_MS;
+        st->codecpar->codec_id = (start == 0x10) ? AV_CODEC_ID_ADPCM_MS_MONO : AV_CODEC_ID_ADPCM_MS;
         st->codecpar->ch_layout.nb_channels = channels;
         st->codecpar->sample_rate = rate;
-        st->codecpar->block_align = 0x800;
-        st->codecpar->bit_rate = 8LL * channels * 0x800 * rate / (2 + (0x800 - 7 * channels) * 2);
+        st->codecpar->block_align = align;
+        st->codecpar->bit_rate = 8LL * channels * align * rate / (2 + (align - 7 * channels) * 2);
 
         avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
     }
 
-    avio_seek(pb, 0x800, SEEK_SET);
+    avio_seek(pb, start, SEEK_SET);
 
     return 0;
 }
 
 static int read_packet(AVFormatContext *s, AVPacket *pkt)
 {
+    const int64_t offset = ffformatcontext(s)->data_offset;
+    int block_align = s->streams[0]->codecpar->block_align;
     AVIOContext *pb = s->pb;
     int64_t pos;
     int ret;
 
     pos = avio_tell(pb);
-    ret = av_get_packet(s->pb, pkt, 0x800);
+    if (offset == 0x10)
+        block_align *= 2;
+    ret = av_get_packet(s->pb, pkt, block_align);
 
     pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
-    pkt->stream_index = ((pos - 0x800) / 0x1000) % s->nb_streams;
+    if (offset != 0x10)
+        pkt->stream_index = ((pos - offset) / 0x1000) % s->nb_streams;
+    else
+        pkt->stream_index = 0;
 
     return ret;
 }
