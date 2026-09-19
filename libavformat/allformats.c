f120e1373301a7b39c8452d104f5e512088f5699
Paul B Mahol <onemda@gmail.com>
2026-09-19 20:57:27 +0000

    avformat: add OPUSNX demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -499,6 +499,7 @@ extern const FFInputFormat  ff_omu_demuxer;
 extern const FFInputFormat  ff_oor_demuxer;
 extern const FFInputFormat  ff_opns_demuxer;
 extern const FFOutputFormat ff_opus_muxer;
+extern const FFInputFormat  ff_opusnx_demuxer;
 extern const FFInputFormat  ff_osq_demuxer;
 extern const FFInputFormat  ff_p3d_demuxer;
 extern const FFInputFormat  ff_paf_demuxer;
